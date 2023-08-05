cf2b2abd7577f7e307bfdb862703f6326442dfba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:16:33 +0000

    Add package prettymaps/0.1.3/prettymaps-0.1.3.tar.gz

---
+++ packages/prettymaps/prettymaps-0.1.3.tar.gz/prettymaps-0.1.3/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+parent_dir = Path(__file__).resolve().parent
+
+setup(
+    name="prettymaps",
+    version="v0.1.3",
+    description="A simple python library to draw pretty maps from OpenStreetMap data",
+    long_description=parent_dir.joinpath("README.md").read_text(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/marceloprates/prettymaps",
+    author="Marcelo Prates",
+    author_email="marceloorp@gmail.com",
+    license="MIT License",
+    packages=find_packages(exclude=("assets", "notebooks", "prints", "script")),
+    install_requires=parent_dir.joinpath("requirements.txt").read_text().splitlines(),
+    classifiers=[
+        "Intended Audience :: Science/Research",
+    ],
+)
