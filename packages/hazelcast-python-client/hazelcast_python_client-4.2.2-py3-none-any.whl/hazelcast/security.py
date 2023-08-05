9ff18c6558a899ca65dd6ff900628d2b67c9356c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:03 +0000

    Add package hazelcast-python-client/4.2.2/hazelcast_python_client-4.2.2-py3-none-any.whl

---
+++ packages/hazelcast-python-client/hazelcast_python_client-4.2.2-py3-none-any.whl/hazelcast/security.py
@@ -0,0 +1,33 @@
+from hazelcast.six import string_types
+from hazelcast.core import Address
+
+
+class TokenProvider(object):
+    """TokenProvider is a base class for token providers."""
+
+    def token(self, address=None):
+        # type: (TokenProvider, Address) -> bytes
+        """Returns a token to be used for token-based authentication.
+
+        Args:
+            address (hazelcast.core.Address): Connected address for the member.
+
+        Returns:
+            bytes: token as a bytes object.
+        """
+        pass
+
+
+class BasicTokenProvider(TokenProvider):
+    """BasicTokenProvider sends the given token to the authentication endpoint."""
+
+    def __init__(self, token=""):
+        if isinstance(token, string_types):
+            self._token = token.encode("utf-8")
+        elif isinstance(token, bytes):
+            self._token = token
+        else:
+            raise TypeError("token must be either a str or bytes object")
+
+    def token(self, address=None):
+        return self._token
