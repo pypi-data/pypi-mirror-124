a52816fd629abe43f12a3e4f24a4ead76a8ed926
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:20:38 +0000

    Add package sbol3/1.0b9/sbol3-1.0b9.tar.gz

---
+++ packages/sbol3/sbol3-1.0b9.tar.gz/sbol3-1.0b9/sbol3/__init__.py
@@ -0,0 +1,60 @@
+__version__ = '1.0b9'
+
+# ---------- BEGIN WARNING WORKAROUND ----------
+# Avoid unsightly warnings caused by rdflib 6, and manifested
+# during the import of owlrl either directly or via pyshacl.
+# See https://github.com/RDFLib/OWL-RL/issues/44
+#
+# TODO: Delete this workaround when owlrl handles these warnings
+#
+import warnings
+with warnings.catch_warnings():
+    warnings.simplefilter("ignore", category=UserWarning)
+    import owlrl
+# ---------- END WARNING WORKAROUND ----------
+
+from .constants import *
+from .config import set_defaults, get_namespace, set_namespace
+# get_homespace and set_homespace are deprecated and included for backward compatibility
+from .config import get_homespace, set_homespace
+from .error import *
+from .validation import *
+from .object import SBOLObject
+from .property_base import Property, SingletonProperty, ListProperty
+from .text_property import TextSingletonProperty, TextProperty
+from .int_property import IntProperty
+from .float_property import FloatProperty
+from .boolean_property import BooleanProperty
+from .datetime_property import DateTimeProperty
+from .uri_property import URIProperty
+from .ownedobject import OwnedObject
+from .refobj_property import ReferencedObject
+from .identified import Identified
+from .toplevel import TopLevel
+from .custom import CustomIdentified, CustomTopLevel
+from .document import Document
+from .constraint import Constraint
+from .sequence import Sequence
+from .feature import Feature
+from .location import Location, Range, Cut, EntireSequence
+from .varcomp import VariableFeature
+from .participation import Participation
+from .interaction import Interaction
+from .interface import Interface
+from .component import Component
+from .model import Model
+from .subcomponent import SubComponent
+from .localsub import LocalSubComponent
+from .seqfeat import SequenceFeature
+from .collection import Collection, Experiment
+from .combderiv import CombinatorialDerivation
+from .compref import ComponentReference
+from .extdef import ExternallyDefined
+from .implementation import Implementation
+from .attachment import Attachment
+from .expdata import ExperimentalData
+from .provenance import Activity, Agent, Association, Plan, Usage
+from .om_prefix import BinaryPrefix, SIPrefix
+from .om_unit import Measure, PrefixedUnit, SingularUnit
+from .om_compound import UnitDivision, UnitExponentiation, UnitMultiplication
+from .util import string_to_display_id
