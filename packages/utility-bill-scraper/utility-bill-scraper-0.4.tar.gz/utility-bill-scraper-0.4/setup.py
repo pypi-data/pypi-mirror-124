4955e5246ed98523030b1f780c971f6bd55049a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:13:02 +0000

    Add package utility-bill-scraper/0.4/utility-bill-scraper-0.4.tar.gz

---
+++ packages/utility-bill-scraper/utility-bill-scraper-0.4.tar.gz/utility-bill-scraper-0.4/setup.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['utility_bill_scraper',
+ 'utility_bill_scraper.bin',
+ 'utility_bill_scraper.canada.on']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['arrow>=1.2.0,<2.0.0',
+ 'beautifulsoup4>=4.10.0,<5.0.0',
+ 'google-api-python-client>=2.27.0,<3.0.0',
+ 'matplotlib>=3.4.3,<4.0.0',
+ 'numpy>=1.21.2,<2.0.0',
+ 'pandas>=1.3.3,<2.0.0',
+ 'pdfminer>=20191125,<20191126',
+ 'selenium>=3.141.0,<4.0.0']
+
+setup_kwargs = {
+    'name': 'utility-bill-scraper',
+    'version': '0.4',
+    'description': 'Utility bill scraper for extracting data from pdfs and websites.',
+    'long_description': '# Utility bill scraper\n\n[![build](https://github.com/ryanfobel/utility-bill-scraper/actions/workflows/build.yml/badge.svg?branch=main)](https://github.com/ryanfobel/utility-bill-scraper/actions/workflows/build.yml)\n[![Binder](https://mybinder.org/badge_logo.svg)](https://mybinder.org/v2/gh/ryanfobel/utility-bill-scraper/main)\n[![PyPI version shields.io](https://img.shields.io/pypi/v/utility-bill-scraper.svg)](https://pypi.python.org/pypi/utility-bill-scraper/)\n\nExtract energy usage and carbon footprint from utility websites or pdf bills. Currently, this library supports:\n\n * [Kitchener Utilities (gas & water)](https://mybinder.org/v2/gh/ryanfobel/utility-bill-scraper/main?labpath=notebooks%2Fcanada%2Fon%2Fkitchener_utilities.ipynb)\n\n# Install\n\n```\npip install utility-bill-scraper\n```',
+    'author': 'Ryan Fobel',
+    'author_email': 'ryan@fobel.net',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.8,<3.11',
+}
+
+
+setup(**setup_kwargs)
