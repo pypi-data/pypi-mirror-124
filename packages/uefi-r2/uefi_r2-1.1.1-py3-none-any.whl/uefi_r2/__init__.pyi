507c8e02af3a7f6b9fbcbe40e49865e3abb5595c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:13:16 +0000

    Add package uefi-r2/1.1.1/uefi_r2-1.1.1-py3-none-any.whl

---
+++ packages/uefi-r2/uefi_r2-1.1.1-py3-none-any.whl/uefi_r2/__init__.pyi
@@ -0,0 +1,3 @@
+from .uefi_analyzer import UefiAnalyzer as UefiAnalyzer
+from .uefi_scanner import UefiRule as UefiRule, UefiScanner as UefiScanner
+from .uefi_te import TerseExecutableParser as TerseExecutableParser
