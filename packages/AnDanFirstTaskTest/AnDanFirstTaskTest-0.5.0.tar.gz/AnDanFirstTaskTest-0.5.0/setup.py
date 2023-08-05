05b0eee6ccf883aa30a1a2b9d32bfca6edcaf9c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:15:05 +0000

    Add package AnDanFirstTaskTest/0.5.0/AnDanFirstTaskTest-0.5.0.tar.gz

---
+++ packages/AnDanFirstTaskTest/AnDanFirstTaskTest-0.5.0.tar.gz/AnDanFirstTaskTest-0.5.0/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+setup(
+    name='AnDanFirstTaskTest',
+    version='0.5.0',
+    description='A example Python package',
+    url='',
+    author='Deimoz',
+    author_email='',
+    license='BSD 2-clause',
+    packages=find_packages(),
+    install_requires=['numpy'],
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+)
