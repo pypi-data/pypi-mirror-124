1f6e64cb82cfb638c0101c3afd149d71dda8c4e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:20:27 +0000

    Add package framefile/0.0.8/framefile-0.0.8-py3-none-any.whl

---
+++ packages/framefile/framefile-0.0.8-py3-none-any.whl/framefile/__init__.py
@@ -0,0 +1,9 @@
+# SPDX-FileCopyrightText: (c) 2021 Artёm IG <github.com/rtmigo>
+# SPDX-License-Identifier: MIT
+
+
+from ._base import hash_extract_number, pct_extract_number, \
+    PatternMismatchError, PatternNotFoundError, hash_pattern_to_glob, pct_pattern_to_glob, \
+    hash_pattern_to_regex, NumbersCountError, filename_to_pct_pattern, \
+    filename_to_hash_pattern, pct_to_hash_pattern, pct_pattern_to_regex, \
+    is_pattern, Format
