47437150f0f006871560451dc1e8f2d737c97dc8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:20:52 +0000

    Add package sumandpultiplyer/1.0.1/sumandpultiplyer-1.0.1.tar.gz

---
+++ packages/sumandpultiplyer/sumandpultiplyer-1.0.1.tar.gz/sumandpultiplyer-1.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='sumandpultiplyer',
+    version='1.0.1',
+    description='A package for summarizing array and multiplying the result on a number',
+    url="https://github.com/brenscrazy/sumandmulitlyer",
+    author='Vinnikov Gleb',
+    author_email='glebmanufa@gmail.com',
+    install_requires=['numpy==1.19'
+                      ]
+    )
