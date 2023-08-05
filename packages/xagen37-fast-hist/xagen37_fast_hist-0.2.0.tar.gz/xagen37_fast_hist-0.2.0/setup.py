289114a46df99cad5b61a6fc6b3277178921a5c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:11:42 +0000

    Add package xagen37-fast-hist/0.2.0/xagen37_fast_hist-0.2.0.tar.gz

---
+++ packages/xagen37-fast-hist/xagen37_fast_hist-0.2.0.tar.gz/xagen37_fast_hist-0.2.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name = 'xagen37_fast_hist',
+    version = '0.2.0',
+    description = 'Dummy module for doing lab.',
+    author = 'Alexander Sofrygin',
+    author_email = 'xagen37@gmail.com',
+    install_requires = ['numpy>=1.20']
+)
