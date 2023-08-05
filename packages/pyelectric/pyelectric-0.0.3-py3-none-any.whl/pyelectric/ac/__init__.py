f48aab72f7360b4da89922db6838d24c6c024926
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:11:42 +0000

    Add package pyelectric/0.0.3/pyelectric-0.0.3-py3-none-any.whl

---
+++ packages/pyelectric/pyelectric-0.0.3-py3-none-any.whl/pyelectric/ac/__init__.py
@@ -0,0 +1,7 @@
+import math
+
+
+def cutoff_frequencies(R: float, L: float, C: float) -> Tuple[float, float]:
+    f1 = (1/(2*math.pi))*(-(R/(2*L)) + (1/2)*math.sqrt((R/L)**2 + (4/L*C)))
+    f2 = (1/(2*math.pi))*((R/(2*L)) + (1/2)*math.sqrt((R/L)**2 + (4/L*C)))
+    return f1, f2
