47da3103b1f57f43975f042cce984c819eaad3cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:32 +0000

    Add package xellusmodule/0.1.2/xellusmodule-0.1.2.tar.gz

---
+++ packages/xellusmodule/xellusmodule-0.1.2.tar.gz/xellusmodule-0.1.2/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='xellusmodule',
+    version='0.1.2',    
+    description='A example Python package',
+    author='Alexey Vasilyev',
+    url='https://github.com/xe11us/pyexample',
+    author_email='vasilyev-av22@mail.ru',
+    packages=['xellusmodule'],
+    install_requires=[
+                        'numpy'     
+                      ],
+)
