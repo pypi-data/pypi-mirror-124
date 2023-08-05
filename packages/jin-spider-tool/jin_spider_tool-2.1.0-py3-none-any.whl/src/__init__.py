ab26a80892822dea95756a038ca309475f45ec63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:26 +0000

    Add package jin-spider-tool/2.1.0/jin_spider_tool-2.1.0-py3-none-any.whl

---
+++ packages/jin-spider-tool/jin_spider_tool-2.1.0-py3-none-any.whl/src/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding:utf-8 -*-
+# !/usr/bin python3                                 
+# encoding    :   -*- utf-8 -*-                            
+# @author     :    None                             
+# @software   :   PyCharm      
+# @file       :   __init__.py.py
+# @Time       :   2021/10/18 3:34 下午
