89b86f074f2db88be39f16db02aafb21ab95cbce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:11:51 +0000

    Add package xchainpy-bitcoin/0.2.3/xchainpy_bitcoin-0.2.3.tar.gz

---
+++ packages/xchainpy-bitcoin/xchainpy_bitcoin-0.2.3.tar.gz/xchainpy_bitcoin-0.2.3/xchainpy_bitcoin/client.py
@@ -0,0 +1,175 @@
+from xchainpy_client.models.tx_types import TX, TxHistoryParams, TxPage
+from xchainpy_client.utxo_client import UTXOClient
+from xchainpy_util.chain import Chain
+from . models.client_types import BitcoinTxParams, BitcoinClientParams
+from . import crypto, sochain_api
+from . import utils
+
+
+class Client(UTXOClient):
+    sochain_url = blockstream_url = ''
+
+    def __init__(self, params:BitcoinClientParams):
+        """
+        :param params: params
+        :type params: BitcoinClientParams
+        """
+        UTXOClient.__init__(self, Chain.Bitcoin, params)
+
+        self.set_sochain_url(params.sochain_url)
+        self.set_blockstream_url(params.blockstream_url)
+
+    def set_sochain_url(self, sochain_url:str):
+        """Set/Update the sochain url
+
+        :param sochain_url: The new sochain url
+        :type sochain_url: str
+        """
+        self.sochain_url = sochain_url
+
+    def set_blockstream_url(self, blockstream_url:str):
+        """Set/Update the blockstream url
+
+        :param blockstream_url: The new blockstream url
+        :type blockstream_url: str
+        """
+        self.blockstream_url = blockstream_url
+
+    def get_explorer_url(self) -> str:
+        """Get explorer url
+        :returns: the explorer url based on the network
+        """
+        return 'https://blockstream.info/testnet' if self.network == 'testnet' else 'https://blockstream.info'
+
+    def get_explorer_address_url(self, address:str) -> str:
+        """Get the explorer url for the given address
+
+        :param address: address
+        :type address: str
+        :returns: The explorer url for the given address based on the network
+        """
+        return f'{self.get_explorer_url()}/address/{address}'
+
+    def get_explorer_tx_url(self, tx_id:str) -> str:
+        """Get the explorer url for the given transaction id
+
+        :param tx_id: tx_id
+        :type tx_id: str
+        :returns: The explorer url for the given transaction id based on the network
+        """
+        return f'{self.get_explorer_url()}/tx/{tx_id}'
+
+    def get_address(self, index:int=0):
+        """Get the current address
+
+        :returns: the current address
+        :raises: Raises if phrase has not been set before. A phrase is needed to create a wallet and to derive an address from it.
+        """
+        if index < 0:
+            raise Exception('index must be greater than zero')
+        
+        if self.phrase:
+            derivation_path = self.root_derivation_paths.testnet if self.get_network() == 'testnet' else self.root_derivation_paths.mainnet
+            address = crypto.mnemonic_to_address(mnemonic=self.phrase, derivation_path=derivation_path + str(index), network=self.get_network())
+
+            if not address:
+                raise Exception('Address not defined')
+
+            return address
+        raise Exception('Phrase must be provided')
+
+    def validate_address(self, network, address):
+        """Validate the given address
+
+        :param network: testnet or mainnet
+        :type network: str
+        :param address: address
+        :type address: str
+        :returns: True or False
+        """
+        return utils.validate_address(network, address)
+
+    async def get_balance(self, address):
+        """Get the BTC balance of a given address
+
+        :param address: BTC address
+        :type address: str
+        :returns: The BTC balance of the address
+        """
+        balance = await utils.get_balance(self.sochain_url, address, self.get_network())
+        return balance
+
+    async def get_transactions(self, params:TxHistoryParams) -> TxPage:
+        """Get transaction history of a given address with pagination options
+        By default it will return the transaction history of the current wallet
+
+        :param params: params
+        :type params: TxHistoryParams
+        :returns: The transaction history
+        """
+        try:
+            transactions = await sochain_api.get_transactions(self.sochain_url, self.get_network(), params.address)
+            total = transactions['total_txs']
+            offset = params.offset or 0
+            limit = params.limit or 10
+            transactions['txs'] = transactions['txs'][offset:offset+limit]
+            txs = []
+            for tx in transactions['txs']:
+                tx = await sochain_api.get_tx(self.sochain_url, self.get_network(), tx['txid'])
+                tx = utils.parse_tx(tx)
+                txs.append(tx)
+ 
+            return TxPage(total=total, txs=txs)
+
+        except Exception as err:
+            raise Exception(str(err))
+
+    async def get_transaction_data(self, tx_id:str) -> TX:
+        """Get the transaction details of a given transaction id
+
+        if you want to give a hash that is for mainnet and the current self.network is 'testnet',
+        you should call self.set_network('mainnet') (and vice versa) and then call this method.
+
+        :param tx_id: The transaction id
+        :type tx_id: str
+        :returns: The transaction details of the given transaction id
+        """
+        try:
+            tx = await sochain_api.get_tx(self.sochain_url, self.get_network(), tx_id)
+            tx = utils.parse_tx(tx)
+            return tx
+        except Exception as err:
+            raise Exception(str(err))
+
+    async def _get_suggested_fee_rate(self):
+        fee_rate = await sochain_api.get_suggested_tx_fee()
+        return fee_rate
+
+    def _calc_fee(self, fee_rate, memo):
+        fee = utils.calc_fee(fee_rate, memo)
+        return fee
+
+    async def transfer(self, params:BitcoinTxParams):
+        """Transfer BTC
+
+        :param params: transfer params
+        :type params: BitcoinTxParams
+        :returns: the transaction hash
+        """
+        if not params.fee_rate:
+            fee_rates = await self.get_fee_rates()
+            params.fee_rate = fee_rates.fast
+
+        spend_pending_UTXO = False if params.memo else True
+
+        t, utxos = await utils.build_tx(sochain_url=self.sochain_url, amount=round(params.amount*10**8), recipient=params.recipient,
+                                        memo=params.memo, fee_rate=params.fee_rate, sender=self.get_address(), network=self.get_network(), 
+                                        spend_pending_UTXO=spend_pending_UTXO)
+
+        derivation_path = self.root_derivation_paths.testnet if self.get_network() == 'testnet' else self.root_derivation_paths.mainnet
+        derivation_path += str(params.wallet_index)
+        private_key = crypto.mnemonic_to_private_key(self.phrase, derivation_path, self.get_network())
+        private_bytes = private_key.Raw().ToBytes()
+        t.sign(private_bytes)
+
+        return await utils.broadcast_tx(self.blockstream_url, self.get_network(), t.raw_hex())
---
+++ packages/xchainpy-bitcoin/xchainpy_bitcoin-0.2.3.tar.gz/xchainpy_bitcoin-0.2.3/xchainpy_bitcoin/utils.py
@@ -0,0 +1,310 @@
+import binascii
+from bitcoinlib.keys import Address
+from typing import List, Optional, Union
+from .const import MIN_TX_FEE
+from .models.common import DerivePath, UTXO
+from . import sochain_api, haskoin_api, blockstream_api
+from bitcoinlib.services.services import *
+from xchainpy_util.asset import AssetBTC
+from xchainpy_client.fees import standard_fees, calc_fees
+from xchainpy_client.models import tx_types
+from xchainpy_client.models.types import FeesWithRates
+from xchainpy_client.models.types import FeesWithRates
+import datetime
+
+
+TX_EMPTY_SIZE = 4 + 1 + 1 + 4  # 10
+TX_INPUT_BASE = 32 + 4 + 1 + 4  # 41
+TX_INPUT_PUBKEYHASH = 107
+TX_OUTPUT_BASE = 8 + 1  # 9
+TX_OUTPUT_PUBKEYHASH = 25
+DUST_THRESHOLD = 1000
+
+
+def get_default_fees_with_rates() -> FeesWithRates:
+    """Get the default fees with rates
+
+    :returns: The default fees and rates (FeesWithRates)
+    """
+    rates = standard_fees(20)
+    rates.fastest = 50
+    fees = calc_fees(rates, calc_fee)
+    return FeesWithRates(fees, rates)
+
+def get_default_fees() -> FeesWithRates:
+    """Get the default fees
+
+    :returns: The default fees (Fees)
+    """
+    fees = get_default_fees_with_rates().fees
+    return fees
+
+
+def parse_tx(tx) -> tx_types.TX:
+    """Parse tx
+
+    :param tx: The transaction to be parsed
+    :type tx: str
+    :returns: The transaction parsed from the binance tx
+    """
+    asset = AssetBTC
+    tx_from = [tx_types.TxFrom(i['address'], i['value']) for i in tx['inputs']]
+    tx_to = [tx_types.TxTo(i['address'], i['value']) for i in tx['outputs']]
+    tx_date = datetime.datetime.fromtimestamp(tx['time'])
+    tx_type = 'transfer'
+    tx_hash = tx['txid']
+
+    tx = tx_types.TX(asset, tx_from, tx_to, tx_date, tx_type, tx_hash)
+    return tx
+
+
+def calc_fee(fee_rate, memo=''):
+    """Calculate fees based on fee rate and memo
+
+    :param fee_rate: fee rate
+    :type fee_rate: int
+    :param memo: memo
+    :type memo: str
+    :returns: The calculated fees based on fee rate and the memo
+    """
+    compiled_memo = compile_memo(memo) if memo else None
+    fee = get_fee([], fee_rate, compiled_memo)
+    return fee
+
+
+def compile_memo(memo: str):
+    """Compile memo
+
+    :param memo: The memo to be compiled
+    :type memo: str
+    :returns: The compiled memo
+    """
+    metadata = bytes(memo, 'utf-8')
+    metadata_len = len(metadata)
+
+    if metadata_len <= 75:
+        # length byte + data (https://en.bitcoin.it/wiki/Script)
+        payload = bytearray((metadata_len,))+metadata
+    elif metadata_len <= 256:
+        # OP_PUSHDATA1 format
+        payload = "\x4c"+bytearray((metadata_len,))+metadata
+    else:
+        payload = "\x4d"+bytearray((metadata_len % 256,))+bytearray(
+            (int(metadata_len/256),))+metadata  # OP_PUSHDATA2 format
+    
+
+    compiled_memo = binascii.b2a_hex(payload).decode('utf-8')
+    compiled_memo = '6a' + compiled_memo
+    compiled_memo = binascii.unhexlify(compiled_memo)
+    return compiled_memo
+
+
+def validate_address(network, address):
+    """Validate the BTC address
+
+    :param network: testnet or mainnet
+    :type network: str
+    :param address: address
+    :type address: str
+    :returns: True or False
+    """
+    try:
+        address = Address.import_address(address=address, network=(
+            'bitcoin' if network == 'mainnet' else network))
+        return True
+    except:
+        return False
+
+
+def get_fee(inputs: List[UTXO], fee_rate: float, data: Optional[bytes]=None):
+    """Get the transaction fee
+
+    :param inputs: the UTXOs
+    :type inputs: List[UTXO]
+    :param fee_rate: the fee rate
+    :type fee_rate: float
+    :param data: The compiled memo (Optional)
+    :type data: bytes
+    :returns: The fee amount
+    """
+    lst_reduce = 0
+    if len(inputs) > 0:
+        for x in inputs:
+            lst_reduce += TX_INPUT_BASE + \
+                (len(x.witness_utxo.script)
+                 if x.witness_utxo.script else TX_INPUT_PUBKEYHASH)
+
+    sum = TX_EMPTY_SIZE + lst_reduce + \
+        len(inputs) + TX_OUTPUT_BASE + TX_OUTPUT_PUBKEYHASH + \
+        TX_OUTPUT_BASE + TX_OUTPUT_PUBKEYHASH
+    if data:
+        sum = sum + TX_OUTPUT_BASE + len(data)
+    fee = sum * fee_rate
+    result = fee if fee > MIN_TX_FEE else MIN_TX_FEE
+    return result
+
+
+async def scan_UTXOs(sochain_url, network, address, confirmed_only=True):
+    """Scan UTXOs from sochain
+
+    :param sochain_url: sochain url
+    :type sochain_url: str
+    :param network: testnet or mainnet
+    :type network: str
+    :param address: address
+    :type address: str
+    :param confirmed_only: By default is True, scan only confirmed UTXOs
+    :type confirmed_only: bool
+    :returns: The UTXOs of the given address
+    """
+    if network == 'testnet':
+        if confirmed_only:
+            utxos = await sochain_api.get_confirmed_unspent_txs(sochain_url, network, address)
+        else:
+            utxos = await sochain_api.get_unspent_txs(sochain_url, network, address)
+
+        utxos = list(map(sochain_api.sochain_utxo_to_xchain_utxo, utxos))
+    else:
+        if confirmed_only:
+            utxos = await haskoin_api.get_confirmed_unspent_txs(address)
+        else:
+            utxos = await haskoin_api.get_unspent_txs(address)
+
+        utxos = list(map(haskoin_api.haskoin_utxo_to_xchain_utxo, utxos))
+
+    return utxos
+
+
+
+    # utxos = await sochain_api.get_unspent_txs(sochain_url, network, address)
+    # utxos = list(map(UTXO.from_sochain_utxo, utxos))
+    # return utxos
+
+
+async def get_change(sochain_url:str, value_out, network:str, address:str):
+    """Get the balance changes amount
+
+    :param sochain_url: sochain url
+    :type sochain_url: str
+    :param value_out: amount you wnat to transfer
+    :type value_out: int
+    :param network: testnet or mainnet
+    :type network: str
+    :param address: address
+    :type address: str
+    :returns: The UTXOs of the given address
+    """
+    try:
+        balance = await sochain_api.get_balance(sochain_url, network, address)
+        balance = round(balance[0].amount * 10 ** 8)
+        change = 0
+        if balance - value_out > DUST_THRESHOLD:
+            change = balance - value_out
+        return change
+    except Exception as err:
+        raise Exception(str(err))
+
+
+async def build_tx(sochain_url, amount, recipient, memo, fee_rate, sender, network, spend_pending_UTXO=False):
+    """Build transcation
+
+    :param sochain_url: sochain url
+    :type sochain_url: str
+    :param amount: amount of BTC to transfer
+    :type amount: int
+    :param recipient: destination address
+    :type recipient: str
+    :param memo: optional memo for transaction
+    :type memo: str
+    :param fee_rate: fee rates for transaction
+    :type fee_rate: int
+    :param sender: sender's address
+    :type sender: str
+    :param network: testnet or mainnet
+    :type network: str
+    :param spend_pending_UTXO: By default is False, prevent spending uncomfirmed UTXOs
+    :type spend_pending_UTXO: bool
+    :returns: transaction
+    """
+    try:
+        # search only confirmed UTXOs if pending UTXO is not allowed
+        confirmed_only = not spend_pending_UTXO
+
+        utxos = await scan_UTXOs(sochain_url, network, sender, confirmed_only)
+        if len(utxos) == 0:
+            raise Exception("No utxos to send")
+
+        balance = await sochain_api.get_balance(sochain_url, network, sender)
+        
+        if not validate_address(network, recipient):
+            raise Exception('Invalid address')
+        
+        fee_rate_whole = int(fee_rate)
+        
+        compiled_memo = None
+        if memo:
+            compiled_memo = compile_memo(memo)
+
+        fee = get_fee(utxos, fee_rate_whole, compiled_memo)
+
+        if fee + amount > round(balance[0].amount * 10 ** 8):
+            raise Exception('Balance insufficient for transaction')
+
+        t = Transaction(network=network, witness_type='segwit')
+
+        for i in utxos:
+            t.add_input(prev_txid=i.hash, output_n=i.index,
+                    value=i.witness_utxo.value, witnesses=i.witness_utxo.script)
+
+        t.add_output(address=recipient, value=amount)
+        change = await get_change(sochain_url, amount + fee, network, sender)
+        
+        if change > 0:
+            
+            t.add_output(address=sender, value=change)
+
+        if compiled_memo:
+            t.add_output(lock_script=compiled_memo, value=0)
+
+        return t, utxos
+
+    except Exception as err:
+        raise Exception(str(err))
+
+
+async def broadcast_tx(blockstream_url, network, tx_hex):
+    """Broadcast the transaction
+
+    :param sochain_url: sochain url
+    :type sochain_url: str
+    :param network: testnet or mainnet
+    :type network: str
+    :param tx_hex: tranaction hex
+    :type tx_hex: str
+    :returns: The transaction hash
+    """
+    return await blockstream_api.broadcast_tx(blockstream_url, network, tx_hex)
+
+
+async def get_balance(sochain_url:str, address:str, network:str):
+    """Get the BTC balance of a given address
+
+    :param sochain_url: sochain url
+    :type sochain_url: str
+    :param address: BTC address
+    :type address: str
+    :param network: mainnet or testnet
+    :type network: str
+    :returns: The BTC balance of the address
+    """
+    try:
+        if network == 'testnet':
+            balance = await sochain_api.get_balance(sochain_url, network, address)
+        else:
+            balance = await haskoin_api.get_balance(address)
+        
+        if balance == None:
+            raise Exception("Invalid Address")
+        return balance
+    except Exception as err:
+        raise Exception(str(err))
