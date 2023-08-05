438f8fd65d6b69d14914efb3adee6b7c6fd7d85f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:12:09 +0000

    Add package MLDashboard/1.0.8/MLDashboard-1.0.8.tar.gz

---
+++ packages/MLDashboard/MLDashboard-1.0.8.tar.gz/MLDashboard-1.0.8/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+    name='MLDashboard',
+    url='https://github.com/RobertJN64/MLDashboard',
+    author='Robert Nies',
+    author_email='robertjnies@gamil.com',
+    # Needed to actually package something
+    packages=['MLDashboard'],
+    install_requires=[
+        'matplotlib',
+        'tensorflow',
+        'numpy',
+        'pillow'
+    ],
+    # *strongly* suggested for sharing
+    version='1.0.8',
+    # The license can be anything you like
+    license='MIT',
+    description='Machine learning dashboard that integrates with tensorflow. Great for monitoring training. Has tools for classification and images.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown'
+)
