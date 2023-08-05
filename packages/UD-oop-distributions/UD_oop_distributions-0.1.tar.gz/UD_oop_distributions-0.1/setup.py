ed168083e932057fdfc1d2484a795b421690459c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:15:14 +0000

    Add package UD-oop-distributions/0.1/UD_oop_distributions-0.1.tar.gz

---
+++ packages/UD-oop-distributions/UD_oop_distributions-0.1.tar.gz/UD_oop_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='UD_oop_distributions',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['UD_oop_distributions'],
+      author = 'Riccardo Galli',
+      author_email = 'riccardomgalli@gmail.com',
+      zip_safe=False)
