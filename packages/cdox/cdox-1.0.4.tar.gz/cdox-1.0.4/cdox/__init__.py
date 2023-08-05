e9efc555a6686c762a4bc39e320193f59239a91e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:18 +0000

    Add package cdox/1.0.4/cdox-1.0.4.tar.gz

---
+++ packages/cdox/cdox-1.0.4.tar.gz/cdox-1.0.4/cdox/__init__.py
@@ -0,0 +1,9 @@
+'''
+cdox
+documentation generator for generating API documentation in markdown format from source code
+'''
+#from cdox import Doc
+#from __main__ import main
+
+__version__ = '1.0.4'
+__author__ = 'Taylor Gamache'
