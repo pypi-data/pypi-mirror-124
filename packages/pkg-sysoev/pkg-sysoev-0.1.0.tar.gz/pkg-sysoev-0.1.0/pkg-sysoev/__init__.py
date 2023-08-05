dbc3dd833c75afab976c4b0fcbb15bf3a6bfa744
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:11:40 +0000

    Add package pkg-sysoev/0.1.0/pkg-sysoev-0.1.0.tar.gz

---
+++ packages/pkg-sysoev/pkg-sysoev-0.1.0.tar.gz/pkg-sysoev-0.1.0/pkg-sysoev/__init__.py
@@ -0,0 +1,8 @@
+"""
+pkg-sysoev.
+
+An example python library.
+"""
+
+__version__ = "0.1.0"
+__author__ = 'Alexander Sysoev'
---
+++ packages/pkg-sysoev/pkg-sysoev-0.1.0.tar.gz/pkg-sysoev-0.1.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='pkg-sysoev',
+    version='0.1.0',
+    description='A example Python package',
+    author='Sysoev Alexander',
+    url='https://github.com/Mr3zee',
+    author_email='shudson@anl.gov',
+    license='MIT',
+    packages=['pkg-sysoev'],
+    install_requires=['numpy'],
+
+    classifiers=[
+        'Programming Language :: Python :: 3.9',
+    ],
+)
