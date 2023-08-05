2cffaf8d137a13fd1944b6b449631ff5f3daaa50
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:13:43 +0000

    Add package pygerber/1.1.0/pygerber-1.1.0-py3-none-any.whl

---
+++ packages/pygerber/pygerber-1.1.0-py3-none-any.whl/pygerber/parser/blender/apertures/obround.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+from __future__ import annotations
+
+from pygerber.parser.blender.apertures.rectangle import BlenderRectangle
+
+# from pygerber.renderer.spec import ArcSpec, FlashSpec, LineSpec
+
+
+class BlenderObround(BlenderRectangle):
+    pass
