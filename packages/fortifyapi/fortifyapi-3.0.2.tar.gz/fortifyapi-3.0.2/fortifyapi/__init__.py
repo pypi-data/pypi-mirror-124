388a497990dde673c2e7c138bc27a7fd7f454d93
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:11:13 +0000

    Add package fortifyapi/3.0.2/fortifyapi-3.0.2.tar.gz

---
+++ packages/fortifyapi/fortifyapi-3.0.2.tar.gz/fortifyapi-3.0.2/fortifyapi/__init__.py
@@ -0,0 +1,7 @@
+__version__ = '3.0.2'
+
+from fortifyapi.client import *
+from fortifyapi.api import *
+from fortifyapi.query import Query
+
+__all__ = ['FortifySSCAPI', 'FortifySSCClient', 'Query', 'fortify', '__version__']
