720ffb13afa6c5a0c10105171c5972e757006dfa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:11:47 +0000

    Add package pysiaalarm/3.0.3b1/pysiaalarm-3.0.3b1.tar.gz

---
+++ packages/pysiaalarm/pysiaalarm-3.0.3b1.tar.gz/pysiaalarm-3.0.3b1/src/pysiaalarm/aio/__init__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""Init for aio."""
+from .. import (
+    CommunicationsProtocol,
+    InvalidAccountFormatError,
+    InvalidAccountLengthError,
+    InvalidKeyFormatError,
+    InvalidKeyLengthError,
+    SIAAccount,
+    SIAEvent,
+    __author__,
+    __copyright__,
+    __license__,
+    __version__,
+)
+from .client import SIAClient
