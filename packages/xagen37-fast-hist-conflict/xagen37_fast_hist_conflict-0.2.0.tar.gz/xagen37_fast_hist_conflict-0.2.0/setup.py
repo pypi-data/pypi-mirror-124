78b048769ad179e365de2beb80faa577ccb92aac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:12:00 +0000

    Add package xagen37-fast-hist-conflict/0.2.0/xagen37_fast_hist_conflict-0.2.0.tar.gz

---
+++ packages/xagen37-fast-hist-conflict/xagen37_fast_hist_conflict-0.2.0.tar.gz/xagen37_fast_hist_conflict-0.2.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name = 'xagen37_fast_hist_conflict',
+    version = '0.2.0',
+    description = 'Dummy module for doing lab.',
+    author = 'Alexander Sofrygin',
+    author_email = 'xagen37@gmail.com',
+    install_requires = ['numpy<=1.19']
+)
