67c78586a8ba9e81bd06c784a5e291551c12250b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:54 +0000

    Add package motorized/0.2.4/motorized-0.2.4-py3-none-any.whl

---
+++ packages/motorized/motorized-0.2.4-py3-none-any.whl/motorized/types.py
@@ -0,0 +1,18 @@
+from bson import ObjectId
+from pydantic.main import BaseModel
+
+
+class PydanticObjectId(ObjectId):
+    @classmethod
+    def __get_validators__(cls):
+        yield cls.validate
+
+    @classmethod
+    def validate(cls, v):
+        if not isinstance(v, ObjectId):
+            raise TypeError('ObjectId required')
+        return v
+
+    @classmethod
+    def __modify_schema__(cls, schema: dict):
+        schema['type'] = 'string'
