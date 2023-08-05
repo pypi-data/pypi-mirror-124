ce3d5cf35695b59c5e2b34492f0e9be52f485727
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:24 +0000

    Add package openmetadata-ingestion/0.4.0.dev0/openmetadata-ingestion-0.4.0.dev0.tar.gz

---
+++ packages/openmetadata-ingestion/openmetadata-ingestion-0.4.0.dev0.tar.gz/openmetadata-ingestion-0.4.0.dev0/src/metadata/ingestion/source/redshift_usage.py
@@ -0,0 +1,125 @@
+#  Licensed to the Apache Software Foundation (ASF) under one or more
+#  contributor license agreements. See the NOTICE file distributed with
+#  this work for additional information regarding copyright ownership.
+#  The ASF licenses this file to You under the Apache License, Version 2.0
+#  (the "License"); you may not use this file except in compliance with
+#  the License. You may obtain a copy of the License at
+#
+#  http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+
+# This import verifies that the dependencies are available.
+import logging
+from typing import Any, Dict, Iterable, Iterator, Union
+
+from metadata.ingestion.api.source import Source, SourceStatus
+from metadata.ingestion.models.table_queries import TableQuery
+from metadata.ingestion.ometa.openmetadata_rest import MetadataServerConfig
+from metadata.ingestion.source.redshift import RedshiftConfig
+from metadata.ingestion.source.sql_alchemy_helper import (
+    SQLAlchemyHelper,
+    SQLSourceStatus,
+)
+from metadata.utils.helpers import get_start_and_end
+
+logger = logging.getLogger(__name__)
+
+
+class RedshiftUsageSource(Source):
+    # SELECT statement from mysql information_schema to extract table and column metadata
+    SQL_STATEMENT = """
+        WITH query_sql AS (
+                 SELECT
+                    query,
+                    LISTAGG(text) WITHIN GROUP (ORDER BY sequence) AS sql
+                FROM stl_querytext 
+                GROUP BY 1
+        )
+
+        SELECT
+            q.query,  q.label, userid,  xid,  pid,  starttime,  endtime,
+            DATEDIFF(milliseconds, starttime, endtime) AS duration,
+            TRIM(database) AS database,
+            '{start_date}' as analysis_date,
+            (CASE aborted WHEN 1 THEN TRUE ELSE FALSE END) AS aborted,
+        sql
+        FROM
+            stl_query q JOIN query_sql qs ON (q.query = qs.query)
+        WHERE
+        endtime between '{start_date}' and '{end_date}'
+        {where_clause}
+        ORDER BY starttime;
+        """
+
+    # CONFIG KEYS
+    WHERE_CLAUSE_SUFFIX_KEY = "where_clause"
+    CLUSTER_SOURCE = "cluster_source"
+    CLUSTER_KEY = "cluster_key"
+    USE_CATALOG_AS_CLUSTER_NAME = "use_catalog_as_cluster_name"
+    DATABASE_KEY = "database_key"
+    SERVICE_TYPE = "Redshift"
+    DEFAULT_CLUSTER_SOURCE = "CURRENT_DATABASE()"
+
+    def __init__(self, config, metadata_config, ctx):
+        super().__init__(ctx)
+        start, end = get_start_and_end(config.duration)
+        self.sql_stmt = RedshiftUsageSource.SQL_STATEMENT.format(
+            where_clause=config.where_clause, start_date=start, end_date=end
+        )
+        self.alchemy_helper = SQLAlchemyHelper(
+            config, metadata_config, ctx, "Redshift", self.sql_stmt
+        )
+        self._extract_iter: Union[None, Iterator] = None
+        self._database = "redshift"
+        self.status = SQLSourceStatus()
+
+    @classmethod
+    def create(cls, config_dict, metadata_config_dict, ctx):
+        config = RedshiftConfig.parse_obj(config_dict)
+        metadata_config = MetadataServerConfig.parse_obj(metadata_config_dict)
+        return cls(config, metadata_config, ctx)
+
+    def prepare(self):
+        pass
+
+    def _get_raw_extract_iter(self) -> Iterable[Dict[str, Any]]:
+        """
+        Provides iterator of result row from SQLAlchemy helper
+        :return:
+        """
+        rows = self.alchemy_helper.execute_query()
+        for row in rows:
+            yield row
+
+    def next_record(self) -> Iterable[TableQuery]:
+        """
+        Using itertools.groupby and raw level iterator, it groups to table and yields TableMetadata
+        :return:
+        """
+        for row in self._get_raw_extract_iter():
+            tq = TableQuery(
+                row["query"],
+                row["label"],
+                row["userid"],
+                row["xid"],
+                row["pid"],
+                str(row["starttime"]),
+                str(row["endtime"]),
+                str(row["analysis_date"]),
+                row["duration"],
+                row["database"],
+                row["aborted"],
+                row["sql"],
+            )
+            yield tq
+
+    def close(self):
+        self.alchemy_helper.close()
+
+    def get_status(self) -> SourceStatus:
+        return self.status
---
+++ packages/openmetadata-ingestion/openmetadata-ingestion-0.4.0.dev0.tar.gz/openmetadata-ingestion-0.4.0.dev0/src/metadata/utils/helpers.py
@@ -0,0 +1,125 @@
+#  Licensed to the Apache Software Foundation (ASF) under one or more
+#  contributor license agreements. See the NOTICE file distributed with
+#  this work for additional information regarding copyright ownership.
+#  The ASF licenses this file to You under the Apache License, Version 2.0
+#  (the "License"); you may not use this file except in compliance with
+#  the License. You may obtain a copy of the License at
+#
+#  http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+from datetime import datetime, timedelta
+from typing import List
+
+from metadata.generated.schema.api.services.createDashboardService import (
+    CreateDashboardServiceEntityRequest,
+)
+from metadata.generated.schema.api.services.createDatabaseService import (
+    CreateDatabaseServiceEntityRequest,
+)
+from metadata.generated.schema.api.services.createMessagingService import (
+    CreateMessagingServiceEntityRequest,
+)
+from metadata.generated.schema.entity.services.dashboardService import DashboardService
+from metadata.generated.schema.entity.services.databaseService import DatabaseService
+from metadata.generated.schema.entity.services.messagingService import MessagingService
+from metadata.ingestion.ometa.openmetadata_rest import OpenMetadataAPIClient
+
+
+def get_start_and_end(duration):
+    today = datetime.utcnow()
+    start = (today + timedelta(0 - duration)).replace(
+        hour=0, minute=0, second=0, microsecond=0
+    )
+    end = (today + timedelta(3)).replace(hour=0, minute=0, second=0, microsecond=0)
+    return start, end
+
+
+def snake_to_camel(s):
+    a = s.split("_")
+    a[0] = a[0].capitalize()
+    if len(a) > 1:
+        a[1:] = [u.title() for u in a[1:]]
+    return "".join(a)
+
+
+def get_database_service_or_create(config, metadata_config) -> DatabaseService:
+    client = OpenMetadataAPIClient(metadata_config)
+    service = client.get_database_service(config.service_name)
+    if service is not None:
+        return service
+    else:
+        service = {
+            "jdbc": {
+                "connectionUrl": f"jdbc://{config.host_port}",
+                "driverClass": "jdbc",
+            },
+            "name": config.service_name,
+            "description": "",
+            "serviceType": config.get_service_type(),
+        }
+        print(service)
+        created_service = client.create_database_service(
+            CreateDatabaseServiceEntityRequest(**service)
+        )
+        return created_service
+
+
+def get_messaging_service_or_create(
+    service_name: str,
+    message_service_type: str,
+    schema_registry_url: str,
+    brokers: List[str],
+    metadata_config,
+) -> MessagingService:
+    client = OpenMetadataAPIClient(metadata_config)
+    service = client.get_messaging_service(service_name)
+    if service is not None:
+        return service
+    else:
+        create_messaging_service_request = CreateMessagingServiceEntityRequest(
+            name=service_name,
+            serviceType=message_service_type,
+            brokers=brokers,
+            schemaRegistry=schema_registry_url,
+        )
+        created_service = client.create_messaging_service(
+            create_messaging_service_request
+        )
+        return created_service
+
+
+def get_dashboard_service_or_create(
+    service_name: str,
+    dashboard_service_type: str,
+    username: str,
+    password: str,
+    dashboard_url: str,
+    metadata_config,
+) -> DashboardService:
+    client = OpenMetadataAPIClient(metadata_config)
+    service = client.get_dashboard_service(service_name)
+    if service is not None:
+        return service
+    else:
+        create_dashboard_service_request = CreateDashboardServiceEntityRequest(
+            name=service_name,
+            serviceType=dashboard_service_type,
+            username=username,
+            password=password,
+            dashboardUrl=dashboard_url,
+        )
+        created_service = client.create_dashboard_service(
+            create_dashboard_service_request
+        )
+        return created_service
+
+
+def convert_epoch_to_iso(seconds_since_epoch):
+    dt = datetime.utcfromtimestamp(seconds_since_epoch)
+    iso_format = dt.isoformat() + "Z"
+    return iso_format
