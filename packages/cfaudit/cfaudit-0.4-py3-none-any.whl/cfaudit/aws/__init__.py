b7fe5fef8aa376c28fbe039c7ff92b5ae04a15b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:10:44 +0000

    Add package cfaudit/0.4/cfaudit-0.4-py3-none-any.whl

---
+++ packages/cfaudit/cfaudit-0.4-py3-none-any.whl/cfaudit/aws/__init__.py
@@ -0,0 +1,11 @@
+
+from cfaudit.aws import analysis
+from cfaudit.aws import attached
+from cfaudit.aws import authentication
+from cfaudit.aws import exceptions
+from cfaudit.aws import output
+from cfaudit.aws import whitelist
+
+
+if __name__ == "__main__":
+    pass
---
+++ packages/cfaudit/cfaudit-0.4-py3-none-any.whl/cfaudit/aws/authentication.py
@@ -0,0 +1,54 @@
+""" cfaudit - AWS - Authentication
+
+Using AWS best practices forcing the user to write Named Profiles instead of
+direct IAM credentials through CLI.
+"""
+
+import boto3
+import cfaudit
+
+
+def create_session(
+        region: str,
+        profile: str = None,
+        session_token: str = None) -> boto3.session.Session:
+    """
+    Generates a Boto3 session from named profile and region inputs
+    """
+    session = boto3.Session(
+        profile_name=profile,
+        region_name=region,
+        aws_session_token=session_token,
+    )
+    if not session.get_credentials():
+        raise cfaudit.aws.exceptions.cfauditAWSCreateSessionError(
+            "cfaudit could not authenticate to AWS. "
+            "Check if your credentials exist and work."
+        )
+    return session
+
+
+def get_session_info(session: boto3.session.Session) -> str:
+    """
+    Get ARN from the current session
+    """
+    return session.client('sts').get_caller_identity()['Arn']
+
+
+def get_boto_clients(session: boto3.session.Session) -> dict:
+    """
+    Receives the session, and return all cfaudit used Boto3 Clients
+    """
+    return {
+        'ec2': session.client('ec2'),
+        'rds': session.client('rds'),
+        'elb': session.client('elb'),
+        'elbv2': session.client('elbv2'),
+        'lambda': session.client('lambda'),
+        'elasticache': session.client('elasticache'),
+        'ecs': session.client('ecs'),
+    }
+
+
+if __name__ == "__main__":
+    pass
