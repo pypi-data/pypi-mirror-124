ad93f909359582704840f7f8004a210f5c7679fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:00 +0000

    Add package pyca-blinkstick/0.2/pyca-blinkstick-0.2.tar.gz

---
+++ packages/pyca-blinkstick/pyca-blinkstick-0.2.tar.gz/pyca-blinkstick-0.2/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+import os
+
+path = os.path.abspath(os.path.dirname(__file__))
+
+
+def read(filename):
+    with open(os.path.join(path, filename), encoding='utf-8') as f:
+        return f.read()
+
+
+setup(
+    name="pyca-blinkstick",
+    version="0.2",
+    description="Blinkstick recording light integration with opencast pyca capture agent",
+    author="Kristof Keppens",
+    author_email='kristof.keppens@ugent.be',
+    license="MIT",
+    url="https://github.com/mm-dict/pyca-blinkstick",
+    long_description=read('README.rst'),
+    long_description_content_type='text/x-rst',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "BlinkStick>=1.2.0",
+        "python-dateutil>=2.4.0",
+        "configobj>=5.0.0",
+        "pyusb>=1.2.1",
+        "requests>=2.26.0",
+        "sdnotify>=0.3.2",
+    ],
+    entry_points={
+        'console_scripts': [
+            'pyca_blinkstick = pyca_blinkstick.__main__:main'
+        ]
+    },
+    test_suite="tests",
+)
