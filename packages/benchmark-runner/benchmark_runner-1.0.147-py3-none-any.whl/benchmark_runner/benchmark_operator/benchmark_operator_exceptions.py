afe83e7deb460d7df1421ba6a0361f41c6080c86
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:20:28 +0000

    Add package benchmark-runner/1.0.147/benchmark_runner-1.0.147-py3-none-any.whl

---
+++ packages/benchmark-runner/benchmark_runner-1.0.147-py3-none-any.whl/benchmark_runner/benchmark_operator/benchmark_operator_exceptions.py
@@ -0,0 +1,7 @@
+
+class BenchmarkOperatorError(Exception):
+    """ Base class for all benchmark operator error classes.
+        All exceptions raised by the benchmark runner library should inherit from this class. """
+    pass
+
+
