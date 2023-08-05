76ec9ae81de14943756db230ccf192dd1f89c151
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:14:29 +0000

    Add package pyridy/0.4.9/pyridy-0.4.9.tar.gz

---
+++ packages/pyridy/pyridy-0.4.9.tar.gz/pyridy-0.4.9/README.md
@@ -0,0 +1,7 @@
+# PyRidy
+
+Python Support Library to import and process Ridy files
+
+# Installation
+
+Install using pip, i.e. "pip install pyridy"
---
+++ packages/pyridy/pyridy-0.4.9.tar.gz/pyridy-0.4.9/setup.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['pyridy', 'pyridy.osm', 'pyridy.osm.utils', 'pyridy.utils']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['geopy>=2.1.0,<3.0.0',
+ 'ipyleaflet>=0.14.0,<0.15.0',
+ 'matplotlib>=3.4.1,<4.0.0',
+ 'numpy>=1.20.2,<2.0.0',
+ 'overpy>=0.6,<0.7',
+ 'pandas>=1.2.4,<2.0.0',
+ 'pyproj>=3.0.1,<4.0.0',
+ 'pytest>=6.2.3,<7.0.0',
+ 'requests>=2.25.1,<3.0.0',
+ 'scipy>=1.6.3,<2.0.0',
+ 'tqdm>=4.60.0,<5.0.0']
+
+setup_kwargs = {
+    'name': 'pyridy',
+    'version': '0.4.9',
+    'description': 'Support library for measurements made with the Ridy Android App',
+    'long_description': '# PyRidy\n\nPython Support Library to import and process Ridy files\n\n# Installation\n\nInstall using pip, i.e. "pip install pyridy"\n',
+    'author': 'Philipp Simon Leibner',
+    'author_email': 'philipp.leibner@ifs.rwth-aachen.de',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7.1,<3.10',
+}
+
+
+setup(**setup_kwargs)
