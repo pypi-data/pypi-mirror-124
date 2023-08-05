d7c5703c77022d0112fb7e3924d5e915778c2c70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:15:25 +0000

    Add package ggmail/0.3.2/ggmail-0.3.2-py3-none-any.whl

---
+++ packages/ggmail/ggmail-0.3.2-py3-none-any.whl/ggmail/__init__.py
@@ -0,0 +1,7 @@
+from .account import Account  # noqa
+from .authentication import Login, OAuth2  # noqa
+from .flag import Flag  # noqa
+from .mailbox import Mailbox  # noqa
+from .message import Message  # noqa
+
+__version__ = "0.1.0"
