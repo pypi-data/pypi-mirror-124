490fc5bd8456141ac0f20ad13651ccbe4ccb9f80
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:34 +0000

    Add package odin-bot-entities/0.1.0/odin-bot-entities-0.1.0.tar.gz

---
+++ packages/odin-bot-entities/odin-bot-entities-0.1.0.tar.gz/odin-bot-entities-0.1.0/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['odin_bot_entities']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'odin-bot-entities',
+    'version': '0.1.0',
+    'description': '',
+    'long_description': None,
+    'author': 'adolfrodeno',
+    'author_email': 'amvillalobos@uc.cl',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
