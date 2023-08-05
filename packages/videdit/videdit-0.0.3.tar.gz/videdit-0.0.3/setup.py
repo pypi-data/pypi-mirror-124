d62c9bef57f6d720e0a38b45d19384cf3ea36c78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:11:35 +0000

    Add package videdit/0.0.3/videdit-0.0.3.tar.gz

---
+++ packages/videdit/videdit-0.0.3.tar.gz/videdit-0.0.3/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+with open('README.md') as readme:
+    long_description = readme.read()
+
+setuptools.setup(
+    name="videdit",
+    description="Set of commands used to edit video files",
+    version="0.0.3",
+    packages=setuptools.find_packages(),
+    entry_points={
+        "console_scripts": [
+            "videdit=videdit.__main__:main"
+        ]
+    },
+    long_description=long_description,
+    long_description_content_type='text/markdown'
+)
