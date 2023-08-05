895ededb7d8b470182040161380236cb1bec1c39
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:11:35 +0000

    Add package onyx-mclib/2.1.0/onyx-mclib-2.1.0.tar.gz

---
+++ packages/onyx-mclib/onyx-mclib-2.1.0.tar.gz/onyx-mclib-2.1.0/onyx/__init__.py
@@ -0,0 +1,12 @@
+from onyx.bossbar import Bossbar as bossbar
+from onyx.commands import Commands as commands
+from onyx.execute import Execute as execute
+from onyx.pack_manager import *
+from onyx.registries import *
+from onyx.scoreboard import Scoreboard as scoreboard
+from onyx.selector import Selector as selector
+from onyx.split_registries import *
+from onyx.text_component import TextComponent as text_component
+from onyx.util import *
+
+commands()
