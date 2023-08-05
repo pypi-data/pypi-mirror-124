24a9cf8878e67eb792e263f661b0bb164a552799
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:20:44 +0000

    Add package pygments-ket/0.1.post1/pygments-ket-0.1.post1.tar.gz

---
+++ packages/pygments-ket/pygments-ket-0.1.post1.tar.gz/pygments-ket-0.1.post1/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pygments-ket',
+    version='0.1.r1',
+    description='Pygments lexer for Ket.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    keywords='pygments ket lexer',
+    license='Apache-2.0',
+
+    author='Evandro Chagas Ribeiro da Rosa',
+    author_email='evandro.crr@posgrad.ufsc.br',
+
+    url='https://gitlab.com/quantum-ket/pygments-ket',
+
+    packages=find_packages(),
+    install_requires=['pygments >= 2.3.1', 'ket-lang'],
+
+    entry_points='''[pygments.lexers]
+                    ket=pygments_ket:KetLexer''',
+)
