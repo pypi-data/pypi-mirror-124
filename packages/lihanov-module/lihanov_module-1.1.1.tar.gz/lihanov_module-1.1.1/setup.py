4ec761a23c1ca193c6466675cf509618d7e40f7b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:59 +0000

    Add package lihanov-module/1.1.1/lihanov_module-1.1.1.tar.gz

---
+++ packages/lihanov-module/lihanov_module-1.1.1.tar.gz/lihanov_module-1.1.1/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+setup(
+    name='lihanov_module',
+    version='1.1.1',    
+    description='A example Python package',
+    author='Lihanov Maxim',
+    author_email='maxlih@mail.ru',
+    license='BSD 2-clause',
+    packages=['lihanov_module'],
+    install_requires=['matplotlib',
+                      'numpy',                     
+                      ],
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',  
+        'Operating System :: POSIX :: Linux',        
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+)
