314b3248bfb4c4f6cc83af8d2768570c32b4bf15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:17 +0000

    Add package oddrn-generator/0.1.10/oddrn_generator-0.1.10-py3-none-any.whl

---
+++ packages/oddrn-generator/oddrn_generator-0.1.10-py3-none-any.whl/oddrn_generator/__init__.py
@@ -0,0 +1,28 @@
+from oddrn_generator.generators import (
+    PostgresqlGenerator, MysqlGenerator, KafkaGenerator, KafkaConnectGenerator, GlueGenerator, SnowflakeGenerator,
+    AirflowGenerator, HiveGenerator, DynamodbGenerator, OdbcGenerator, MssqlGenerator, OracleGenerator,
+    RedshiftGenerator, ClickHouseGenerator, AthenaGenerator, QuicksightGenerator, DbtGenerator, TableauGenerator
+)
+
+__version__ = '0.1.10'
+
+__all__ = [
+    "PostgresqlGenerator",
+    "MysqlGenerator",
+    "KafkaGenerator",
+    "KafkaConnectGenerator",
+    "GlueGenerator",
+    "SnowflakeGenerator",
+    "AirflowGenerator",
+    "HiveGenerator",
+    "DynamodbGenerator",
+    "OdbcGenerator",
+    "MssqlGenerator",
+    "OracleGenerator",
+    "RedshiftGenerator",
+    "ClickHouseGenerator",
+    "AthenaGenerator",
+    "QuicksightGenerator",
+    "DbtGenerator",
+    "TableauGenerator",
+]
