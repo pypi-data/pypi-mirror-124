2b98e88ecf8109149651d0dc6a3db953a496d651
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:13:25 +0000

    Add package pharmpy-core/0.34.1/pharmpy_core-0.34.1-py3-none-any.whl

---
+++ packages/pharmpy-core/pharmpy_core-0.34.1-py3-none-any.whl/pharmpy/workflows/dispatchers/local_run.py
@@ -0,0 +1,18 @@
+import pharmpy.workflows.dispatchers
+
+
+def run(workflow):
+    from dask.distributed import Client
+    from dask.threaded import get
+
+    if pharmpy.workflows.dispatchers.conf.dask_dispatcher:
+        dask_dispatcher = pharmpy.workflows.dispatchers.conf.dask_dispatcher
+    else:
+        dask_dispatcher = 'distributed'
+
+    if dask_dispatcher == 'threaded':
+        res = get(workflow, 'results')
+    else:
+        with Client(threads_per_worker=1, processes=False) as client:
+            res = client.get(workflow, 'results')
+    return res
