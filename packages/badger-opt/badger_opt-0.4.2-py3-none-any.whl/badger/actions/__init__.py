4e1ce255fb122a96ba0a7c9352952a15c81b12af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:27 +0000

    Add package badger-opt/0.4.2/badger_opt-0.4.2-py3-none-any.whl

---
+++ packages/badger-opt/badger_opt-0.4.2-py3-none-any.whl/badger/actions/__init__.py
@@ -0,0 +1,28 @@
+from pkg_resources import get_distribution
+from ..factory import BADGER_PLUGIN_ROOT, BADGER_EXTENSIONS
+from ..db import BADGER_DB_ROOT
+from ..utils import yprint
+
+
+def show_info(args):
+    if args.gui:
+        from ..gui import launch_gui
+
+        launch_gui()
+    else:
+        info = {
+            'name': 'Badger the optimizer',
+            'version': get_distribution('badger-opt').version,
+            'plugin root': BADGER_PLUGIN_ROOT,
+            'database root': BADGER_DB_ROOT,
+        }
+
+        extensions = list(BADGER_EXTENSIONS.keys())
+        if extensions:
+            info['extensions'] = extensions
+
+        yprint(info)
+        # print(f'Badger the optimizer')
+        # print('====================')
+        # print(f'version      : {version}')
+        # print(f'plugins root : {BADGER_PLUGIN_ROOT}')
