721bd2a60750ec27c3654be19b649aad5591bd6b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:26 +0000

    Add package yadism/0.9.1/yadism-0.9.1.tar.gz

---
+++ packages/yadism/yadism-0.9.1.tar.gz/yadism-0.9.1/src/yadism/coefficient_functions/light/f2_cc.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from ..partonic_channel import RSL
+from . import f2_nc, nnlo
+
+
+class NonSinglet(f2_nc.NonSinglet):
+    def NNLO(self):
+        """
+        |ref| implements :eqref:`2.9`, :cite:`vogt-f2lcc`.
+        """
+
+        return RSL(
+            nnlo.xc2ns2p.c2nc2a, nnlo.xc2ns2p.c2ns2b, nnlo.xc2ns2p.c2nc2c, [self.nf]
+        )
+
+
+class Gluon(f2_nc.Gluon):
+    pass
+
+
+class Singlet(f2_nc.Singlet):
+    pass
---
+++ packages/yadism/yadism-0.9.1.tar.gz/yadism-0.9.1/src/yadism/coefficient_functions/light/fl_cc.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from ..partonic_channel import RSL
+from . import fl_nc, nnlo
+
+
+class NonSinglet(fl_nc.NonSinglet):
+    def NNLO(self):
+        """
+        |ref| implements :eqref:`2.9`, :cite:`vogt-f2lcc`.
+        """
+
+        return RSL(
+            nnlo.xclns2p.clnc2a, loc=nnlo.xclns2p.clnc2c, args=dict(reg=[self.nf])
+        )
+
+
+class Gluon(fl_nc.Gluon):
+    pass
+
+
+class Singlet(fl_nc.Singlet):
+    pass
