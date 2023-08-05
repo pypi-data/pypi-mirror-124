9596ab19d1551829509e697bfc75cc036a3f9a6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:13:12 +0000

    Add package oitrend/0.0.4/oitrend-0.0.4.tar.gz

---
+++ packages/oitrend/oitrend-0.0.4.tar.gz/oitrend-0.0.4/setup.py
@@ -0,0 +1,29 @@
+
+import setuptools
+
+from setuptools import setup, Extension
+with open('F:\option_data_startegy\README.md', "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name = 'oitrend',
+    packages=setuptools.find_packages(),
+    version = '0.0.4',
+    include_package_data=True,
+    description = 'Python library for OI trend',
+    long_description=long_description,
+    long_description_content_type="text/markdown",  author = 'VedantSonje',
+    author_email = 'vedant.sonje12@gmail.com',
+    url = '',
+    install_requires=['pandas','scipy','nsepython'],
+    keywords = [ 'python', 'trading', 'oi_trend'],
+    classifiers=[
+      'Intended Audience :: Developers',
+      'Natural Language :: English',
+      'Operating System :: OS Independent',
+      'Programming Language :: Python',
+      'Programming Language :: Python :: 3',
+      'Programming Language :: Python :: Implementation :: PyPy',
+      'Topic :: Software Development :: Libraries :: Python Modules'
+    ],
+)
