8162b38880180fb66aa3ac200696f4e6ee6f8633
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:05 +0000

    Add package lddya/1.1.5/lddya-1.1.5.tar.gz

---
+++ packages/lddya/lddya-1.1.5.tar.gz/lddya-1.1.5/lddya/function.py
@@ -0,0 +1,23 @@
+import datetime
+
+
+
+def matrix_add(A,B):
+    for i in range(len(A)):
+        for j in range(len(A)):
+            A[i][j]+=B[i][j]
+    return A
+
+def matrix_del(A,B):
+    for i in range(len(A)):
+        for j in range(len(A)):
+            A[i][j]-=B[i][j]
+    return A
+
+def map_reverse(fp = 'map.dll'):
+    os.chdir(sys.path[0])
+    with open(fp,'r') as f:
+        d = f.readlines()
+    d.reverse()
+    with open(fp,'w') as f:
+        f.writelines(d)
