2e5e50fdbcd389515f3c632fcae8fa5a714ff87d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:20:59 +0000

    Add package sum-and-mul/0.0.1/sum_and_mul-0.0.1.tar.gz

---
+++ packages/sum-and-mul/sum_and_mul-0.0.1.tar.gz/sum_and_mul-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='sum_and_mul',
+    version='0.0.1',
+    description='A package for summarizing array and multiplying the result on a number',
+    url="https://github.com/brenscrazy/sumandmulitlyer",
+    author='Vinnikov Gleb',
+    author_email='glebmanufa@gmail.com',
+    install_requires=['numpy==1.19'
+                      ]
+    )
