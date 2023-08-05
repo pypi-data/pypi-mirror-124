6f7bd30c9dbc397ee8a5c1316062defbc15e1557
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:04 +0000

    Add package odc-stats/1.0.1/odc_stats-1.0.1-py3-none-any.whl

---
+++ packages/odc-stats/odc_stats-1.0.1-py3-none-any.whl/odc/stats/_plugins.py
@@ -0,0 +1,52 @@
+import pydoc
+from typing import Callable, Dict
+from functools import partial
+
+from .model import StatsPluginInterface
+
+PluginFactory = Callable[..., StatsPluginInterface]
+
+_plugins: Dict[str, PluginFactory] = {}
+
+
+def _new(plugin_class, *args, **kwargs) -> StatsPluginInterface:
+    return plugin_class(*args, **kwargs)
+
+
+def resolve(name: str) -> PluginFactory:
+    maker = _plugins.get(name)
+    if maker is None:
+        maker = pydoc.locate(name)
+        if maker is not None:
+            if not issubclass(maker, (StatsPluginInterface,)):
+                raise ValueError(f"Custom StatsPlugin '{name}' is not derived from StatsPluginInterface")
+            return partial(_new, maker)
+
+    if maker is None:
+        raise ValueError(f"Failed to resolved named plugin: '{name}'")
+    return maker
+
+
+def register(name: str, plugin_class):
+    _plugins[name] = partial(_new, plugin_class)
+
+
+def import_all():
+    import importlib
+
+    # TODO: make that more automatic
+    modules = [
+        "odc.stats._fc_percentiles",
+        "odc.stats._tcw_percentiles",
+        "odc.stats._gm",
+        "odc.stats._gm_ls_bitmask",
+        "odc.stats._pq",
+        "odc.stats._pq_bitmask",
+        "odc.stats._wofs",
+    ]
+
+    for mod in modules:
+        try:
+            importlib.import_module(mod)
+        except ModuleNotFoundError:
+            pass
