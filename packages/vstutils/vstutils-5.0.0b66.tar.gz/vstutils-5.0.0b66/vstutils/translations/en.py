a2f43612400dbcd67a722c3c6fbaee18ca8c595b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:14:17 +0000

    Add package vstutils/5.0.0b66/vstutils-5.0.0b66.tar.gz

---
+++ packages/vstutils/vstutils-5.0.0b66.tar.gz/vstutils-5.0.0b66/vstutils/translations/en.py
@@ -0,0 +1,9 @@
+TRANSLATION = {
+    'version': 'versions | version | versions',
+    'app info': 'about',
+    'on item': 'items | item | items',
+    'file n selected': 'no file selected | 1 file selected | {n} files selected',
+    'image n selected': 'no image selected | 1 image selected | {n} images selected',
+    'imageValidationResolutionError': 'Must be between {min} and {max} px.',
+    'instance': 'instance | instances',
+}
