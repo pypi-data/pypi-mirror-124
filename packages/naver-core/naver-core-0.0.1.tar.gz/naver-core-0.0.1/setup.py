65742524ff9c69660ac2765d07edc58244121f0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:11:29 +0000

    Add package naver-core/0.0.1/naver-core-0.0.1.tar.gz

---
+++ packages/naver-core/naver-core-0.0.1.tar.gz/naver-core-0.0.1/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+    name='naver-core',
+    version='0.0.1',
+    packages=setuptools.find_packages(),
+    author="Jose Cuevas",
+    author_email="jose.cuevas.cv@gmail.com",
+    description="A Web Ancestor Library",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/jacr6/naver-web",
+    classifiers=[
+         "Programming Language :: Python :: 3",
+         "License :: OSI Approved :: MIT License",
+         "Operating System :: OS Independent",
+    ], install_requires=[
+        'naver-config',
+        'naver-db',
+    ],
+)
