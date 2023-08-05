bc2f0af58e41613ad1f74282f1f6a0ca0e1411c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:36 +0000

    Add package oc-mirror/0.1.1/oc_mirror-0.1.1.tar.gz

---
+++ packages/oc-mirror/oc_mirror-0.1.1.tar.gz/oc_mirror-0.1.1/oc_mirror/specs.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+# pylint: disable=too-few-public-methods
+
+"""Reusable string literals."""
+
+
+class OpenShiftReleaseSpecs:
+    """
+    OpenShift string literals.
+    """
+
+    IMAGE_REFERENCES_NAME = "image-references"
+    MANIFEST_PATH_PREFIX = "release-manifests/"
+    RELEASE_ANNOTATION_CONFIG_MAP_VERIFIER = (
+        "release.openshift.io/verification-config-map"
+    )
+    RELEASE_METADATA = "release-metadata"
