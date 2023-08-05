bda6d8b8f31d4aead9b26ae4e8fd35423b0b9f3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:11:01 +0000

    Add package everysk/1.0.9/everysk-1.0.9.tar.gz

---
+++ packages/everysk/everysk-1.0.9.tar.gz/everysk-1.0.9/everysk/api_resources/report_template.py
@@ -0,0 +1,22 @@
+###############################################################################
+#
+# (C) Copyright 2020 EVERYSK TECHNOLOGIES
+#
+# This is an unpublished work containing confidential and proprietary
+# information of EVERYSK TECHNOLOGIES. Disclosure, use, or reproduction
+# without authorization of EVERYSK TECHNOLOGIES is prohibited.
+#
+###############################################################################
+from everysk.api_resources.api_resource import (
+    RetrievableAPIResource,
+    ListableAPIResource
+)
+
+class ReportTemplate(
+    RetrievableAPIResource,
+    ListableAPIResource
+):
+    @classmethod
+    def class_name(cls):
+        return 'report_template'
+
