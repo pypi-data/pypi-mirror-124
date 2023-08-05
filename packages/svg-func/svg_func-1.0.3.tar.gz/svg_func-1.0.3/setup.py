7965a9604e50a1c5d7fe075a2c59fa8c41fc7dbb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:26 +0000

    Add package svg-func/1.0.3/svg_func-1.0.3.tar.gz

---
+++ packages/svg-func/svg_func-1.0.3.tar.gz/svg_func-1.0.3/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(name='svg_func',
+      version='1.0.3',
+      description='functions in svg for logo',
+      author='xmn',
+      author_email='2579015983@qq.com',
+      url='https://www.python.org/',
+      license='MIT',
+      keywords='ga nn',
+      project_urls={
+            'Documentation': 'https://packaging.python.org/tutorials/distributing-packages/',
+            'Funding': 'https://donate.pypi.org',
+            'Source': 'https://github.com/pypa/sampleproject/',
+            'Tracker': 'https://github.com/pypa/sampleproject/issues',
+      },
+      packages=['interpolation'],
+      install_requires=[],
+      python_requires='>=3'
+     )
