3679b39e097eb9b49f44d9dd979f02f689e17bef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:16:24 +0000

    Add package gosling/0.0.6/gosling-0.0.6-py3-none-any.whl

---
+++ packages/gosling/gosling-0.0.6-py3-none-any.whl/gosling/__init__.py
@@ -0,0 +1,28 @@
+import pandas as pd
+
+from gosling.schema import *
+from gosling.api import *
+from gosling.display import renderers
+from gosling.data import (
+    GoslingDataServer,
+    data_server,
+    json,
+    csv,
+    bigwig,
+    beddb,
+    bam,
+    vector,
+    matrix,
+    multivec,
+)
+
+
+@pd.api.extensions.register_dataframe_accessor("gos")  # type: ignore
+class GosAccessor:
+    def __init__(self, df: pd.DataFrame):
+        self._df = df
+
+    def csv(self, **kwargs):
+        content = self._df.to_csv(index=False) or ""
+        url = data_server(content, extension="csv")
+        return dict(type="csv", url=url, **kwargs)
