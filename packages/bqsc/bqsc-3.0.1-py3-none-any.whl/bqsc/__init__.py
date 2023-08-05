ece090ddfcf741c919a3546f941211d36f584475
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:13:15 +0000

    Add package bqsc/3.0.1/bqsc-3.0.1-py3-none-any.whl

---
+++ packages/bqsc/bqsc-3.0.1-py3-none-any.whl/bqsc/__init__.py
@@ -0,0 +1,14 @@
+__version__ = "3.0.1"
+
+from .load import (
+    load as load,
+    loads as loads,
+    load_dir as load_dir,
+    load_bq as load_bq
+)
+from .table import (
+    Table as Table,
+    TypeMismatch as TypeMismatch,
+    NotDefinedColumn as NotDefinedColumn,
+    dataframe as dataframe
+)
