88307d93b3ec7d02bcf7f672f64c597bcd2c466e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:16:07 +0000

    Add package openlxp-notification/1.1.8/openlxp-notification-1.1.8.tar.gz

---
+++ packages/openlxp-notification/openlxp-notification-1.1.8.tar.gz/openlxp-notification-1.1.8/openlxp_notifications/models.py
@@ -0,0 +1,63 @@
+from django.db import models
+from django.forms import ValidationError
+from django.urls import reverse
+from model_utils.models import TimeStampedModel
+
+from openlxp_notifications.management.utils.notification import \
+    email_verification
+
+
+class ReceiverEmailConfiguration(TimeStampedModel):
+    """Model for Receiver Email Configuration """
+
+    email_address = models.EmailField(
+        max_length=254,
+        help_text='Enter email personas addresses to send log data',
+        unique=True)
+
+    def get_absolute_url(self):
+        """ URL for displaying individual model records."""
+        return reverse('Configuration-detail', args=[str(self.id)])
+
+    def __str__(self):
+        """String for representing the Model object."""
+        return f'{self.id}'
+
+    def save(self, *args, **kwargs):
+        email_verification(self.email_address)
+        return super(ReceiverEmailConfiguration, self).save(*args, **kwargs)
+
+
+class SenderEmailConfiguration(TimeStampedModel):
+    """Model for Sender Email Configuration """
+
+    sender_email_address = models.EmailField(
+        max_length=254,
+        help_text='Enter sender email address to send log data from')
+
+    def save(self, *args, **kwargs):
+        if not self.pk and SenderEmailConfiguration.objects.exists():
+            raise ValidationError('There is can be only one '
+                                  'SenderEmailConfiguration instance')
+        return super(SenderEmailConfiguration, self).save(*args, **kwargs)
+
+
+class EmailConfiguration(TimeStampedModel):
+    """Model for Email Configuration """
+
+    SUBJECT = models.CharField(max_length=200, null=True)
+
+    HTML_File = models.CharField(max_length=200, null=True)
+
+    LOG_PATH = models.CharField(max_length=200, null=True)
+
+    def get_absolute_url(self):
+        """ URL for displaying individual model records."""
+        return reverse('Configuration-detail', args=[str(self.id)])
+
+    def __str__(self):
+        """String for representing the Model object."""
+        return f'{self.id}'
+
+    def save(self, *args, **kwargs):
+        return super(EmailConfiguration, self).save(*args, **kwargs)
