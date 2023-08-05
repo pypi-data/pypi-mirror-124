38a4173566af12d43ae27fa44496a2054bdbbac0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:15:08 +0000

    Add package lusid-web-app-sdk-preview/0.4.1356/lusid_web_app_sdk_preview-0.4.1356-py3-none-any.whl

---
+++ packages/lusid-web-app-sdk-preview/lusid_web_app_sdk_preview-0.4.1356-py3-none-any.whl/lusid_web_app/api/__init__.py
@@ -0,0 +1,17 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from lusid_web_app.api.access_api import AccessApi
+from lusid_web_app.api.articles_api import ArticlesApi
+from lusid_web_app.api.auditing_api import AuditingApi
+from lusid_web_app.api.config_api import ConfigApi
+from lusid_web_app.api.contacts_api import ContactsApi
+from lusid_web_app.api.logging_api import LoggingApi
+from lusid_web_app.api.personalisations_api import PersonalisationsApi
+from lusid_web_app.api.reconciliations_api import ReconciliationsApi
+from lusid_web_app.api.reports_api import ReportsApi
+from lusid_web_app.api.roadmap_api import RoadmapApi
+from lusid_web_app.api.settings_api import SettingsApi
+from lusid_web_app.api.tickets_api import TicketsApi
