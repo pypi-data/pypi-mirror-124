a825e6b8db8d4dbb2741e1e1a3cd8abe5cd34217
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:13:28 +0000

    Add package tigran-test/0.1/tigran_test-0.1.tar.gz

---
+++ packages/tigran-test/tigran_test-0.1.tar.gz/tigran_test-0.1/tigran_test/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='tigran_test',
+    version='0.1', 
+    description='My package', 
+    url='https://github.com/ruperson/tigran_test', 
+    install_requires=['matplotlib'],  
+    packages=['tigran_test'],  
+)
