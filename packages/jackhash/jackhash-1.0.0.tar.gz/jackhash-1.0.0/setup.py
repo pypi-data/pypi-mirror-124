fa8918ddd5055dc31943565fdd90031f4fdd4de0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:37 +0000

    Add package jackhash/1.0.0/jackhash-1.0.0.tar.gz

---
+++ packages/jackhash/jackhash-1.0.0.tar.gz/jackhash-1.0.0/setup.py
@@ -0,0 +1,43 @@
+import os
+import sys
+
+here = os.path.abspath(os.path.dirname(__file__))
+src = os.path.join(here, "src/jackhash")
+sys.path.append(src)
+
+from setuptools import find_packages
+from setuptools import setup
+
+meta = {
+    "name": "jackhash",
+    "description": "Japanese, ASCII, Chinese, Korean Hash encoding",
+    "license": "MIT",
+    "url": "https://github.com/amogorkon/jackhash",
+    "version": "1.0.0",
+    "author": "Anselm Kiefner",
+    "author_email": "jackhash@anselm.kiefner.de",
+    "python_requires": ">=3.6",
+    "keywords": [
+        "hash enocding unicode",
+    ],
+    "classifiers": [
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3 :: Only",
+    ],
+}
+
+
+with open("README.md") as f:
+    LONG_DESCRIPTION = f.read()
+
+setup(
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type="text/markdown",
+    zip_safe=False,
+    **meta
+)
