cb44e77e92d42685e08f8da9836eecb24d2d121f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:14:51 +0000

    Add package tigran-test3/0.1/tigran_test3-0.1.tar.gz

---
+++ packages/tigran-test3/tigran_test3-0.1.tar.gz/tigran_test3-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='tigran_test3',
+    version='0.1', 
+    description='My package', 
+    url='https://github.com/ruperson/tigran_test3', 
+    install_requires=['numpy==1.18.5'
+                      ],  
+    packages=['tigran_test3'],  
+)
