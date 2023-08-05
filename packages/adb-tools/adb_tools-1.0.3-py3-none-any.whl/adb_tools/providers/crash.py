974801a2dc9dc7e5522f8dafc81799d23f00afc6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:14:01 +0000

    Add package adb-tools/1.0.3/adb_tools-1.0.3-py3-none-any.whl

---
+++ packages/adb-tools/adb_tools-1.0.3-py3-none-any.whl/adb_tools/providers/crash.py
@@ -0,0 +1,35 @@
+class CRASH(object):
+    def __init__(self, title, app_version=None, time=None, channel=None, device_model=None,
+                 device_os_version=None,process=None, pid=None, tid=None, ABI=None,
+                 other_threads=None, backtrace=None, **kwargs):
+        self.title = title
+        self.app_version = app_version
+        self.channel = channel
+        self.time = time
+        self.device_model = device_model
+        self.device_os_version = device_os_version
+        self.process = process
+        self.pid = pid
+        self.tid = tid
+        self.ABI_list = ABI
+        self.other_threads = other_threads
+        self.backtrace = backtrace
+
+    def __str__(self):
+        return 'title: %s \nTime: %s \nAPP Version: %s \nDevice Channel: %s \n' \
+               'Device Model: %s \nDevice OS Version: %s \n' \
+               'Pid: %s \nTid: %s \nABI: %s \n' \
+               'Process Name: %s \nOther Threads: %s \nBacktrace: %s \n' % (
+                   self.title,
+                   self.time,
+                   self.app_version,
+                   self.channel,
+                   self.device_model,
+                   self.device_os_version,
+                   self.pid,
+                   self.tid,
+                   self.ABI_list,
+                   self.process,
+                   self.other_threads,
+                   self.backtrace
+               )
