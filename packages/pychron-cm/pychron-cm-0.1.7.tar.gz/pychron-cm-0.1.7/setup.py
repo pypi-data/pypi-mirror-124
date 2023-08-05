79b1dd895e0c0349bf5340e9a45225294afc90cc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:08:11 +0000

    Add package pychron-cm/0.1.7/pychron-cm-0.1.7.tar.gz

---
+++ packages/pychron-cm/pychron-cm-0.1.7.tar.gz/pychron-cm-0.1.7/setup.py
@@ -0,0 +1,42 @@
+# ===============================================================================
+# Copyright 2021 ross
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ===============================================================================
+
+from setuptools import setup, find_packages
+
+setup(
+    name="pychron-cm",
+    version="0.1.7",
+    install_requires=[
+        "Click",
+    ],
+    entry_points={
+        "console_scripts": [
+            "pcm = pcm.cli:cli",
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    packages=["pcm"],
+    python_requires=">=3.6",
+    include_package_data=True,
+    # package_data={
+    #     # If any package contains *.txt or *.rst files, include them:
+    #     "templates": ["*.template",],
+    # }
+)
+# ============= EOF =============================================
