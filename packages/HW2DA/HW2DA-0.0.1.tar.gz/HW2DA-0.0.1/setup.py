08174074abb705e2593b04833d36fca2ea4904f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:43 +0000

    Add package HW2DA/0.0.1/HW2DA-0.0.1.tar.gz

---
+++ packages/HW2DA/HW2DA-0.0.1.tar.gz/HW2DA-0.0.1/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+from setuptools import setup
+
+setup(
+    name='HW2DA',
+    version='0.0.1',
+    description='Package for HW 2',
+    author='Eugene Gusarov',
+    author_email='gen05@yandex-team.ru',
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+    install_requires=[
+        'numpy>=1.16',
+        'matplotlib',
+    ]
+)
