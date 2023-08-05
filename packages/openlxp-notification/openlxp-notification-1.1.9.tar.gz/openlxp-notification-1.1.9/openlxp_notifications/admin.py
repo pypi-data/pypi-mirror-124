3d7bd421eb38791265c2515b8099b678d8a346d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:16:34 +0000

    Add package openlxp-notification/1.1.9/openlxp-notification-1.1.9.tar.gz

---
+++ packages/openlxp-notification/openlxp-notification-1.1.9.tar.gz/openlxp-notification-1.1.9/openlxp_notifications/admin.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+
+from .models import ReceiverEmailConfiguration, SenderEmailConfiguration, EmailConfiguration
+
+
+@admin.register(ReceiverEmailConfiguration)
+class ReceiverEmailConfigurationAdmin(admin.ModelAdmin):
+    list_display = ('email_address',)
+
+
+@admin.register(SenderEmailConfiguration)
+class SenderEmailConfigurationAdmin(admin.ModelAdmin):
+    list_display = ('sender_email_address',)
+
+
+@admin.register(EmailConfiguration)
+class EmailConfigurationAdmin(admin.ModelAdmin):
+    list_display = ('SUBJECT',
+                    'HTML_File',
+                    'LOG_PATH',)
