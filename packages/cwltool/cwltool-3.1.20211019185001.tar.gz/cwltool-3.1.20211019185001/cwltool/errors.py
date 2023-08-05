df55b1930e829339d9af45d9e91da668458844f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:40 +0000

    Add package cwltool/3.1.20211019185001/cwltool-3.1.20211019185001.tar.gz

---
+++ packages/cwltool/cwltool-3.1.20211019185001.tar.gz/cwltool-3.1.20211019185001/cwltool/errors.py
@@ -0,0 +1,12 @@
+class WorkflowException(Exception):
+    pass
+
+
+class UnsupportedRequirement(WorkflowException):
+    pass
+
+
+class ArgumentException(Exception):
+    """Mismatched command line arguments provided."""
+
+    pass
