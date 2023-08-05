7972881c193c9f9556e1c99dcb4f6513ebf7d7df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:55 +0000

    Add package mumin/0.3.1/mumin-0.3.1-py3-none-any.whl

---
+++ packages/mumin/mumin-0.3.1-py3-none-any.whl/mumin/__init__.py
@@ -0,0 +1,14 @@
+__version__ = '0.3.1'
+
+# Set up logging
+import logging
+fmt = '%(asctime)s [%(levelname)s] %(message)s'
+logging.basicConfig(level=logging.INFO, format=fmt)
+logging.getLogger('urllib3').disabled = True
+logging.getLogger('urllib3').propagate = False
+logging.getLogger('newspaper').disabled = True
+logging.getLogger('newspaper').propagate = False
+logging.getLogger('jieba').disabled = True
+logging.getLogger('jieba').propagate = False
+
+from .dataset import MuminDataset  # noqa
