80643e93ed1b1110781f5e1562c4208b6b5bc84b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:12:32 +0000

    Add package nettlesome/0.6.0/nettlesome-0.6.0.tar.gz

---
+++ packages/nettlesome/nettlesome-0.6.0.tar.gz/nettlesome-0.6.0/nettlesome/__init__.py
@@ -0,0 +1,9 @@
+"""Nettlesome: Simplified semantic reasoning."""
+
+from .entities import Entity
+from .groups import FactorGroup
+from .predicates import Predicate
+from .quantities import Comparison, DateRange, IntRange, DecimalRange, UnitRange
+from .statements import Statement, Assertion
+
+__version__ = "0.6.0"
