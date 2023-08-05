22df9f69a35e02fe259bfe059581b95f3efddbbf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:29 +0000

    Add package iOS-tools/1.0.1/iOS_tools-1.0.1.tar.gz

---
+++ packages/iOS-tools/iOS_tools-1.0.1.tar.gz/iOS_tools-1.0.1/setup.py
@@ -0,0 +1,13 @@
+# -*- coding:utf-8 -*-
+from setuptools import setup
+
+setup(
+    name="iOS_tools",
+    version="1.0.1",
+    author="crystal",
+    author_email="zhuhuiping@shizhuang-inc.com",
+    description="iOS tools",
+    packages=["iOS_tools"],
+    python_requires=">=3.7",
+    long_description_content_type="text/x-rst"
+)
