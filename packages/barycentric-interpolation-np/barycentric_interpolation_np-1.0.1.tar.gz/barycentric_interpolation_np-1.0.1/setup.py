09ee146016eff89f770b06b17e514a8299cf5197
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:11:15 +0000

    Add package barycentric-interpolation-np/1.0.1/barycentric_interpolation_np-1.0.1.tar.gz

---
+++ packages/barycentric-interpolation-np/barycentric_interpolation_np-1.0.1.tar.gz/barycentric_interpolation_np-1.0.1/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+setup(
+    name='barycentric_interpolation_np',
+    version='1.0.1',
+    description='A example Python package',
+    url='https://3vilwind.com',
+    author='Nikolai Rakov',
+    author_email='thrash@3vilwind.com',
+    license='MIT',
+    packages=['barycentric_interpolation_np'],
+    install_requires=['numpy==1.17.2'],
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+)
