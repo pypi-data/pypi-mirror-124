b8505f2b96ea09f34bffe0c01892b41dd1aae32b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 17:19:48 +0000

    Add package webshell/0.0.2/webshell-0.0.2.tar.gz

---
+++ packages/webshell/webshell-0.0.2.tar.gz/webshell-0.0.2/README.md
@@ -0,0 +1,21 @@
+
+## **Web Shel**l
+
+A Python package that helps to add web UI on command line application.
+
+**Uses**
+
+To use this package.
+
+*  pip install webshell
+*  In your script say `main.py`
+
+---
+```markdown
+from webshell import app
+if __name__ == '__main__':
+    app.run(port=4000)
+```
+---
+
+*  Now open http://localhost:4000 in your browser, that's it.
---
+++ packages/webshell/webshell-0.0.2.tar.gz/webshell-0.0.2/setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+import pypandoc
+
+def long_description():
+    try:
+        long_description = pypandoc.convert_file('README.md', 'rst')
+    except(IOError, ImportError):
+        long_description = open('README.md').read()
+    return long_description
+
+
+setup(
+    name='webshell',
+    version='0.0.2',    
+    description='A Python package to add web ui on command line application',
+    url='https://gitlab.midwestholding.dev/programmers.io/web_shell',
+    author='Manoj Datt',
+    platforms='any',
+    long_description=long_description(),
+    author_email='manoj.datt@programmers.io',
+    license='BSD 2-clause',
+    include_package_data=True,
+    packages=find_packages(),
+    install_requires=['Flask==2.0.2','pypandoc==1.6.4'],
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',  
+        'Operating System :: POSIX :: Linux',        
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+)
