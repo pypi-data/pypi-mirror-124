884f045b95578f872a9e9871147c5e66ba119d8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:12 +0000

    Add package xchainpy-crypto/0.1.9/xchainpy_crypto-0.1.9.tar.gz

---
+++ packages/xchainpy-crypto/xchainpy_crypto-0.1.9.tar.gz/xchainpy_crypto-0.1.9/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='xchainpy_crypto',
+    packages=find_packages(),
+    version='0.1.9',
+    license='MIT',
+    description='XCHAIN-CRYPTO encrypts a master phrase to a keystore',
+    author='THORChain',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/xchainjs/xchainpy-lib/tree/main/xchainpy/xchainpy_crypto',
+    keywords=["THORChain", "XChainpy","XChainpy_crypto"],
+    install_requires=['mnemonic>=0.18', 'bip_utils ==1.11.1, <=2.0.0', 'pycryptodome >= 3.10.1, <=4.0.0'],
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python'
+    ],
+)
