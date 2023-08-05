fd2b5f764f3566426de7f61c2852d8dc5362116a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:14:15 +0000

    Add package PythonReports/0.10.1/PythonReports-0.10.1.tar.gz

---
+++ packages/PythonReports/PythonReports-0.10.1.tar.gz/PythonReports-0.10.1/PythonReports/version.py
@@ -0,0 +1,9 @@
+"""PythonReports package version declaration"""
+
+from datetime import date
+
+__version__ = "0.10.1"
+__date__ = date(2021, 10, 20)
+__all__ = ["__version__", "__date__"]
+
+# vim: set et sts=4 sw=4 :
