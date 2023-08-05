059959b5cb067f11217998633386b408169d0afc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:13:46 +0000

    Add package kwtools/0.0.1/kwtools-0.0.1-py3-none-any.whl

---
+++ packages/kwtools/kwtools-0.0.1-py3-none-any.whl/kwtools/tools2/utils_python.py
@@ -0,0 +1,29 @@
+import sys
+import time
+import json
+import random
+import functools
+import logging
+import traceback
+import pandas as pd
+import numpy as np
+
+
+
+
+class UtilsPython():
+    def __init__(self):
+        pass
+
+
+
+
+
+
+utils_python = UtilsPython()
+
+
+
+
+if __name__ == "__main__":
+    pass
---
+++ packages/kwtools/kwtools-0.0.1-py3-none-any.whl/kwtools/tools2/utils_requests.py
@@ -0,0 +1,33 @@
+import os
+import sys
+import re
+import json
+import traceback
+import user_agent
+import socket
+import websocket
+import ssl
+import time
+import random
+import requests
+import collections
+import urllib
+from copy import deepcopy
+from retry import retry
+
+
+
+
+class UtilsRequests():
+    def  __init__(self):
+        pass
+
+
+
+
+utils_requests = UtilsRequests()
+
+
+
+if __name__ == '__main__':
+    pass
