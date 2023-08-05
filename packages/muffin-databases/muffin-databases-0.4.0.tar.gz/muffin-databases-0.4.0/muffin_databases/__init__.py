2863c0088f0a4cf36a73a6a4fed42b220e81f65c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:32 +0000

    Add package muffin-databases/0.4.0/muffin-databases-0.4.0.tar.gz

---
+++ packages/muffin-databases/muffin-databases-0.4.0.tar.gz/muffin-databases-0.4.0/muffin_databases/__init__.py
@@ -0,0 +1,44 @@
+"""Support databases for Muffin framework."""
+
+import typing as t
+
+from databases import Database
+from muffin.plugins import BasePlugin
+
+
+__version__ = "0.4.0"
+__project__ = "muffin-databases"
+__author__ = "Kirill Klenov <horneds@gmail.com>"
+__license__ = "MIT"
+
+
+class Plugin(BasePlugin):
+
+    """Manage databases connections for Muffin."""
+
+    name = 'databases'
+    defaults: t.Dict = {
+        'params': {},
+        'url': 'sqlite:///:memory:',
+    }
+
+    def __getattr__(self, name) -> t.Any:
+        """Proxy attributes to self database."""
+        if not self.installed:
+            raise AttributeError(name)
+
+        return getattr(self.__database__, name)
+
+    def setup(self, *args, **options):
+        """Initialize a database."""
+        super(Plugin, self).setup(*args, **options)
+
+        self.__database__ = Database(self.cfg.url, **self.cfg.params)
+
+    async def startup(self):
+        """Disconnect the database."""
+        await self.__database__.connect()
+
+    async def shutdown(self):
+        """Disconnect the database."""
+        await self.__database__.disconnect()
