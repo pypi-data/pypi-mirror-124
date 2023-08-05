ba908e831cfa4aab98ba14baecc9daea73e47a56
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:11:51 +0000

    Add package istra-hw2package/0.2/istra_hw2package-0.2.tar.gz

---
+++ packages/istra-hw2package/istra_hw2package-0.2.tar.gz/istra_hw2package-0.2/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(name='istra_hw2package',
+      version='0.2',
+      description='Package for hw2',
+      long_description='Package for hw2',
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.7',
+        'Topic :: Text Processing :: Linguistic',
+      ],
+      keywords='hello_world datetime',
+      author='Istratov_N',
+      license='MIT',
+      packages=find_packages(),
+      install_requires=[
+          'Flask',
+      ],
+      include_package_data=True,
+      zip_safe=False)
