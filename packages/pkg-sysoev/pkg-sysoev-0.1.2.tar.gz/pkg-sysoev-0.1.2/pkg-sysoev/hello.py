da8ef6858107d5855bfa857eaae4e10c77ac1105
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:11:59 +0000

    Add package pkg-sysoev/0.1.2/pkg-sysoev-0.1.2.tar.gz

---
+++ packages/pkg-sysoev/pkg-sysoev-0.1.2.tar.gz/pkg-sysoev-0.1.2/pkg-sysoev/hello.py
@@ -0,0 +1,30 @@
+import random
+
+import numpy as np
+import scipy.interpolate as si
+import matplotlib.pylab as plt
+
+def test():
+    a = [-1, 2, -3]
+    print(np.abs(a))
+
+    xi = [i for i in range(20)]
+    yi = [random.randint(1, 20) for _ in range(20)]
+    min_x = min(xi)
+    max_x = max(xi)
+    x = np.arange(min_x, max_x, (max_x - min_x) / 100)
+
+    interpolator = si.CubicSpline(xi, yi)
+    y = interpolator(x)
+
+    plt.scatter(xi, yi)
+    plt.plot(x, y)
+
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.title('Interpolation Demo')
+    plt.show()
+
+
+if __name__ == '__main__':
+    test()
---
+++ packages/pkg-sysoev/pkg-sysoev-0.1.2.tar.gz/pkg-sysoev-0.1.2/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+    name='pkg-sysoev',
+    version='0.1.2',
+    description='A example Python package',
+    author='Sysoev Alexander',
+    url='https://github.com/Mr3zee',
+    author_email='shudson@anl.gov',
+    license='MIT',
+    packages=['pkg-sysoev'],
+    install_requires=['numpy==1.20.0', 'scipy', 'matplotlib'],
+
+    classifiers=[
+        'Programming Language :: Python :: 3.9',
+    ],
+)
