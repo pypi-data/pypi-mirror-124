c0d088cf7b713c0c6ee6a13b79c2ed4858d10ae2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 22:11:00 +0000

    Add package patchwork-node/0.0.7/patchwork-node-0.0.7.tar.gz

---
+++ packages/patchwork-node/patchwork-node-0.0.7.tar.gz/patchwork-node-0.0.7/patchwork/node/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+"""Created on 20.01.2020
+
+.. moduleauthor:: Paweł Pecio
+"""
+
+from patchwork.node.core.worker import PatchworkWorker
+
+if __name__ == '__main__':
+    worker = PatchworkWorker()
+    worker.main()
