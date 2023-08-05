be1c4d4ef92315424a242f875a8170eb90b1e3a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 21:09:52 +0000

    Add package pydetex/0.7.0/pydetex-0.7.0-py3-none-any.whl

---
+++ packages/pydetex/pydetex-0.7.0-py3-none-any.whl/pydetex/pipelines.py
@@ -0,0 +1,58 @@
+"""
+PyDetex
+https://github.com/ppizarror/PyDetex
+
+PIPELINES
+Defines the pipelines which apply parsers.
+"""
+
+__all__ = [
+    'simple',
+    'strict',
+    'PipelineType'
+]
+
+import pydetex.parsers as par
+from typing import Callable
+
+PipelineType = Callable[[str], str]
+
+
+def simple(s: str) -> str:
+    """
+    The most simple pipeline ever.
+
+    :param s: String latex
+    :return: String with no latex!
+    """
+    if len(s) == 0:
+        return s
+    s = '\n'.join(s.splitlines())
+    s = par.remove_comments(s)
+    s = par.simple_replace(s)
+    s = par.remove_common_tags(s)
+    s = par.process_cite(s)
+    s = par.process_ref(s)
+    s = par.process_labels(s)
+    s = par.remove_comments(s)
+    s = par.process_quotes(s)
+    s = par.process_inputs(s)
+    s = par.process_single_char_equations(s)
+    if len(s) > 0 and s[-1] == '\\':
+        s = s[0:len(s) - 1]
+    return s
+
+
+def strict(s: str) -> str:
+    """
+    Applies simple + removes all commands.
+
+    :param s: String latex
+    :return: String with no latex!
+    """
+    s = simple(s)
+    s = par.remove_commands_char(s, '$')
+    s = par.remove_commands_param(s)
+    s = par.remove_commands_param_noargv(s)
+    s = par.remove_comments(s)
+    return s
