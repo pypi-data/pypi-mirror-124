6ca45c88a54a8084c900886e39b5f1bf6e33ed57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:30:55 +0000

    Add package asgi-correlation-id/0.1.0/asgi-correlation-id-0.1.0.tar.gz

---
+++ packages/asgi-correlation-id/asgi-correlation-id-0.1.0.tar.gz/asgi-correlation-id-0.1.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['asgi_correlation_id']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['starlette>=0.16.0,<0.17.0']
+
+setup_kwargs = {
+    'name': 'asgi-correlation-id',
+    'version': '0.1.0',
+    'description': 'Middleware that propagates HTTP header correlation IDs to project logs',
+    'long_description': '[![test](https://github.com/snok/asgi-correlation-id/actions/workflows/test.yml/badge.svg)](https://github.com/snok/asgi-correlation-id/actions/workflows/test.yml)\n[![codecov](https://codecov.io/gh/snok/asgi-correlation-id/branch/master/graph/badge.svg?token=1aXlWPm2gb)](https://codecov.io/gh/snok/asgi-correlation-id)\n[![pypi](https://pypi.org/pypi/asgi-correlation-id)](https://github.com/snok/asgi-correlation-id)\n\n\n# ASGI Correlation ID middleware\n\nThis is a rewrite of [django-guid](django-guid) for ASGI apps.\n',
+    'author': 'Jonas Krüger Svensson',
+    'author_email': 'jonas-ks@hotmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/snok/asgi-correlation-id',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
