03ee564882855ea47fc873ffbf34a19e58f9aec7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:36:54 +0000

    Add package django-gamma-cloudinary/0.0.3/django-gamma-cloudinary-0.0.3.tar.gz

---
+++ packages/django-gamma-cloudinary/django-gamma-cloudinary-0.0.3.tar.gz/django-gamma-cloudinary-0.0.3/tests/settings/development.py
@@ -0,0 +1,56 @@
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+SECRET_KEY = 'test_secret_key'
+
+DEBUG = False
+
+TEST_ROOT = os.path.join(BASE_DIR, 'tests')
+
+INSTALLED_APPS = [
+    'django.contrib.staticfiles',
+    'cloudinary',
+    'gamma_cloudinary'
+]
+
+MIDDLEWARE = [
+
+]
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+            ],
+        },
+    },
+]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+DEFAULT_FILE_STORAGE = 'gamma_cloudinary.storage.CloudinaryStorage'
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_STORAGE = 'gamma_cloudinary.storage.StaticCloudinaryStorage'
+
+STATICFILES_DIRS = [
+    os.path.join(TEST_ROOT, 'static')
+]
+
+
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': 'baraka',
+    'API_KEY': '848287299472914',
+    'API_SECRET': 'AsvFgnhE9jh2FArUDJ1ORYzUuTE',
+    'BASE_STORAGE_LOCATION': '/gamma_test/',
+    'SECURE': True
+}
+
