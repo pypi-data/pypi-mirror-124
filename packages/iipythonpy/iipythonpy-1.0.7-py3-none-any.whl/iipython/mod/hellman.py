94fab6137a4c44e27f5bdd8ef51914dfa06209f4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:35:58 +0000

    Add package iipythonpy/1.0.7/iipythonpy-1.0.7-py3-none-any.whl

---
+++ packages/iipythonpy/iipythonpy-1.0.7-py3-none-any.whl/iipython/mod/hellman.py
@@ -0,0 +1,51 @@
+# Copyright 2021 iiPython
+# Diffie Hellman Algorithm
+
+# Modules
+import random
+from Crypto.Cipher import AES    
+from Crypto.Hash import SHA256
+
+# Hellman class
+class Hellman(object):
+    def __init__(self, base: int = 9385, modu: int = 15 ** 15) -> None:
+        self.base = base
+        self.modu = modu
+
+        self.priv_key = random.randint(1000, 9999)
+        self.pub_key = self.generate_public(self.priv_key)
+
+        self.shared_key = None
+        self.hash_key = None
+
+    def pad(self, string: str) -> str:
+        return string + (16 - len(string) % 16) * "{"
+
+    def generate_public(self, priv: int) -> int:
+        """
+        Generates a public key given a private key.
+        """
+        return (self.base ** priv) % self.modu
+
+    def generate_shared(self, pub: int) -> int:
+        """
+        Generates a shared secret key given another users public key.
+        """
+        self.shared_key = (pub ** self.priv_key) % self.modu
+        self.hash_key = SHA256.new(str(self.shared_key).encode("utf8")).digest()
+        return self.shared_key
+
+    def encrypt(self, data: str) -> bytes:
+        if self.shared_key is None:
+            raise RuntimeError("no shared key has been established yet!")
+
+        cipher = AES.new(self.hash_key, AES.MODE_ECB)
+        return cipher.encrypt(self.pad(data).encode("utf8"))
+
+    def decrypt(self, data: bytes) -> str:
+        if self.shared_key is None:
+            raise RuntimeError("no shared key has been established yet!")
+
+        decipher = AES.new(self.hash_key, AES.MODE_ECB)
+        pt = decipher.decrypt(data).decode("utf8")
+        return pt[:pt.find("{")]
