2874d82915d473e919d0dfa56e0f1684214aa54f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:14:43 +0000

    Add package ztools/2.4.2/ztools-2.4.2-py2.py3-none-any.whl

---
+++ packages/ztools/ztools-2.4.2-py2.py3-none-any.whl/ztools/__init__.py
@@ -0,0 +1,37 @@
+# Copyright (c) 2018-2021 ZouMingzhe <zoumingzhe@qq.com>
+# This module is part of the ztools package, which is released under a MIT licence.
+
+
+"""
+for common
+"""
+from .common.file           import (fbasic)
+from .common.hash           import (hash)
+from .common.sheet          import (csv, xls)
+
+"""
+for database
+"""
+from .db.CRUD               import (CRUD)
+from .db.MySQL              import (MySQL)
+from .db.SQLite             import (SQLite)
+
+"""
+for data structure
+"""
+from .ds.tqueue             import (tqueue)
+from .ds.tstack             import (tstack)
+
+
+"""
+for gui
+"""
+from .gui.plot              import (plot)
+
+
+"""
+for utils
+"""
+from .utils.progressbar     import (progressbar)
+from .utils.timeout         import (timeout)
+from .utils.tprint          import (AnsiStyle, AnsiFore, AnsiBack, tprint)
