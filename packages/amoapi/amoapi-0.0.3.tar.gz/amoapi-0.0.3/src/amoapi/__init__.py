9a82be26af2d6ca278eeeb882a868250aea1b85a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:42:14 +0000

    Add package amoapi/0.0.3/amoapi-0.0.3.tar.gz

---
+++ packages/amoapi/amoapi-0.0.3.tar.gz/amoapi-0.0.3/src/amoapi/__init__.py
@@ -0,0 +1,18 @@
+import os
+from pathlib import Path
+from .interaction import BaseInteraction
+from .tokens import TokenManager, FileTokensStorage
+
+# load token manager
+token_path = Path.cwd().resolve()
+
+default_token_manager = TokenManager()
+default_token_manager(
+    client_id=os.environ.get("client_id"),
+    client_secret=os.environ.get("client_secret"),
+    subdomain=os.environ.get("subdomain"),
+    redirect_url=os.environ.get("redirect_url"),
+    storage=FileTokensStorage(token_path),  # by default FileTokensStorage
+)
+default_token_manager.init(code=os.environ.get("start_code"), skip_error=True)
+amo_interaction = BaseInteraction(token_manager=default_token_manager)
