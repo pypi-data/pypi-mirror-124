338416f35a8b643ba0a98840ce0cfebd764e3285
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:42:00 +0000

    Add package unicon.plugins/21.10/unicon.plugins-21.10-py3-none-any.whl

---
+++ packages/unicon.plugins/unicon.plugins-21.10-py3-none-any.whl/unicon/plugins/__init__.py
@@ -0,0 +1,39 @@
+__version__ = '21.10'
+
+supported_chassis = [
+    'single_rp',
+    'dual_rp',
+    'stack',
+    'quad',
+]
+
+supported_os = [
+    'generic',
+    'ios',
+    'nxos',
+    'iosxe',
+    'iosxr',
+    'aireos',
+    'linux',
+    'cheetah',
+    'ise',
+    'asa',
+    'nso',
+    'confd',
+    'vos',
+    'cimc',
+    'fxos',
+    'junos',
+    'staros',
+    'aci',
+    'sdwan',
+    'sros',
+    'apic',
+    'windows',
+    'dell',
+    'comware',
+    'ironware',
+    'eos',
+    'gaia',
+    'slxos'
+]
