bb706f647c649984dee4fa25a0aee870fb36b55a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:34:51 +0000

    Add package rball/0.1.2/rball-0.1.2.tar.gz

---
+++ packages/rball/rball-0.1.2.tar.gz/rball-0.1.2/rball/__init__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+"""Top-level package for rball."""
+
+from .response_database import ResponseDatabase
+from .rballlike import RBallLike
+
+__author__ = """J. Michael Burgess"""
+__email__ = "jburgess@mpe.mpg.de"
+
+from ._version import get_versions
+
+__version__ = get_versions()["version"]
+del get_versions
