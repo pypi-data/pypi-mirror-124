cb7788da6d03fad2b146d99e25d0defb44cf76c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:28:23 +0000

    Add package french-invoice-generator/0.3/french_invoice_generator-0.3-py2.py3-none-any.whl

---
+++ packages/french-invoice-generator/french_invoice_generator-0.3-py2.py3-none-any.whl/invoice_generator/__init__.py
@@ -0,0 +1,7 @@
+"""Top-level package for Invoice Generator."""
+
+__author__ = """Thibault Grandjean"""
+__email__ = 'thibault@cornet-grandjean.com'
+__version__ = '0.1.0'
+
+from .invoice_generator import InvoiceGenerator  # noqa: F401
