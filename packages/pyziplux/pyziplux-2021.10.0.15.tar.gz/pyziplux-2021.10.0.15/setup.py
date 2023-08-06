3386345231e26f4e1c65ffa4f89d323af0fed5ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:10:30 +0000

    Add package pyziplux/2021.10.0.15/pyziplux-2021.10.0.15.tar.gz

---
+++ packages/pyziplux/pyziplux-2021.10.0.15.tar.gz/pyziplux-2021.10.0.15/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+import os
+pkgs=list(filter(None, [x[0].replace('{}/src/'.format(os.getcwd()), '').replace('/','.') if '.git' not in x[0] else '' for x in os.walk('{}/src/'.format(os.getcwd()))]))
+setup(
+    name='pyziplux',
+    version='2021.10.0.15',
+    license='MIT',
+    author="Ziplux LHS",
+    author_email='ziplux.so@ziplux.so',
+    packages=find_packages(include=pkgs),
+    package_dir={'': 'src'},
+    url='https://github.com/ZipluxLhs/pyziplux',
+    keywords='middleware',
+    install_requires=[
+           'pip',
+	  'setuptools',
+    	  'wheel',
+           'et_xmlfile',
+      ],
+
+)
