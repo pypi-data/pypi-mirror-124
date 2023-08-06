b3654f9e7eeb064c565d7376ba22982e7523086d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:07:11 +0000

    Add package pgeng/1.3.2/pgeng-1.3.2-py3-none-any.whl

---
+++ packages/pgeng/pgeng-1.3.2-py3-none-any.whl/pgeng/tile.py
@@ -0,0 +1,44 @@
+'''Functions and classes for tiles and physics objects'''
+#IMPORTS
+from pygame import Rect
+#IMPORTS
+
+#VARIABLES
+tile_size = 0
+#VARIABLES
+
+#TILE
+class Tile:
+	'''A tile used by the Entity object to check collision
+	size can be a single number and it will be the width and the height, or it can be a tuple or list
+	It also supports ramps (45 degrees)
+	If the width and height are not the same, ramp will be 0
+	ramp 0 = no ramp
+	ramp 1 = top left
+	ramp 2 = top right
+	ramp 3 = bottom right
+	ramp 4 = bottom left
+
+
+	Attributes:
+
+	location
+
+	size
+
+	ramp'''
+	#__INIT__
+	def __init__(self, location, size, ramp=0):
+		'''Initialising a Tile object'''
+		self.location = list(location)
+		self.size = list(size) if type(size) is list or type(size) is tuple else [size, size]
+		self.ramp = ramp if self.size[0] == self.size[1] else 0
+	#__INIT__
+
+	#RECT
+	@property
+	def rect(self):
+		'''Returns a pygame.Rect object of the Tile object'''
+		return Rect(self.location[0], self.location[1], self.size[0], self.size[1])
+	#RECT
+#TILE
