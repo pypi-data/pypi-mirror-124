1ed845737f93e95120b0bcf643e874918d48b958
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:21:27 +0000

    Add package igi-file-transformation-contract/0.0.4/igi_file_transformation_contract-0.0.4.tar.gz

---
+++ packages/igi-file-transformation-contract/igi_file_transformation_contract-0.0.4.tar.gz/igi_file_transformation_contract-0.0.4/igi_file_transformation_contract/__init__.py
@@ -0,0 +1,7 @@
+from igi_file_transformation_contract.interface import IFileTransformer
+from igi_file_transformation_contract.exceptions import IGIUserFriendlyException
+from igi_file_transformation_contract.result import (
+    TransformationResult,
+    Status,
+    SuccessStatus,
+)
