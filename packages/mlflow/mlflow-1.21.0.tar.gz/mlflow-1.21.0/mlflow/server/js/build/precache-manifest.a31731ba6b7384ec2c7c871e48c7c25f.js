0edd5eb0efb683c57d39de7d6b37a97528f7d99a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:28:59 +0000

    Add package mlflow/1.21.0/mlflow-1.21.0.tar.gz

---
+++ packages/mlflow/mlflow-1.21.0.tar.gz/mlflow-1.21.0/mlflow/server/js/build/precache-manifest.a31731ba6b7384ec2c7c871e48c7c25f.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = [
+  {
+    "revision": "1ff1dec60726a2304708",
+    "url": "static-files/static/css/main.98e99bde.chunk.css"
+  },
+  {
+    "revision": "1ff1dec60726a2304708",
+    "url": "static-files/static/js/main.285c7155.chunk.js"
+  },
+  {
+    "revision": "f018b2737859e512146b",
+    "url": "static-files/static/js/runtime~main.f92e4d4d.js"
+  },
+  {
+    "revision": "6c61d1bca00baef579f5",
+    "url": "static-files/static/css/2.98ab96dd.chunk.css"
+  },
+  {
+    "revision": "6c61d1bca00baef579f5",
+    "url": "static-files/static/js/2.8b1f65b9.chunk.js"
+  },
+  {
+    "revision": "ab1151827f7dd0db6cca",
+    "url": "static-files/static/js/3.c6118478.chunk.js"
+  },
+  {
+    "revision": "c17ec543e0d6a592928535f43867f969",
+    "url": "static-files/static/media/index.c17ec543.less"
+  },
+  {
+    "revision": "f8c411a9a70c61fd7000bb3082b16e23",
+    "url": "static-files/static/media/default-error.f8c411a9.svg"
+  },
+  {
+    "revision": "5092675c27ede31134e05ef939bb3b7f",
+    "url": "static-files/static/media/filter-icon.5092675c.svg"
+  },
+  {
+    "revision": "7a9dd62d43af29b22609a5aa93325481",
+    "url": "static-files/static/media/home-logo.7a9dd62d.png"
+  },
+  {
+    "revision": "d58c509937a4d6b0b74ade8a685b16cd",
+    "url": "static-files/static/media/notebook.d58c5099.svg"
+  },
+  {
+    "revision": "8b250f7f9dfd82d346bc7c4e34fb5400",
+    "url": "static-files/static/media/revision.8b250f7f.svg"
+  },
+  {
+    "revision": "91661969e9b84d8458d5be802ce436d6",
+    "url": "static-files/static/media/empty.91661969.svg"
+  },
+  {
+    "revision": "0e9e57fc119af79203e8a87520122dd4",
+    "url": "static-files/static/media/laptop.0e9e57fc.svg"
+  },
+  {
+    "revision": "673c346d4c92cca2efb614a8013994de",
+    "url": "static-files/static/media/project.673c346d.svg"
+  },
+  {
+    "revision": "31372da50f2b97dd943a05080bbd1b0f",
+    "url": "static-files/static/media/job.31372da5.svg"
+  },
+  {
+    "revision": "3f204b5402b58877f290f685a8e20d99",
+    "url": "static-files/static/media/warning.3f204b54.svg"
+  },
+  {
+    "revision": "246bfb44ab6d6f0973e0516ae6436ec0",
+    "url": "static-files/static/media/404-overflow.246bfb44.svg"
+  },
+  {
+    "revision": "27426c1d3a2e50de28af78c2017b3bf0",
+    "url": "static-files/static/media/registered-model.27426c1d.svg"
+  },
+  {
+    "revision": "52a4e4559a79eb2745a5c05eb321b4b3",
+    "url": "static-files/static/media/logged-model.52a4e455.svg"
+  },
+  {
+    "revision": "aee7e216079bea4ebe0816d94970b022",
+    "url": "static-files/static/media/registryIcon.aee7e216.svg"
+  },
+  {
+    "revision": "d1cc4fe780317a855dabe7cf22e42792",
+    "url": "static-files/static/media/expand-more.d1cc4fe7.svg"
+  },
+  {
+    "revision": "24a6085fcb8a5fd2977c376e13b22e30",
+    "url": "static-files/static/media/sync.24a6085f.svg"
+  },
+  {
+    "revision": "74bba15d71d39cdeae9e3edce246e3ac",
+    "url": "static-files/static/media/permission-denied-lock.74bba15d.svg"
+  },
+  {
+    "revision": "a1bbaadbf82282f9b9a44f18d932a893",
+    "url": "static-files/static/media/no-experiments.a1bbaadb.svg"
+  },
+  {
+    "revision": "489b3f42476612b4530794847d2cfac6",
+    "url": "static-files/static/media/index.489b3f42.less"
+  },
+  {
+    "revision": "237fe918a8f55117dd21c70b0a721630",
+    "url": "static-files/static/media/index.237fe918.less"
+  },
+  {
+    "revision": "1520f43432fc67cada28df5f9669ef5c",
+    "url": "static-files/static/media/index.1520f434.less"
+  },
+  {
+    "revision": "2dc5f250598fc910d2b30ac047bc392e",
+    "url": "static-files/static/media/index.2dc5f250.less"
+  },
+  {
+    "revision": "e5b8d0de6159c45d856debebc1f07013",
+    "url": "static-files/static/media/index.e5b8d0de.less"
+  },
+  {
+    "revision": "3d54e7cc46dc2b26f27dd000843234e7",
+    "url": "static-files/static/media/index.3d54e7cc.less"
+  },
+  {
+    "revision": "cba9708e267f91114e11f0688edbf558",
+    "url": "static-files/static/media/index.cba9708e.less"
+  },
+  {
+    "revision": "fb35abc2b74403df8bcef3e8130ea1cb",
+    "url": "static-files/static/media/index.fb35abc2.less"
+  },
+  {
+    "revision": "cbe7b1eba5c1e25c4db695a8ddd0cd82",
+    "url": "static-files/static/media/index.cbe7b1eb.less"
+  },
+  {
+    "revision": "76f4d39e672c5f1c45258e92b9aad14b",
+    "url": "static-files/static/media/index.76f4d39e.less"
+  },
+  {
+    "revision": "0a83a5703640fce5a156d024b0158335",
+    "url": "static-files/index.html"
+  }
+];
