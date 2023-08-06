c927fc8f959a05c02c443df40a042cc59a237900
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:07 +0000

    Add package evt73-distributions/1.3/evt73_distributions-1.3.tar.gz

---
+++ packages/evt73-distributions/evt73_distributions-1.3.tar.gz/evt73_distributions-1.3/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='evt73_distributions',
+      version='1.3',
+      description='Gaussian and Binomial distributions',
+      packages=['evt73_distributions'],
+      author="Everton Mendes",
+      author_email="evertonmendes73@usp.br",
+      zip_safe=False)
