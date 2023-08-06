24a98cd4f961a6f666c6e12b18a473c2e83bb1ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:53 +0000

    Add package code-inspector/0.0.17/code-inspector-0.0.17.tar.gz

---
+++ packages/code-inspector/code-inspector-0.0.17.tar.gz/code-inspector-0.0.17/code_inspector/constants.py
@@ -0,0 +1,6 @@
+GRAPHQL_ENDPOINT_URL = 'https://api.code-inspector.com/graphql'
+
+DEFAULT_TIMEOUT = 600  # 20 minutes
+VALID_SCM_KINDS = ["Bitbucket", "Git", "Github", "Gitlab"]
+
+BLANK_SHA = "0000000000000000000000000000000000000000"
