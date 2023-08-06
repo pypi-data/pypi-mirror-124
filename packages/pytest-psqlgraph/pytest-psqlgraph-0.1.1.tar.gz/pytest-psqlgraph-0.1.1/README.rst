9376e7d965a01a0c325ad685b41bcb5f0fe1e36b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:23:50 +0000

    Add package pytest-psqlgraph/0.1.1/pytest-psqlgraph-0.1.1.tar.gz

---
+++ packages/pytest-psqlgraph/pytest-psqlgraph-0.1.1.tar.gz/pytest-psqlgraph-0.1.1/README.rst
@@ -0,0 +1,67 @@
+================
+pytest-psqlgraph
+================
+
+|Pypi version| |ci| |Documentation Status| |Python versions|
+
+An extension for `pytest <https://pytest.org>`_ provide useful tools for testing applications that use `psqlgraph <https://github.com/NCI-GDC/psqlgraph>`_
+
+------------
+How to Start
+------------
+
+Install dependency ::
+
+    pip install pytest-psqlgraph
+
+Define a session scoped ``psqlgraph_config`` fixture in ``conftest.py``:
+
+.. code-block:: python
+
+    import pytest
+
+    @pytest.fixture(scope="session")
+    def psqlgraph_config() -> Dict[str, DatabaseDriverConfig]:
+        return {
+            "pg_driver": DatabaseDriverConfig(
+                host="localhost",
+                user="username",
+                password="pword",
+                database="db_name",
+                model=active_model,
+                dictionary=active_dictionary
+            )
+        }
+
+All set to start using the fixture ``pg_driver`` or whatever key was defined in the dictionary above
+
+.. code-block:: python
+
+    def test_something(pg_driver):
+        with pg_driver.session_scoped() as s:
+            s.add(Node(..))
+
+Contributing
+------------
+Don't hesitate to create a `Github issue <https://github.com/kulgan/pytest-psqlgraph/issues>`__ for any bugs or suggestions or submit a PR.
+
+
+.. |PyPI version| image:: https://img.shields.io/pypi/v/pytest-psqlgraph.svg
+   :target: https://pypi.python.org/pypi/pytest-psqlgraph
+   :alt: PyPi version
+
+.. |conda-forge version| image:: https://img.shields.io/conda/vn/conda-forge/pytest-psqlgraph.svg
+   :target: https://anaconda.org/conda-forge/pytest-psqlgraph
+   :alt: conda-forge version
+
+.. |ci| image:: https://github.com/kulgan/pytest-psqlgraph/workflows/ci/badge.svg
+   :target: https://github.com/kulgan/pytest-psqlgraph/actions
+   :alt: CI status
+
+.. |Python versions| image:: https://img.shields.io/pypi/pyversions/pytest-psqlgraph.svg
+   :target: https://pypi.org/project/pytest-psqlgraph
+   :alt: PyPi downloads
+
+.. |Documentation status| image:: https://readthedocs.org/projects/pytest-psqlgraph/badge/?version=latest
+   :target: https://pytest-psqlgraph.readthedocs.io/en/latest/?badge=latest
+   :alt: Documentation Status
---
+++ packages/pytest-psqlgraph/pytest-psqlgraph-0.1.1.tar.gz/pytest-psqlgraph-0.1.1/tests/models.py
@@ -0,0 +1,127 @@
+import pkg_resources
+import yaml
+from psqlgraph import Edge, Node, pg_property
+
+data_dir = pkg_resources.resource_filename("tests", "data/dictionary.yaml")
+
+with open(data_dir) as f:
+    dictionary = yaml.safe_load(f.read())
+
+
+class Dictionary:
+    schema = dictionary
+
+
+class PersonMixin:
+
+    _pg_edges = {}
+
+    @pg_property
+    def name(self, val):
+        self._set_property("name", val)
+
+
+class Father(Node, PersonMixin):
+
+    __label__ = "father"
+
+
+class Son(Node, PersonMixin):
+
+    __label__ = "son"
+
+
+class Mother(Node, PersonMixin):
+
+    __label__ = "mother"
+
+
+class Daughter(Node, PersonMixin):
+
+    __label__ = "daughter"
+
+
+class HusbandWifeEdge(Edge):
+
+    __src_class__ = "Father"
+    __dst_class__ = "Mother"
+    __src_dst_assoc__ = "wife"
+    __dst_src_assoc__ = "husband"
+
+
+class FatherSonEdge(Edge):
+
+    __src_class__ = "Father"
+    __dst_class__ = "Son"
+    __src_dst_assoc__ = "sons"
+    __dst_src_assoc__ = "father"
+
+
+class FatherDaughterEdge(Edge):
+    __src_class__ = "Father"
+    __dst_class__ = "Daughter"
+    __src_dst_assoc__ = "daughters"
+    __dst_src_assoc__ = "father"
+
+
+class MotherSonEdge(Edge):
+
+    __src_class__ = "Mother"
+    __dst_class__ = "Son"
+    __src_dst_assoc__ = "sons"
+    __dst_src_assoc__ = "mother"
+
+
+class MotherDaughterEdge(Edge):
+    __src_class__ = "Mother"
+    __dst_class__ = "Daughter"
+    __src_dst_assoc__ = "daughters"
+    __dst_src_assoc__ = "mother"
+
+
+Father._pg_edges.update(
+    {
+        "sons": {"type": Son, "backref": "father"},
+        "daugthers": {"type": Daughter, "backref": "father"},
+        "wife": {"type": Mother, "backref": "husband"},
+    }
+)
+
+
+Mother._pg_edges.update(
+    {
+        "sons": {"type": Son, "backref": "mother"},
+        "daughters": {"type": Daughter, "backref": "mother"},
+        "husband": {"type": Father, "backref": "wife"},
+    }
+)
+
+Son._pg_edges.update(
+    {
+        "father": {
+            "type": Father,
+            "backref": "sons",
+            "required": True,
+        },
+        "mother": {
+            "type": Mother,
+            "backref": "sons",
+            "required": True,
+        },
+    }
+)
+
+Daughter._pg_edges.update(
+    {
+        "father": {
+            "type": Father,
+            "backref": "daughters",
+            "required": True,
+        },
+        "mother": {
+            "type": Mother,
+            "backref": "daughters",
+            "required": True,
+        },
+    }
+)
