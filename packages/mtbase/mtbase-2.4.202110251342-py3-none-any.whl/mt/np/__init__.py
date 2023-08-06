de26759edadb7bb58a2bf8b666eacc4860cdb611
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:31:44 +0000

    Add package mtbase/2.4.202110251342/mtbase-2.4.202110251342-py3-none-any.whl

---
+++ packages/mtbase/mtbase-2.4.202110251342-py3-none-any.whl/mt/np/__init__.py
@@ -0,0 +1,12 @@
+'''Alias module. Instead of 'import numpy as np' you can do 'from mt import np'.'''
+
+from .matrix import *
+
+from numpy import *
+from numpy import __version__
+
+import numpy as _np
+
+for key in _np.__dict__:
+    globals()[key] = _np.__dict__[key]
+
