c38456303e4a571564000c0178b1faec4ed65963
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:30:08 +0000

    Add package hanzi/1.3/hanzi-1.3-py3-none-any.whl

---
+++ packages/hanzi/hanzi-1.3-py3-none-any.whl/hanzi/core.py
@@ -0,0 +1,31 @@
+import json
+import pkg_resources
+
+
+class Hanzi(object):
+    def __init__(self):
+        data_file = pkg_resources.resource_filename(__name__, "data/data.json")
+        self.data = json.load(open(data_file, "r", encoding="utf-8"))
+
+    def get_full_information(self, input_char):
+        if input_char in self.data:
+            bujian, pinyin, bihua = self.data[input_char]
+        else:
+            bujian, pinyin, bihua = None, None, None
+
+        return bujian, pinyin, bihua
+
+    def get_bujian(self, input_char):
+        bujian, pinyin, bihua = self.get_full_information(input_char)
+
+        return bujian
+
+    def get_pinyin(self, input_char):
+        bujian, pinyin, bihua = self.get_full_information(input_char)
+
+        return pinyin
+
+    def get_bihua(self, input_char):
+        bujian, pinyin, bihua = self.get_full_information(input_char)
+
+        return bihua
