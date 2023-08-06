beea55cc8b915ba11ee1a748874406bfc8e94c1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:48:07 +0000

    Add package distributions-udacity-akshayudayahegde/0.0.1/distributions-udacity-akshayudayahegde-0.0.1.tar.gz

---
+++ packages/distributions-udacity-akshayudayahegde/distributions-udacity-akshayudayahegde-0.0.1.tar.gz/distributions-udacity-akshayudayahegde-0.0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='distributions-udacity-akshayudayahegde',
+      version='0.0.1',
+      description='Gaussian and Binomial Distribution Basic Functions',
+      packages=['distributions'],
+      author="Akshay Udaya Hegde",
+      author_email="akshayudayhegde2@gmail.com",
+      classifiers=[
+          "Programming Language :: Python :: 3",
+          "License :: OSI Approved :: MIT License",
+          "Operating System :: OS Independent",
+      ],
+      zip_safe=False)
