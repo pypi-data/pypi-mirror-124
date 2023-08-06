c8fcd1ae421da63729a423674dc41b6a0be29d3e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:17 +0000

    Add package panaetius/2.1.0/panaetius-2.1.0-py3-none-any.whl

---
+++ packages/panaetius/panaetius-2.1.0-py3-none-any.whl/panaetius/exceptions.py
@@ -0,0 +1,13 @@
+"""Exceptions for the module."""
+
+
+class KeyErrorTooDeepException(Exception):
+    pass
+
+
+class LoggingDirectoryDoesNotExistException(Exception):
+    pass
+
+
+class InvalidPythonException(Exception):
+    pass
