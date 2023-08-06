b3544f976dae340fefca7f67c7a774b41050f49d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:23:54 +0000

    Add package MsTeamsConnector/0.1.0/MsTeamsConnector-0.1.0.tar.gz

---
+++ packages/MsTeamsConnector/MsTeamsConnector-0.1.0.tar.gz/MsTeamsConnector-0.1.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='MsTeamsConnector',
+    version='0.1.0',
+    packages=['MsTeamsConnector'],
+    url='https://pypi.org/project/MsTeamsConnector/',
+    license='MIT',
+    author='Kirill Kravchenko',
+    author_email='',
+    description='',
+    install_requires=['setuptools~=58.2.0','requests~=2.26.0', 'msal~=1.10.0', 'pydantic~=1.8.2']
+)
