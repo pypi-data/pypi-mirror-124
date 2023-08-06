6be1ebaa17b01169777203cbe9792f3f4d6b6bd5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:51:13 +0000

    Add package santolabs-logging-fastapi/0.0.1/santolabs_logging_fastapi-0.0.1-py3-none-any.whl

---
+++ packages/santolabs-logging-fastapi/santolabs_logging_fastapi-0.0.1-py3-none-any.whl/santolabs/logging/fastapi/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+
+"""SantoLabs Top-level package for Logging with Google Cloud Platform."""
+
+from __future__ import absolute_import
+from santolabs.logging.fastapi import *
