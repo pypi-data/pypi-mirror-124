379371ae370b332d18caceda767243c194ed805f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:21:50 +0000

    Add package satus/0.0.1/satus-0.0.1.tar.gz

---
+++ packages/satus/satus-0.0.1.tar.gz/satus-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='satus',
+    version='0.0.1',
+    description='',
+    long_description='',
+    url='https://www.example.com',
+    author='Taylor Hicks',
+    author_email='mail@taylorhicks.net',
+    packages=[],
+    classifiers=['Development Status :: 1 - Planning'],
+)
