437004e0fbec3e2b0d6ddc5aab79200cea92fe1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:29:42 +0000

    Add package miningpoolhub-py/1.0.8/miningpoolhub_py-1.0.8.tar.gz

---
+++ packages/miningpoolhub-py/miningpoolhub_py-1.0.8.tar.gz/miningpoolhub_py-1.0.8/miningpoolhub_py/__init__.py
@@ -0,0 +1,13 @@
+import os
+from dotenv import load_dotenv
+import pkg_resources
+from .exceptions import APIKeyMissingError
+
+load_dotenv()
+
+__version__ = pkg_resources.get_distribution("miningpoolhub_py").version
+__author__ = "Cory Krol"
+
+API_KEY = os.environ.get("MPH_API_KEY", None)
+
+from .miningpoolhubapi import MiningPoolHubAPI
