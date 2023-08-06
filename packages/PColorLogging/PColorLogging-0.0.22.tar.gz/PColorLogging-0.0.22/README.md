0f51d8514edcedd74b37d7374ae8a18c34af1df9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:24:26 +0000

    Add package PColorLogging/0.0.22/PColorLogging-0.0.22.tar.gz

---
+++ packages/PColorLogging/PColorLogging-0.0.22.tar.gz/PColorLogging-0.0.22/README.md
@@ -0,0 +1,38 @@
+<h1 align="center">PColorLogging</h1>
+<p align="center">
+  The powerful python logging, you can create colorful logging and easy to add logging level or record attribute
+</p>
+
+### Getting Start
+To create the logger
+```python
+import logging
+
+from PColorLogging import DEBUG, INFO, ERROR, WARNING, CRITICAL
+from PColorLogging.Drawer.color import PColor, TextMode
+from PColorLogging.Formatter.colored_formatter import ColoredFormatter
+from PColorLogging.Logger.Adapter.extra_adapter_logger import ExtraAdapterLogger
+
+colored_formatter = ColoredFormatter(f"[%(asctime)s] %(levelname)s:%(custom)s %(message)s", [
+    {"config": {"message": [PColor.BLUE]}, "level": [DEBUG]},
+    {"config": {"message": [PColor.BLUE], "custom": [PColor.B_WHITE]}, "level": [INFO]},
+    {"config": {"message": [PColor.CYAN, TextMode.UNDERLINE]}, "level": [ERROR]},
+    {"config": {"message": [PColor.GREEN, TextMode.SLOW_BLINK]}, "level": [WARNING]},
+    {"config": {"message": [PColor.WHITE, TextMode.FAST_BLINK]}, "level": [CRITICAL]},
+    {"config": {"levelname": [PColor.B_CYAN, PColor.WHITE]}, "level": [25]}
+])
+
+extra_logger = ExtraAdapterLogger("logger", {"custom": "1111"})
+
+console_handler = logging.StreamHandler()
+console_handler.setFormatter(colored_formatter)
+extra_logger.add_handler(console_handler)
+extra_logger.setLevel(logging.INFO)
+
+extra_logger.debug("this is debug", extra={"custom": 4321})
+extra_logger.info("this is info")
+extra_logger.warning("this is warning")
+extra_logger.error("this is error")
+extra_logger.critical("this is critical")
+extra_logger.log(25, "this is custom logging level")
+```
---
+++ packages/PColorLogging/PColorLogging-0.0.22.tar.gz/PColorLogging-0.0.22/src/PColorLogging/Drawer/color/__init__.py
@@ -0,0 +1,93 @@
+from PColorLogging.Drawer.color.color_code import _ColorModeCode, _TextModeCode, _ForegroundCode, _BackgroundCode
+
+
+class ColorMode:
+    RESET = "reset"
+    BOLD = "bold"
+    DARK = "dark"
+
+
+class TextMode:
+    ITALIC = "italic"
+    UNDERLINE = "underline"
+    SLOW_BLINK = "slow_blink"
+    FAST_BLINK = "fast_blink"
+    REVERSE = "reverse"
+    HIDE = "hide"
+    CROSS = "cross"
+
+
+class PColor:
+    BLACK = "black"
+    RED = "red"
+    GREEN = "green"
+    YELLOW = "yellow"
+    BLUE = "blue"
+    PURPLE = "purple"
+    CYAN = "cyan"
+    WHITE = "white"
+    B_BLACK = "b_black"
+    B_RED = "b_red"
+    B_GREEN = "b_green"
+    B_YELLOW = "b_yellow"
+    B_BLUE = "b_blue"
+    B_PURPLE = "b_purple"
+    B_CYAN = "b_cyan"
+    B_WHITE = "b_white"
+
+
+def get_color(key):
+    if key == ColorMode.RESET:
+        return _ColorModeCode.RESET
+    elif key == ColorMode.BOLD:
+        return _ColorModeCode.BOLD
+    elif key == ColorMode.DARK:
+        return _ColorModeCode.DARK
+    elif key == TextMode.ITALIC:
+        return _TextModeCode.ITALIC
+    elif key == TextMode.UNDERLINE:
+        return _TextModeCode.UNDERLINE
+    elif key == TextMode.SLOW_BLINK:
+        return _TextModeCode.SLOW_BLINK
+    elif key == TextMode.FAST_BLINK:
+        return _TextModeCode.FAST_BLINK
+    elif key == TextMode.REVERSE:
+        return _TextModeCode.REVERSE
+    elif key == TextMode.HIDE:
+        return _TextModeCode.HIDE
+    elif key == TextMode.CROSS:
+        return _TextModeCode.CROSS
+    elif key == PColor.BLACK:
+        return _ForegroundCode.BLACK
+    elif key == PColor.RED:
+        return _ForegroundCode.RED
+    elif key == PColor.GREEN:
+        return _ForegroundCode.GREEN
+    elif key == PColor.YELLOW:
+        return _ForegroundCode.YELLOW
+    elif key == PColor.BLUE:
+        return _ForegroundCode.BLUE
+    elif key == PColor.PURPLE:
+        return _ForegroundCode.PURPLE
+    elif key == PColor.CYAN:
+        return _ForegroundCode.CYAN
+    elif key == PColor.WHITE:
+        return _ForegroundCode.WHITE
+    elif key == PColor.B_BLACK:
+        return _BackgroundCode.BLACK
+    elif key == PColor.B_RED:
+        return _BackgroundCode.RED
+    elif key == PColor.B_GREEN:
+        return _BackgroundCode.GREEN
+    elif key == PColor.B_YELLOW:
+        return _BackgroundCode.YELLOW
+    elif key == PColor.B_BLUE:
+        return _BackgroundCode.BLUE
+    elif key == PColor.B_PURPLE:
+        return _BackgroundCode.PURPLE
+    elif key == PColor.B_CYAN:
+        return _BackgroundCode.CYAN
+    elif key == PColor.B_WHITE:
+        return _BackgroundCode.WHITE
+    else:
+        raise Exception
