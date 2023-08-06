94a69cc5eb6a33177148794c6b715f86c3b85566
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:36:31 +0000

    Add package pyrangelist/0.1.0/pyrangelist-0.1.0.tar.gz

---
+++ packages/pyrangelist/pyrangelist-0.1.0.tar.gz/pyrangelist-0.1.0/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pyrangelist",
+    version="0.1.0",
+    author="Sergiy Popovych",
+    author_email="sergiy.popovich@gmail.com",
+    description="Implementation of the RangeList datastructure in python. ",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    include_package_data=True,
+    package_data={"": ["*.py"]},
+    install_requires=["intervaltree"],
+    entry_points={},
+    extras_require={
+        'dev': [
+            'pytest',
+            'pytest-cov',
+            'astroid',
+            'isort',
+            'pylint',
+            'black',
+            'twine'
+        ]
+    },
+    packages=setuptools.find_packages(),
+    python_requires=">=3.8",
+)
