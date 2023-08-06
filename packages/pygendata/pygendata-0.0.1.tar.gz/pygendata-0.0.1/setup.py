3f7fd85f765c43b824f0fb167c56464a2b6ed24e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:01:09 +0000

    Add package pygendata/0.0.1/pygendata-0.0.1.tar.gz

---
+++ packages/pygendata/pygendata-0.0.1.tar.gz/pygendata-0.0.1/setup.py
@@ -0,0 +1,25 @@
+from distutils.core import setup
+
+setup(
+    name = 'pygendata',
+    packages = ['pygendata'],
+    version = '0.0.1',
+    license = 'MIT',
+    description = 'data generation library supports multiple input/output file types',
+    author = 'Lucas Padden',
+    author_email='lucaspadden@gmail.com',
+    url = 'https://github.com/Dynee/pygendata',
+    download_url = 'https://github.com/Dynee/pygendata/archive/refs/tags/v0.0.1.tar.gz',
+    keywords=['python', 'data generation'],
+    install_requires=[
+        'faker'
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8'
+    ]
+)
