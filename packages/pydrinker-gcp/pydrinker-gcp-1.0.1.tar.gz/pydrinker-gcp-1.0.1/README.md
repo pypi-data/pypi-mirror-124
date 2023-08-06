a4c46387a8124e79af392e4f70578df674d8eda2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:43 +0000

    Add package pydrinker-gcp/1.0.1/pydrinker-gcp-1.0.1.tar.gz

---
+++ packages/pydrinker-gcp/pydrinker-gcp-1.0.1.tar.gz/pydrinker-gcp-1.0.1/README.md
@@ -0,0 +1,6 @@
+# pydrinker-gcp
+Google Cloud Platform "plugin" for pydrinker 
+
+---
+
+Calm down your fingers! This project is under construction... wait for scenes from the next chapters :smile:
---
+++ packages/pydrinker-gcp/pydrinker-gcp-1.0.1.tar.gz/pydrinker-gcp-1.0.1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['pydrinker_gcp']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['google-cloud-pubsub>=2.8.0,<3.0.0', 'pydrinker>=1.0.0,<2.0.0']
+
+setup_kwargs = {
+    'name': 'pydrinker-gcp',
+    'version': '1.0.1',
+    'description': "Google Cloud Platform 'plugin' for pydrinker",
+    'long_description': '# pydrinker-gcp\nGoogle Cloud Platform "plugin" for pydrinker \n\n---\n\nCalm down your fingers! This project is under construction... wait for scenes from the next chapters :smile:\n',
+    'author': 'Rafael Henrique da Silva Correia',
+    'author_email': 'rafael@abraseucodigo.com.br',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/rafaelhenrique/pydrinker-gcp',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.9,<3.10',
+}
+
+
+setup(**setup_kwargs)
