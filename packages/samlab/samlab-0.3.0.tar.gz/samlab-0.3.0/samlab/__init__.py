8890fdffcd8392c5776d83ce070735d917a6cff6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:14:30 +0000

    Add package samlab/0.3.0/samlab-0.3.0.tar.gz

---
+++ packages/samlab/samlab-0.3.0.tar.gz/samlab-0.3.0/samlab/__init__.py
@@ -0,0 +1,28 @@
+# Copyright 2018, National Technology & Engineering Solutions of Sandia, LLC
+# (NTESS).  Under the terms of Contract DE-NA0003525 with NTESS, the U.S.
+# Government retains certain rights in this software.
+
+"""Tools for managing machine learning experiments."""
+
+__version__ = "0.3.0"
+
+import logging
+import warnings
+
+import arrow
+
+log = logging.getLogger(__name__)
+log.addHandler(logging.NullHandler())
+
+
+class DeprecationWarning(Warning):
+    pass
+
+
+def deprecated(message):
+    warnings.warn(message, DeprecationWarning, stacklevel=3) # pragma: no cover
+
+
+from threading import Timer
+
+
