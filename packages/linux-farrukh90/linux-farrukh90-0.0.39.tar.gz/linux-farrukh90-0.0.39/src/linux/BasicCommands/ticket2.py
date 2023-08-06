e23a46ba921733dcc96f4e47f6824471cc7a25ee
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:26:50 +0000

    Add package linux-farrukh90/0.0.39/linux-farrukh90-0.0.39.tar.gz

---
+++ packages/linux-farrukh90/linux-farrukh90-0.0.39.tar.gz/linux-farrukh90-0.0.39/src/linux/BasicCommands/ticket2.py
@@ -0,0 +1,18 @@
+import sys
+sys.path.append('../')
+from linux.shared_scripts.modules import filechecker, folderchecker, sizechecker, linechecker, linetotalchecker
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+def taskchecker():
+    filechecker("/tmp/fruits")
+    sizechecker("/tmp/services")
+    linechecker("/tmp/top10")
+    linechecker("/tmp/bottom10")
+    sizechecker("/tmp/sorted_fruits")
+    filechecker("/tmp/sorted_fruits")
+
+    sizechecker("/tmp/counted_services")
+    filechecker("/tmp/counted_services")
+    linetotalchecker("/tmp/counted_services", '1300')
+ # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # 
+ 
