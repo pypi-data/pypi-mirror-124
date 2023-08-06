78e188c16a5c4d198376a6c04cfc0e0db3493828
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:28:28 +0000

    Add package mypy-boto3-chime-sdk-identity/1.19.3/mypy-boto3-chime-sdk-identity-1.19.3.tar.gz

---
+++ packages/mypy-boto3-chime-sdk-identity/mypy-boto3-chime-sdk-identity-1.19.3.tar.gz/mypy-boto3-chime-sdk-identity-1.19.3/mypy_boto3_chime_sdk_identity/client.pyi
@@ -0,0 +1,238 @@
+"""
+Type annotations for chime-sdk-identity service client.
+
+[Open documentation](https://vemel.github.io/boto3_stubs_docs/mypy_boto3_chime_sdk_identity/client.html)
+
+Usage::
+
+    ```python
+    import boto3
+    from mypy_boto3_chime_sdk_identity import ChimeSDKIdentityClient
+
+    client: ChimeSDKIdentityClient = boto3.client("chime-sdk-identity")
+    ```
+"""
+from typing import Any, Dict, Mapping, Sequence, Type
+
+from botocore.client import BaseClient, ClientMeta
+
+from .type_defs import (
+    AppInstanceRetentionSettingsTypeDef,
+    CreateAppInstanceAdminResponseTypeDef,
+    CreateAppInstanceResponseTypeDef,
+    CreateAppInstanceUserResponseTypeDef,
+    DescribeAppInstanceAdminResponseTypeDef,
+    DescribeAppInstanceResponseTypeDef,
+    DescribeAppInstanceUserResponseTypeDef,
+    GetAppInstanceRetentionSettingsResponseTypeDef,
+    ListAppInstanceAdminsResponseTypeDef,
+    ListAppInstancesResponseTypeDef,
+    ListAppInstanceUsersResponseTypeDef,
+    PutAppInstanceRetentionSettingsResponseTypeDef,
+    TagTypeDef,
+    UpdateAppInstanceResponseTypeDef,
+    UpdateAppInstanceUserResponseTypeDef,
+)
+
+__all__ = ("ChimeSDKIdentityClient",)
+
+class BotocoreClientError(BaseException):
+    MSG_TEMPLATE: str
+    def __init__(self, error_response: Mapping[str, Any], operation_name: str) -> None:
+        self.response: Dict[str, Any]
+        self.operation_name: str
+
+class Exceptions:
+    BadRequestException: Type[BotocoreClientError]
+    ClientError: Type[BotocoreClientError]
+    ConflictException: Type[BotocoreClientError]
+    ForbiddenException: Type[BotocoreClientError]
+    ResourceLimitExceededException: Type[BotocoreClientError]
+    ServiceFailureException: Type[BotocoreClientError]
+    ServiceUnavailableException: Type[BotocoreClientError]
+    ThrottledClientException: Type[BotocoreClientError]
+    UnauthorizedClientException: Type[BotocoreClientError]
+
+class ChimeSDKIdentityClient(BaseClient):
+    """
+    [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/1.19.3/reference/services/chime-sdk-identity.html#ChimeSDKIdentity.Client)
+    [Show boto3-stubs documentation](https://vemel.github.io/boto3_stubs_docs/mypy_boto3_chime_sdk_identity/client.html)
+    """
+
+    meta: ClientMeta
+    @property
+    def exceptions(self) -> Exceptions:
+        """
+        ChimeSDKIdentityClient exceptions.
+        """
+    def can_paginate(self, operation_name: str) -> bool:
+        """
+        Check if an operation can be paginated.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/1.19.3/reference/services/chime-sdk-identity.html#ChimeSDKIdentity.Client.can_paginate)
+        [Show boto3-stubs documentation](https://vemel.github.io/boto3_stubs_docs/mypy_boto3_chime_sdk_identity/client.html#can_paginate)
+        """
+    def create_app_instance(
+        self,
+        *,
+        Name: str,
+        ClientRequestToken: str,
+        Metadata: str = ...,
+        Tags: Sequence["TagTypeDef"] = ...
+    ) -> CreateAppInstanceResponseTypeDef:
+        """
+        Creates an Amazon Chime SDK messaging `AppInstance` under an AWS account.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/1.19.3/reference/services/chime-sdk-identity.html#ChimeSDKIdentity.Client.create_app_instance)
+        [Show boto3-stubs documentation](https://vemel.github.io/boto3_stubs_docs/mypy_boto3_chime_sdk_identity/client.html#create_app_instance)
+        """
+    def create_app_instance_admin(
+        self, *, AppInstanceAdminArn: str, AppInstanceArn: str
+    ) -> CreateAppInstanceAdminResponseTypeDef:
+        """
+        Promotes an `AppInstanceUser` to an `AppInstanceAdmin`.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/1.19.3/reference/services/chime-sdk-identity.html#ChimeSDKIdentity.Client.create_app_instance_admin)
+        [Show boto3-stubs documentation](https://vemel.github.io/boto3_stubs_docs/mypy_boto3_chime_sdk_identity/client.html#create_app_instance_admin)
+        """
+    def create_app_instance_user(
+        self,
+        *,
+        AppInstanceArn: str,
+        AppInstanceUserId: str,
+        Name: str,
+        ClientRequestToken: str,
+        Metadata: str = ...,
+        Tags: Sequence["TagTypeDef"] = ...
+    ) -> CreateAppInstanceUserResponseTypeDef:
+        """
+        Creates a user under an Amazon Chime `AppInstance`.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/1.19.3/reference/services/chime-sdk-identity.html#ChimeSDKIdentity.Client.create_app_instance_user)
+        [Show boto3-stubs documentation](https://vemel.github.io/boto3_stubs_docs/mypy_boto3_chime_sdk_identity/client.html#create_app_instance_user)
+        """
+    def delete_app_instance(self, *, AppInstanceArn: str) -> None:
+        """
+        Deletes an `AppInstance` and all associated data asynchronously.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/1.19.3/reference/services/chime-sdk-identity.html#ChimeSDKIdentity.Client.delete_app_instance)
+        [Show boto3-stubs documentation](https://vemel.github.io/boto3_stubs_docs/mypy_boto3_chime_sdk_identity/client.html#delete_app_instance)
+        """
+    def delete_app_instance_admin(self, *, AppInstanceAdminArn: str, AppInstanceArn: str) -> None:
+        """
+        Demotes an `AppInstanceAdmin` to an `AppInstanceUser`.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/1.19.3/reference/services/chime-sdk-identity.html#ChimeSDKIdentity.Client.delete_app_instance_admin)
+        [Show boto3-stubs documentation](https://vemel.github.io/boto3_stubs_docs/mypy_boto3_chime_sdk_identity/client.html#delete_app_instance_admin)
+        """
+    def delete_app_instance_user(self, *, AppInstanceUserArn: str) -> None:
+        """
+        Deletes an `AppInstanceUser` .
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/1.19.3/reference/services/chime-sdk-identity.html#ChimeSDKIdentity.Client.delete_app_instance_user)
+        [Show boto3-stubs documentation](https://vemel.github.io/boto3_stubs_docs/mypy_boto3_chime_sdk_identity/client.html#delete_app_instance_user)
+        """
+    def describe_app_instance(self, *, AppInstanceArn: str) -> DescribeAppInstanceResponseTypeDef:
+        """
+        Returns the full details of an `AppInstance` .
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/1.19.3/reference/services/chime-sdk-identity.html#ChimeSDKIdentity.Client.describe_app_instance)
+        [Show boto3-stubs documentation](https://vemel.github.io/boto3_stubs_docs/mypy_boto3_chime_sdk_identity/client.html#describe_app_instance)
+        """
+    def describe_app_instance_admin(
+        self, *, AppInstanceAdminArn: str, AppInstanceArn: str
+    ) -> DescribeAppInstanceAdminResponseTypeDef:
+        """
+        Returns the full details of an `AppInstanceAdmin` .
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/1.19.3/reference/services/chime-sdk-identity.html#ChimeSDKIdentity.Client.describe_app_instance_admin)
+        [Show boto3-stubs documentation](https://vemel.github.io/boto3_stubs_docs/mypy_boto3_chime_sdk_identity/client.html#describe_app_instance_admin)
+        """
+    def describe_app_instance_user(
+        self, *, AppInstanceUserArn: str
+    ) -> DescribeAppInstanceUserResponseTypeDef:
+        """
+        Returns the full details of an `AppInstanceUser` .
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/1.19.3/reference/services/chime-sdk-identity.html#ChimeSDKIdentity.Client.describe_app_instance_user)
+        [Show boto3-stubs documentation](https://vemel.github.io/boto3_stubs_docs/mypy_boto3_chime_sdk_identity/client.html#describe_app_instance_user)
+        """
+    def generate_presigned_url(
+        self,
+        ClientMethod: str,
+        Params: Mapping[str, Any] = ...,
+        ExpiresIn: int = 3600,
+        HttpMethod: str = ...,
+    ) -> str:
+        """
+        Generate a presigned url given a client, its method, and arguments.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/1.19.3/reference/services/chime-sdk-identity.html#ChimeSDKIdentity.Client.generate_presigned_url)
+        [Show boto3-stubs documentation](https://vemel.github.io/boto3_stubs_docs/mypy_boto3_chime_sdk_identity/client.html#generate_presigned_url)
+        """
+    def get_app_instance_retention_settings(
+        self, *, AppInstanceArn: str
+    ) -> GetAppInstanceRetentionSettingsResponseTypeDef:
+        """
+        Gets the retention settings for an `AppInstance` .
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/1.19.3/reference/services/chime-sdk-identity.html#ChimeSDKIdentity.Client.get_app_instance_retention_settings)
+        [Show boto3-stubs documentation](https://vemel.github.io/boto3_stubs_docs/mypy_boto3_chime_sdk_identity/client.html#get_app_instance_retention_settings)
+        """
+    def list_app_instance_admins(
+        self, *, AppInstanceArn: str, MaxResults: int = ..., NextToken: str = ...
+    ) -> ListAppInstanceAdminsResponseTypeDef:
+        """
+        Returns a list of the administrators in the `AppInstance` .
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/1.19.3/reference/services/chime-sdk-identity.html#ChimeSDKIdentity.Client.list_app_instance_admins)
+        [Show boto3-stubs documentation](https://vemel.github.io/boto3_stubs_docs/mypy_boto3_chime_sdk_identity/client.html#list_app_instance_admins)
+        """
+    def list_app_instance_users(
+        self, *, AppInstanceArn: str, MaxResults: int = ..., NextToken: str = ...
+    ) -> ListAppInstanceUsersResponseTypeDef:
+        """
+        List all `AppInstanceUsers` created under a single `AppInstance` .
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/1.19.3/reference/services/chime-sdk-identity.html#ChimeSDKIdentity.Client.list_app_instance_users)
+        [Show boto3-stubs documentation](https://vemel.github.io/boto3_stubs_docs/mypy_boto3_chime_sdk_identity/client.html#list_app_instance_users)
+        """
+    def list_app_instances(
+        self, *, MaxResults: int = ..., NextToken: str = ...
+    ) -> ListAppInstancesResponseTypeDef:
+        """
+        Lists all Amazon Chime `AppInstance` s created under a single AWS account.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/1.19.3/reference/services/chime-sdk-identity.html#ChimeSDKIdentity.Client.list_app_instances)
+        [Show boto3-stubs documentation](https://vemel.github.io/boto3_stubs_docs/mypy_boto3_chime_sdk_identity/client.html#list_app_instances)
+        """
+    def put_app_instance_retention_settings(
+        self,
+        *,
+        AppInstanceArn: str,
+        AppInstanceRetentionSettings: "AppInstanceRetentionSettingsTypeDef"
+    ) -> PutAppInstanceRetentionSettingsResponseTypeDef:
+        """
+        Sets the amount of time in days that a given `AppInstance` retains data.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/1.19.3/reference/services/chime-sdk-identity.html#ChimeSDKIdentity.Client.put_app_instance_retention_settings)
+        [Show boto3-stubs documentation](https://vemel.github.io/boto3_stubs_docs/mypy_boto3_chime_sdk_identity/client.html#put_app_instance_retention_settings)
+        """
+    def update_app_instance(
+        self, *, AppInstanceArn: str, Name: str, Metadata: str
+    ) -> UpdateAppInstanceResponseTypeDef:
+        """
+        Updates `AppInstance` metadata.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/1.19.3/reference/services/chime-sdk-identity.html#ChimeSDKIdentity.Client.update_app_instance)
+        [Show boto3-stubs documentation](https://vemel.github.io/boto3_stubs_docs/mypy_boto3_chime_sdk_identity/client.html#update_app_instance)
+        """
+    def update_app_instance_user(
+        self, *, AppInstanceUserArn: str, Name: str, Metadata: str
+    ) -> UpdateAppInstanceUserResponseTypeDef:
+        """
+        Updates the details of an `AppInstanceUser`.
+
+        [Show boto3 documentation](https://boto3.amazonaws.com/v1/documentation/api/1.19.3/reference/services/chime-sdk-identity.html#ChimeSDKIdentity.Client.update_app_instance_user)
+        [Show boto3-stubs documentation](https://vemel.github.io/boto3_stubs_docs/mypy_boto3_chime_sdk_identity/client.html#update_app_instance_user)
+        """
