291ff2791a8350c0f94491e57f5965631e74a3dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:28:43 +0000

    Add package minio-driver/0.0.2/minio_driver-0.0.2.tar.gz

---
+++ packages/minio-driver/minio_driver-0.0.2.tar.gz/minio_driver-0.0.2/minio_driver/MinioProvider.py
@@ -0,0 +1,26 @@
+"""A MicroserviceProvider Service Provider."""
+
+from masonite.managers.UploadManager import UploadManager
+from masonite.provider import ServiceProvider
+from masonite.view import View
+
+from minio_driver.minio_driver import UploadMinioDriver
+from masonite.helpers import config
+from masonite.helpers.static import static
+from masonite import Upload
+
+
+class MicroserviceProvider(ServiceProvider):
+    """Provides Services To The Service Container."""
+
+    wsgi = False
+
+    def register(self):
+        """Register objects into the Service Container."""
+        self.app.bind('UploadMinioDriver', UploadMinioDriver)
+
+    def boot(self, manager: UploadManager, view: View):
+        """Boots services required by the container."""
+        self.app.bind("Upload", manager.driver(config("storage").DRIVER))
+        self.app.swap(Upload, manager.driver(config("storage").DRIVER))
+        view.share({"static": static})
---
+++ packages/minio-driver/minio_driver-0.0.2.tar.gz/minio_driver-0.0.2/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name="minio_driver",
+    version='0.0.2',
+    packages=['minio_driver'],
+    author="Yubaraj Shrestha",
+    author_email="companion.krish@outlook.com",
+    install_requires=[
+        'masonite',
+    ],
+    include_package_data=True,
+    keywords=["masonite", "storage", "minio", "masonite-storage-driver"]
+)
