a75b134060bbf7f10e5242c7c76c311796c7cbfe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:28:17 +0000

    Add package TayTules-pkg-taytek/0.0.9/TayTules_pkg_taytek-0.0.9-py3-none-any.whl

---
+++ packages/TayTules-pkg-taytek/TayTules_pkg_taytek-0.0.9-py3-none-any.whl/TayTools/configure.py
@@ -0,0 +1,11 @@
+import subprocess
+
+def boot(path):
+  subprocess.check_call(['mkdir','TayTulesInit/'])
+  f = open('TayTulesInit/boot.py','w')
+  f.write("import subprocess\npackage='TayTules-pkg-taytek'\nsubprocess.check_call(['pip','install',package])\nsubprocess.check_call(['python3','"+path+"'])")
+  f.close()
+  
+  f = open(".replit", 'w')
+  f.write("run= \"python3 TayTulesInit/boot.py\"")
+  f.close()
