223f3b16ef59ea165450079b1bfa3b2f4d5f73cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:56 +0000

    Add package PyTweet/1.1.1/PyTweet-1.1.1.tar.gz

---
+++ packages/PyTweet/PyTweet-1.1.1.tar.gz/PyTweet-1.1.1/main.py
@@ -0,0 +1,19 @@
+import os
+from twitter import Client
+
+    # def __lt__(self, other):
+    #     if not isinstance(other, PollOptions):
+    #         raise Exception(f"{other} is not from class PollOptions")
+    #     return self.position < other.position
+
+    # def __gt__(self, other):
+    #     if not isinstance(other, PollOptions):
+    #         raise Exception(f"{other} is not from class PollOptions")
+    #     return self.position > other.position
+
+client=Client(os.environ['bearer_token'])
+tweet=client.get_tweet(1450353845293383683)
+user=client.get_user_by_username("TheGenocides")
+
+print(tweet.poll.options[0] > tweet.poll.options[1])
+# print(tweet.original_payload)
