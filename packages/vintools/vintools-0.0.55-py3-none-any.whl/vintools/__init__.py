40ad005a7231a41e4b7ecb2a02662d77e6070552
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:31:19 +0000

    Add package vintools/0.0.55/vintools-0.0.55-py3-none-any.whl

---
+++ packages/vintools/vintools-0.0.55-py3-none-any.whl/vintools/__init__.py
@@ -0,0 +1,22 @@
+# main __init__.py
+
+__author__ = ", ".join(["Michael E. Vinyard"])
+__email__ = ", ".join(["vinyard@g.harvard.edu",])
+
+
+from . import _plotting as pl
+from . import _utilities as ut
+from . import _tools as tl
+from . import _single_cell as sc
+from . import _genome_editing as edit
+from . import _testing_and_development as dev
+from . import _data
+
+# matplotlib defaults
+import matplotlib
+import matplotlib.font_manager
+
+font = {"size": 12}
+matplotlib.rc("font", **font)
+matplotlib.rcParams["font.sans-serif"] = "Arial"
+matplotlib.rcParams["font.family"] = "sans-serif"
