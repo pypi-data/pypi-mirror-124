9279355222f14fc5f4405827660a8256fb166108
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:37:46 +0000

    Add package texttv/0.1/texttv-0.1.tar.gz

---
+++ packages/texttv/texttv-0.1.tar.gz/texttv-0.1/README.md
@@ -0,0 +1,10 @@
+# SVT Text TV terminal client
+texttv is a terminal client for SVT Text TV written in Python.
+
+# Installation
+
+`pip install texttv`
+
+# Usage
+`usage: texttv.py [-h] [-i] [page]`
+`page` need to be a valid SVT TextTV page. If not provided the page defaults to the index page 100 and starts in interactive mode.
---
+++ packages/texttv/texttv-0.1.tar.gz/texttv-0.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name='texttv',
+    version='0.1',    
+    description='A terminal SVT Text TV client',
+    url='https://github.com/erikstenlund/texttv',
+    author='Erik Stenlund',
+    author_email='erikstenlund@protonmail.com',
+    license='MIT',
+    install_requires=['readchar'],
+    packages=["texttv"],
+    entry_points={
+        "console_scripts": [
+            "texttv = texttv:cli"
+        ]
+    },
+)
