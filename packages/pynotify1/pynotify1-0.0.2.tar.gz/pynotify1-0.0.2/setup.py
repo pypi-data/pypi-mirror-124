23adaec5015152a2cf735819048e0fa5f543a7ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:05 +0000

    Add package pynotify1/0.0.2/pynotify1-0.0.2.tar.gz

---
+++ packages/pynotify1/pynotify1-0.0.2.tar.gz/pynotify1-0.0.2/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+setup(
+  name = 'pynotify1',       
+  packages = ['pynotify'],  
+  version = 'v0.0.2',      
+  license='Apache 2.0',       
+  description = 'convenient email notification, timing and logging of code execution',  
+  author = 'Tom1919',                   
+  author_email = 'py.notify1@gmail.com',      
+  url = 'https://github.com/tom1919',  
+  download_url = 'https://github.com/tom1919/PyNotify/archive/refs/tags/v0.0.1.tar.gz',   
+  keywords = ['email', 'logging', 'timimg', 'notification'],  
+  install_requires=[
+          'pandas',
+          'tabulate'
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Intended Audience :: Developers',     
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   
+    'Programming Language :: Python :: 3',     
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
