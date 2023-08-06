6599afa093103d048af7431b8883ea054fa0e008
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:36 +0000

    Add package jnobless-dsnd-distributions/0.1/jnobless_dsnd_distributions-0.1.tar.gz

---
+++ packages/jnobless-dsnd-distributions/jnobless_dsnd_distributions-0.1.tar.gz/jnobless_dsnd_distributions-0.1/README.md
@@ -0,0 +1,5 @@
+# jnobless_dsnd_distributions package
+
+Make Gaussian, binomial distributions and calculate the mean, standard deviation from data.
+
+# Installation
---
+++ packages/jnobless-dsnd-distributions/jnobless_dsnd_distributions-0.1.tar.gz/jnobless_dsnd_distributions-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='jnobless_dsnd_distributions',
+      version='0.1',
+      description='Gaussian, Binomial distributions',
+      packages=['distributions'],
+      author = 'June Ho Choi',
+      author_email = 'mylegna0308@gmail.com',
+      zip_safe=False)
