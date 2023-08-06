a95929a8d4c1e84896e49e75b7f5423322839ff1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:16:13 +0000

    Add package tweetscleaner/0.2/tweetscleaner-0.2.tar.gz

---
+++ packages/tweetscleaner/tweetscleaner-0.2.tar.gz/tweetscleaner-0.2/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='tweetscleaner',
+    version='0.2',
+    license='MIT',
+    long_description_content_type="text/x-rst",
+    author="Rizki Maulana",
+    author_email='rizkimaulana348@gmail.com',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    url='https://github.com/rizki4106/tweets_cleaner',
+    keywords='twitter tweets data data-science machine-learning data-trasformation data-preprocessing',
+)
