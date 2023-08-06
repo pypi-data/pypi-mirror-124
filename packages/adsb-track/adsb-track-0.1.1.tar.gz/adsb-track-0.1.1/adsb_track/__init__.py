b280a068c6e2958fc32a33a1c7a59218826d4d7a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:18 +0000

    Add package adsb-track/0.1.1/adsb-track-0.1.1.tar.gz

---
+++ packages/adsb-track/adsb-track-0.1.1.tar.gz/adsb-track-0.1.1/adsb_track/__init__.py
@@ -0,0 +1,2 @@
+from adsb_track.aircraft import Aircraft, Airspace
+from adsb_track.database import DBSQLite
---
+++ packages/adsb-track/adsb-track-0.1.1.tar.gz/adsb-track-0.1.1/adsb_track/const.py
@@ -0,0 +1,31 @@
+TIMESTAMP = 'timestamp'
+ICAO = 'icao'
+
+LAST_UPDATE = 'last_update'
+
+SESSION = 'session'
+SESSION_UUID = 'session_uuid'
+HOST = 'host'
+PORT = 'port'
+START = 'start'
+STOP = 'stop'
+DURATION = 'duration'
+
+IDENT = 'ident'
+CALLSIGN = 'callsign'
+TYPECODE = 'typecode'
+CATEGORY = 'category'
+
+VELOCITY = 'velocity'
+SPEED = 'speed'
+SPEED_TYPE = 'speed_type'
+VERTICAL_SPEED = 'vertical_speed'
+VERTICAL_SPEED_SRC = 'vertical_speed_src'
+ANGLE = 'angle'
+ANGLE_SRC = 'angle_src'
+
+POSITION = 'position'
+LATITUDE = 'latitude'
+LONGITUDE = 'longitude'
+ALTITUDE = 'altitude'
+ALTITUDE_SRC = 'altitude_src'
