3391d0f3bb5eb76aa4edfb788917181c73f3a835
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:49:14 +0000

    Add package space-rocks/1.3.3/space-rocks-1.3.3.tar.gz

---
+++ packages/space-rocks/space-rocks-1.3.3.tar.gz/space-rocks-1.3.3/rocks/__init__.py
@@ -0,0 +1,29 @@
+"""For space rocks."""
+import os
+import sys
+
+from . import datacloud, definitions, plots, ssodnet, utils
+
+# Expose API to user
+from .core import Rock
+from .core import rocks_ as rocks
+from .resolve import identify
+
+__version__ = "1.3.3"
+
+# Path to rocks auxilliary files
+PATH_CACHE = os.path.join(os.path.expanduser("~"), ".cache/rocks")
+
+PATH_META = {
+    "description": os.path.join(PATH_CACHE, "ssoCard_description.json"),
+    "template": os.path.join(PATH_CACHE, "ssoCard_template.json"),
+    "units": os.path.join(PATH_CACHE, "ssoCard_units.json"),
+}
+
+PATH_INDEX = os.path.join(PATH_CACHE, "index.pkl")
+
+# Check for existence of index file and cache directory
+os.makedirs(PATH_CACHE, exist_ok=True)
+
+if not os.path.isfile(PATH_INDEX):
+    utils.retrieve_index_from_repository()
