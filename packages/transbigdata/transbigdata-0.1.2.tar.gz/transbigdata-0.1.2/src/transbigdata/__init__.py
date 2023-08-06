35a5644469cb3194955425833ced7e9c9fbe7479
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:24:29 +0000

    Add package transbigdata/0.1.2/transbigdata-0.1.2.tar.gz

---
+++ packages/transbigdata/transbigdata-0.1.2.tar.gz/transbigdata-0.1.2/src/transbigdata/__init__.py
@@ -0,0 +1,8 @@
+
+from plot_map import plot_map,plotscale
+from .grids import *
+from .ckdnearest import *
+from .odprocess import *
+from .preprocess import *
+from .bikedata import *
+__version__ = '0.1.2'
