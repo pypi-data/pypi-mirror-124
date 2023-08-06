495041200bea5c3a603ebd2c409e4601253721aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:51 +0000

    Add package noteodps/0.0.2/noteodps-0.0.2-py3-none-any.whl

---
+++ packages/noteodps/noteodps-0.0.2-py3-none-any.whl/noteodps/__init__.py
@@ -0,0 +1,13 @@
+from notetool.secret import read_secret
+from odps import ODPS
+
+access_id = read_secret(cate1='DataWorks', cate2='nm', cate3='secret', cate4='access_id')
+secret_access_key = read_secret(cate1='DataWorks', cate2='nm', cate3='secret', cate4='access_key')
+project = read_secret(cate1='DataWorks', cate2='nm', cate3='secret', cate4='project')
+endpoint = read_secret(cate1='DataWorks', cate2='nm', cate3='secret', cate4='endpoint')
+
+opt = ODPS(access_id=access_id,
+           secret_access_key=secret_access_key,
+           project=project,
+           endpoint=endpoint)
+opt.to_global()
