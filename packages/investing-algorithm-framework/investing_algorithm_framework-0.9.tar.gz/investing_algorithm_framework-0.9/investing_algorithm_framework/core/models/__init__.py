dfa0b648f138734d4832db55599fba8fe728154a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:26:06 +0000

    Add package investing-algorithm-framework/0.9/investing_algorithm_framework-0.9.tar.gz

---
+++ packages/investing-algorithm-framework/investing_algorithm_framework-0.9.tar.gz/investing_algorithm_framework-0.9/investing_algorithm_framework/core/models/__init__.py
@@ -0,0 +1,34 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+def create_all_tables():
+    db.create_all()
+
+
+def initialize_db(app: Flask):
+    db.init_app(app)
+    db.app = app
+
+from investing_algorithm_framework.core.models.order_status import OrderStatus
+from investing_algorithm_framework.core.models.order_type import OrderType
+from investing_algorithm_framework.core.models.order_side import OrderSide
+from investing_algorithm_framework.core.models.time_unit import TimeUnit
+from investing_algorithm_framework.core.models.order import Order
+from investing_algorithm_framework.core.models.portfolio import Portfolio
+from investing_algorithm_framework.core.models.position import Position
+
+__all__ = [
+    "db",
+    "Portfolio",
+    "Position",
+    'Order',
+    "OrderType",
+    'OrderSide',
+    "TimeUnit",
+    "create_all_tables",
+    "initialize_db",
+    "OrderStatus"
+]
