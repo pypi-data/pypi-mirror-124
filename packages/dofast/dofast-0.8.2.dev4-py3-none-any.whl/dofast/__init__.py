15c138c3fec2f0576d7b42663d8a2c0df418cce9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:38:25 +0000

    Add package dofast/0.8.2.dev4/dofast-0.8.2.dev4-py3-none-any.whl

---
+++ packages/dofast/dofast-0.8.2.dev4-py3-none-any.whl/dofast/__init__.py
@@ -0,0 +1,21 @@
+from contextlib import contextmanager
+
+from dofast.oss import Bucket
+from dofast.pyavatar import PyAvataaar
+from dofast.toolkits.qredis import QRedis
+from dofast.toolkits.textparser import TextParser
+from dofast.utils import DeeplAPI
+from dofast.weather import Weather
+
+
+class AppInterfaces(object):
+    def __init__(self):
+        self.weather = Weather()
+        self.textparser = TextParser
+        self.deepl = DeeplAPI()
+        self.avatar = PyAvataaar()
+        self.redis = QRedis()
+        self.bucket = Bucket()
+
+
+api = AppInterfaces()
