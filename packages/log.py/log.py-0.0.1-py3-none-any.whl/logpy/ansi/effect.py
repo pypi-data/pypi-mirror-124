2091c19eda8996528c08bfef00ea1946ec4f7544
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:14:09 +0000

    Add package log.py/0.0.1/log.py-0.0.1-py3-none-any.whl

---
+++ packages/log.py/log.py-0.0.1-py3-none-any.whl/logpy/ansi/effect.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+# ||Classes||
+########################################################################################################################
+class Effect:
+    """
+    A collection of ANSI effects in the Python format.\n
+    """
+    #region Variables
+    reset = "\033[0m"
+    bold = "\033[01m"
+    disable = "\033[02m"
+    underline = "\033[04m"
+    reverse = "\033[07m"
+    strikethrough = "\033[09m"
+    invisible = "\033[08m"
+    #endregion
