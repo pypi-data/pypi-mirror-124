13b5c367c9bbfd4c8aa984c58624d9508bf5e232
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:42:48 +0000

    Add package kingdom-sdk/0.3.0.dev1/kingdom_sdk-0.3.0.dev1-py3-none-any.whl

---
+++ packages/kingdom-sdk/kingdom_sdk-0.3.0.dev1-py3-none-any.whl/kingdom_sdk/config.py
@@ -0,0 +1,27 @@
+import os
+
+from kingdom_sdk.utils import casting
+
+try:
+    from dotenv import load_dotenv
+
+    load_dotenv()
+except:  # noqa
+    pass
+
+DEFAULT_TIMEZONE_REGION = "America/Sao_Paulo"
+DEFAULT_DEBUG = "False"
+DEFAULT_DATABASE_URL = "postgresql://user:password@localhost:5432/databse"
+
+
+def get_timezone_region() -> str:
+    return os.environ.get("TIMEZONE_REGION", DEFAULT_TIMEZONE_REGION)
+
+
+def is_debug_active() -> bool:
+    value = os.environ.get("DEBUG", DEFAULT_DEBUG)
+    return casting.bool_from_string(value)
+
+
+def get_database_url() -> str:
+    return os.environ.get("DATABASE_URL", DEFAULT_DATABASE_URL)
