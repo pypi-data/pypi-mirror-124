61fbb810b21f316c7100f1365095be25b87218f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:48:17 +0000

    Add package pytest-testrail-api-client/0.2/pytest_testrail_api_client-0.2-py3-none-any.whl

---
+++ packages/pytest-testrail-api-client/pytest_testrail_api_client-0.2-py3-none-any.whl/pytest_testrail_api_client/client_config.py
@@ -0,0 +1,28 @@
+TR_PREFIX = '@C'
+SECTIONS_SEPARATOR = ' - '
+
+MAIN_CASE_TEMPLATE_NAME = 'Test Case (Steps)'
+PRIORITY_REPLACE = {
+    'Critical': ['regression'],
+    'High': ['sanity'],
+    'Medium': ['smoke'],
+    'Low': []
+}
+
+REPLACE_TAGS = {
+    'to be automated': 'to_automate',
+    'productivity': 'tablet',
+    'mobile': 'phone'
+}
+
+# ------Validate features--------
+
+VALIDATE_FEATURES = True
+NO_TAG_IN_FEATURE_HEADER = True
+ONE_OF_TAGS = [
+    ['@to_automate', '@automated', '@manual'],
+    ['@smoke', '@critical', '@regression']
+]
+AT_LEAST_ONE = [
+    ['@phone', '@tablet']
+]
