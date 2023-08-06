7d96ac16ed9d28649f844b7d071c2b40dfe78aa3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:26:57 +0000

    Add package kekpack/0.1.0/kekpack-0.1.0.tar.gz

---
+++ packages/kekpack/kekpack-0.1.0.tar.gz/kekpack-0.1.0/kekpack/kek.py
@@ -0,0 +1,2 @@
+def print_str(s):
+    print(s)
---
+++ packages/kekpack/kekpack-0.1.0.tar.gz/kekpack-0.1.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='kekpack',
+    version='0.1.0',    
+    description='A example Python package',
+    url='https://github.com/shuds13/pyexample',
+    author='Emil Garipov',
+    author_email='test@test.test',
+    license='BSD 2-clause',
+    packages=['kekpack'],
+    install_requires=['numpy==1.12.0']
+)
