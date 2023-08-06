0db5c07fc7535fa63d93001b216aa3181b9175e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:06:36 +0000

    Add package dj-auto-multitenant/0.0.2/dj_auto_multitenant-0.0.2-py3-none-any.whl

---
+++ packages/dj-auto-multitenant/dj_auto_multitenant-0.0.2-py3-none-any.whl/customers/apps.py
@@ -0,0 +1,10 @@
+from django.apps import AppConfig
+
+
+class CustomersConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'customers'
+
+    def ready(self):
+        import customers.signals
+        import customers.consumer
