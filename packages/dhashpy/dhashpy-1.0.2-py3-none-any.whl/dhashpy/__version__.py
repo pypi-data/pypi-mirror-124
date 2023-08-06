f92122e4cc59c261a599db06f905b4eda05e89a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:09:06 +0000

    Add package dhashpy/1.0.2/dhashpy-1.0.2-py3-none-any.whl

---
+++ packages/dhashpy/dhashpy-1.0.2-py3-none-any.whl/dhashpy/__version__.py
@@ -0,0 +1,10 @@
+__title__ = "dhashpy"
+__description__ = (
+    "Row-wise gradient dHash (difference hash) algorithm implementation in python."
+)
+__url__ = "https://akamhy.github.io/dhashpy/"
+__version__ = "1.0.2"
+__author__ = "Akash Mahanty"
+__author_email__ = "akamhy@yahoo.com"
+__license__ = "MIT"
+__copyright__ = "Copyright 2021 Akash Mahanty"
