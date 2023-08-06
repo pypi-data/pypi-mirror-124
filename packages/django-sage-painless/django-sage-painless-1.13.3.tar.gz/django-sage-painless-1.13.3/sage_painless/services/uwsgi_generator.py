448cece04691889286a08cc47278f0b64c09acc2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:27:36 +0000

    Add package django-sage-painless/1.13.3/django-sage-painless-1.13.3.tar.gz

---
+++ packages/django-sage-painless/django-sage-painless-1.13.3.tar.gz/django-sage-painless-1.13.3/sage_painless/services/uwsgi_generator.py
@@ -0,0 +1,53 @@
+import os
+import time
+
+from django.conf import settings
+
+# Base
+from sage_painless.services.abstract import AbstractUWSGIGenerator
+
+# Helpers
+from sage_painless.utils.git_service import GitSupport
+from sage_painless.utils.jinja_service import JinjaHandler
+from sage_painless.utils.json_service import JsonHandler
+from sage_painless.utils.timing_service import TimingService
+
+from sage_painless import templates
+
+class UwsgiGenerator(AbstractUWSGIGenerator, JinjaHandler, JsonHandler, TimingService, GitSupport):
+    """generate uwsgi config"""
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def generate(self, diagram_path, git_support=False):
+        """generate uwsgi.ini
+        template:
+            sage_painless/templates/uwsgi.txt
+        """
+        start_time = time.time()
+
+        diagram = self.load_json(diagram_path)
+
+        config = self.extract_uwsgi_config(diagram)  # get uwsgi config from diagram
+
+        if git_support:
+            self.init_repo(settings.BASE_DIR)
+
+        # generate uwsgi.ini
+        self.stream_to_template(
+            output_path=f'{settings.BASE_DIR}/uwsgi.ini',
+            template_path=os.path.abspath(templates.__file__).replace('__init__.py', 'uwsgi.txt'),
+            data={
+                'config': config
+            }
+        )
+
+        if git_support:
+            self.commit_file(
+                f'{settings.BASE_DIR}/uwsgi.ini',
+                f'deploy (uwsgi): Create uwsgi config file'
+            )
+
+        end_time = time.time()
+        return True, 'uwsgi config generated ({:.3f} ms)'.format(self.calculate_execute_time(start_time, end_time))
---
+++ packages/django-sage-painless/django-sage-painless-1.13.3.tar.gz/django-sage-painless-1.13.3/sage_painless/utils/pep8_service.py
@@ -0,0 +1,11 @@
+import autopep8
+
+class Pep8:
+    def fix_pep8(self, file_path):
+        """
+        fix pep8 (E122, E303, E305, W292, W391)
+        """
+        options = autopep8.parse_args(
+            ['--in-place', '--aggressive', '--select', 'E122,E271,E261,E225,E303,E305,E501,W292,W391', file_path]
+        )
+        autopep8.fix_file(filename=file_path, options=options)
