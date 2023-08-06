420a61d94662d2a9df1d07a2e0d4a3997554f9b3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:23:13 +0000

    Add package adb-tools/1.0.7/adb_tools-1.0.7-py3-none-any.whl

---
+++ packages/adb-tools/adb_tools-1.0.7-py3-none-any.whl/adb_tools/logcat_traces.py
@@ -0,0 +1,34 @@
+import os
+import subprocess
+from time import sleep
+
+
+def get_android_crash_info(serialno, log_path):
+    try:
+        cmds = "adb -s %s shell logcat -b crash > %s" % (serialno, log_path)
+        proc = subprocess.Popen(
+            cmds,
+            shell=True,
+        )
+        sleep(5)
+        proc.terminate()
+    finally:
+        print('获取crash日志成功')
+
+
+def get_anr_info(serialno, log_path):
+    os.system("adb -s %s bugreport %s" % (serialno, log_path))
+
+
+def clear_device_logcat_info(serialno):
+    os.system("adb -s %s shell logcat -c" % serialno)
+
+
+def get_app_info(serialno, package_name, log_path):
+    os.system("adb -s %s shell logcat *:W | grep -i %s > %s" % (serialno, package_name, log_path))
+
+
+if __name__ == "__main__":
+    clear_device_logcat_info('52dc62d9')
+    get_anr_info('52dc62d9', '/Users/zhuhuiping/Desktop/脚本/bugreport103')
+    get_android_crash_info('52dc62d9', '/Users/zhuhuiping/Desktop/脚本/crash103.log')
