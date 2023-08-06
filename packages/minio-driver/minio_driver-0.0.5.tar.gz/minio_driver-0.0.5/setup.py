7d401a58d404c8540ef6820052b707c3ae64c4dd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:29:50 +0000

    Add package minio-driver/0.0.5/minio_driver-0.0.5.tar.gz

---
+++ packages/minio-driver/minio_driver-0.0.5.tar.gz/minio_driver-0.0.5/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name="minio_driver",
+    version='0.0.5',
+    packages=['minio_driver'],
+    author="Yubaraj Shrestha",
+    author_email="companion.krish@outlook.com",
+    install_requires=[
+        'masonite',
+    ],
+    license="MIT",
+    include_package_data=True,
+    keywords=["masonite", "storage", "minio", "masonite-storage-driver"]
+)
