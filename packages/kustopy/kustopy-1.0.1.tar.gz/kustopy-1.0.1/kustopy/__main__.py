9181eb46d17e701e7b1b42aae13a2cffad61a9aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:14:29 +0000

    Add package kustopy/1.0.1/kustopy-1.0.1.tar.gz

---
+++ packages/kustopy/kustopy-1.0.1.tar.gz/kustopy-1.0.1/kustopy/__main__.py
@@ -0,0 +1,29 @@
+from configparser import ConfigParser
+from importlib import resources
+import sys
+
+from kustopy import feed
+from kustopy import viewer
+
+
+def main():
+    """Read the Real Python article feed"""
+    # Read URL of the Real Python feed from config file
+    cfg = ConfigParser()
+    cfg.read_string(resources.read_text("kustopy", "config.txt"))
+    url = cfg.get("feed", "url")
+
+    # If an article ID is given, show the article
+    if len(sys.argv) > 1:
+        article = feed.get_article(url, sys.argv[1])
+        viewer.show(article)
+
+    # If no ID is given, show a list of all articles
+    else:
+        site = feed.get_site(url)
+        titles = feed.get_titles(url)
+        viewer.show_list(site, titles)
+
+
+if __name__ == "__main__":
+    main()
