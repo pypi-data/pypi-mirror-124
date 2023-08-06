5b9d7d09e2ce4157335ad888be2348521f6f8f4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:28:53 +0000

    Add package mutwo/0.32.0/mutwo-0.32.0.tar.gz

---
+++ packages/mutwo/mutwo-0.32.0.tar.gz/mutwo-0.32.0/mutwo/converters/frontends/__init__.py
@@ -0,0 +1,39 @@
+"""Convert mutwo objects to external data."""
+
+from mutwo.utilities import tools
+
+from . import csound_constants
+from . import csound
+from . import ekmelily_constants
+from . import ekmelily
+from . import isis_constants
+from . import isis
+
+# import modules with extra require
+for modules, dependencies in (
+    (("midi_constants", "midi"), ("mido",)),
+    (("reaper",), ("rpp",)),
+    (
+        (
+            "abjad_attachments",
+            "abjad_process_container_routines",
+            "abjad_constants",
+            "abjad",
+        ),
+        ("abjad", "abjadext.nauert", "abjadext.rmakers"),
+    ),
+    # for future
+    # (
+    #     (
+    #         "abjad_video_constants",
+    #         "abjad_video",
+    #     ),
+    #     ("abjad", "PIL"),
+    # ),
+):
+    for module in modules:
+        tools.import_module_if_dependencies_have_been_installed(
+            "mutwo.converters.frontends.{}".format(module), dependencies
+        )
+
+del module, dependencies, tools
