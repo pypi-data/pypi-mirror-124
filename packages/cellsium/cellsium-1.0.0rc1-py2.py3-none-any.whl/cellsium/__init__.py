05082475a0442a51a775273177132f9970682d3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:37:53 +0000

    Add package cellsium/1.0.0rc1/cellsium-1.0.0rc1-py2.py3-none-any.whl

---
+++ packages/cellsium/cellsium-1.0.0rc1-py2.py3-none-any.whl/cellsium/__init__.py
@@ -0,0 +1,13 @@
+""" CellSium - _Cell_ _Si_mulator for _micro_fluidic _m_icrocolonies """
+
+__project__ = 'CellSium'
+
+__version__ = '1.0.0rc1'
+
+__author__ = 'Christian C. Sachs'
+
+__copyright__ = '2021, Christian C. Sachs, Forschungszentrum Jülich GmbH'
+
+__citation__ = ''
+
+__banner__ = ''
