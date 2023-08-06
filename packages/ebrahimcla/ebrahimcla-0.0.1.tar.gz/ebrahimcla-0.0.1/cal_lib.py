aa236c562bb2c6af0cb20c0a01303277184b70ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:37:03 +0000

    Add package ebrahimcla/0.0.1/ebrahimcla-0.0.1.tar.gz

---
+++ packages/ebrahimcla/ebrahimcla-0.0.1.tar.gz/ebrahimcla-0.0.1/cal_lib.py
@@ -0,0 +1,11 @@
+def add_num(no1 , no2):
+    return no1 + no2
+
+def sub_num(no1 , no2):
+    return no1 - no2
+
+def mul_num(no1 , no2):
+    return no1 * no2
+
+def div_num(no1 , no2):
+    return no1 / no2
