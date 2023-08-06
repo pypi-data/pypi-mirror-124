946790e856344a9a9c8d86caa8e0e279debd4c77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:38:42 +0000

    Add package switch-api/0.2.0/switch_api-0.2.0-py3-none-any.whl

---
+++ packages/switch-api/switch_api-0.2.0-py3-none-any.whl/switch_api/authentication/authority_config.py
@@ -0,0 +1,6 @@
+# -------------------------------------------------------------------------
+# Copyright (c) Switch Automation Pty Ltd. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for
+# license information.
+# --------------------------------------------------------------------------
+
---
+++ packages/switch-api/switch_api-0.2.0-py3-none-any.whl/switch_api/authentication/msal/__init__.py
@@ -0,0 +1,12 @@
+# -------------------------------------------------------------------------
+# Copyright (c) Switch Automation Pty Ltd. All rights reserved.
+# Licensed under the MIT License. See License.txt in the project root for
+# license information.
+# --------------------------------------------------------------------------
+"""
+MSAL PublicClientApplication extension
+"""
+
+__all__ = ['CustomPublicClientApplication']
+
+from .custom_application import CustomPublicClientApplication
