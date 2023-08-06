259743e8d27d31f4db97b333b82b4c35940ec673
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:14:32 +0000

    Add package proglearn/0.0.6/proglearn-0.0.6-py3-none-any.whl

---
+++ packages/proglearn/proglearn-0.0.6-py3-none-any.whl/proglearn/__init__.py
@@ -0,0 +1,9 @@
+from .forest import *
+from .network import *
+
+__version__ = "0.0.6"
+__all__ = [
+    "LifelongClassificationForest",
+    "LifelongClassificationNetwork",
+    "UncertaintyForest",
+]
