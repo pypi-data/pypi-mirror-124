ee38b3cd33c4228d03a2399a785ed47ef1216b10
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:29:00 +0000

    Add package eezotop-hist-new/0.2.0/eezotop_hist_new-0.2.0.tar.gz

---
+++ packages/eezotop-hist-new/eezotop_hist_new-0.2.0.tar.gz/eezotop_hist_new-0.2.0/eezotop_hist_new/__init__.py
@@ -0,0 +1,8 @@
+"""
+eezotop_hist_new.
+
+My hist for lab.
+"""
+
+__version__ = "0.2.0"
+__author__ = "Yegor Zudin"
---
+++ packages/eezotop-hist-new/eezotop_hist_new-0.2.0.tar.gz/eezotop_hist_new-0.2.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="eezotop_hist_new",
+    version="0.2.0",
+    description="Dummy module for doing lab.",
+    author="Yegor Zudin",
+    author_email="zudin.0197@gmail.com",
+    packages=["eezotop_hist_new"],
+    install_requires=["numpy>=1.21", "matplotlib>=3.4"],
+    setup_requires=["numpy>=1.21", "matplotlib>=3.4"]
+)
