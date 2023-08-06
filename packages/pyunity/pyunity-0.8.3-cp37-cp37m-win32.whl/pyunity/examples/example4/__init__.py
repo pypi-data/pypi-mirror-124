3444c1303322cd456053646ddca9f5d32aba3368
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:40:43 +0000

    Add package pyunity/0.8.3/pyunity-0.8.3-cp37-cp37m-win32.whl

---
+++ packages/pyunity/pyunity-0.8.3-cp37-cp37m-win32.whl/pyunity/examples/example4/__init__.py
@@ -0,0 +1,31 @@
+from pyunity import *
+
+class LookAt(Behaviour):
+    other = ShowInInspector(GameObject)
+    def Update(self, dt):
+        self.transform.LookAtGameObject(self.other)
+
+class Mover(Behaviour):
+    speed = ShowInInspector(float, 6)
+    def Update(self, dt):
+        self.transform.localPosition += Vector3(self.speed * dt, 0, 0)
+
+def main():
+    scene = SceneManager.AddScene("Scene")
+    scene.mainCamera.transform.position = Vector3(0, 3, -10)
+    lookAt = scene.mainCamera.AddComponent(LookAt)
+
+    cube = GameObject("Cube")
+    renderer = cube.AddComponent(MeshRenderer)
+    renderer.mat = Material(RGB(0, 255, 0))
+    renderer.mesh = Loader.Primitives.cube
+    cube.transform.position = Vector3(-20, 0, 0)
+    cube.AddComponent(Mover).speed = 6
+    scene.Add(cube)
+
+    lookAt.other = cube
+
+    SceneManager.LoadScene(scene)
+
+if __name__ == "__main__":
+    main()
