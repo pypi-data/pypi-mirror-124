0903907c66367ea180621d933c1ad18265ccf4da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:20:52 +0000

    Add package argparse-dataclasses/0.1.4/argparse_dataclasses-0.1.4.tar.gz

---
+++ packages/argparse-dataclasses/argparse_dataclasses-0.1.4.tar.gz/argparse_dataclasses-0.1.4/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+from os.path import join, dirname
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='argparse_dataclasses',
+    version='0.1.4',
+    packages=find_packages(),
+    url="https://github.com/Felix-neko/argparse_dataclasses",
+    author="Felix Neko",
+    author_email="felix-neko@list.ru",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+)
