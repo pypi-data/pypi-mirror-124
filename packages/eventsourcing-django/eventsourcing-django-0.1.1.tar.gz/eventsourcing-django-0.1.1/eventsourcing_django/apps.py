0802adaae2401af1ad5fcad8e74a02d33c382ddb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:55 +0000

    Add package eventsourcing-django/0.1.1/eventsourcing-django-0.1.1.tar.gz

---
+++ packages/eventsourcing-django/eventsourcing-django-0.1.1.tar.gz/eventsourcing-django-0.1.1/eventsourcing_django/apps.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+from django.apps import AppConfig
+
+
+class EventsourcingConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "eventsourcing_django"
