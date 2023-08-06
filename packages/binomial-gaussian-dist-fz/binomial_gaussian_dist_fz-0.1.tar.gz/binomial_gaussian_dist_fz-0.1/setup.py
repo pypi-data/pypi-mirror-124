a93de19e7651606d10197e551e6020f6679dbb75
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:43:24 +0000

    Add package binomial-gaussian-dist-fz/0.1/binomial_gaussian_dist_fz-0.1.tar.gz

---
+++ packages/binomial-gaussian-dist-fz/binomial_gaussian_dist_fz-0.1.tar.gz/binomial_gaussian_dist_fz-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='binomial_gaussian_dist_fz',
+      version='0.1',
+      description='Gaussian and Binomial distribution',
+      packages=['binomial_gaussian_dist_fz'],
+      zip_safe=False)
