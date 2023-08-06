bcb8876e3b61dbbb82c4d9e2c5800c63546c10ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:34:51 +0000

    Add package PColorLogging/0.0.31/PColorLogging-0.0.31-py3-none-any.whl

---
+++ packages/PColorLogging/PColorLogging-0.0.31-py3-none-any.whl/PColorLogging/Formatter/colored_formatter.py
@@ -0,0 +1,17 @@
+import logging
+
+from PColorLogging.Drawer import Drawer
+
+
+class ColoredFormatter(logging.Formatter):
+    def __init__(self, ftm, config=None, date_fmt='%m-%d %H:%M:%S', use_color=True):
+        logging.Formatter.__init__(self, ftm, datefmt=date_fmt)
+        self.drawer = Drawer(ftm, config)
+        self.use_color = use_color
+
+    def format(self, record):
+        level_name = record.levelname
+
+        if self.use_color:
+            self._style._fmt = self.drawer.get_message(level_name)
+        return logging.Formatter.format(self, record)
