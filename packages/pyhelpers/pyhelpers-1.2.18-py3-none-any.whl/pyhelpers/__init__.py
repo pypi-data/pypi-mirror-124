ade6f82cd3cc2d79031c8ad6bcf8daf9a47c0322
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:24:48 +0000

    Add package pyhelpers/1.2.18/pyhelpers-1.2.18-py3-none-any.whl

---
+++ packages/pyhelpers/pyhelpers-1.2.18-py3-none-any.whl/pyhelpers/__init__.py
@@ -0,0 +1,26 @@
+# noinspection PyUnresolvedReferences
+from .dir import *
+# noinspection PyUnresolvedReferences
+from .geom import *
+# noinspection PyUnresolvedReferences
+from .ops import *
+# noinspection PyUnresolvedReferences
+from .settings import *
+# noinspection PyUnresolvedReferences
+from .sql import *
+# noinspection PyUnresolvedReferences
+from .store import *
+# noinspection PyUnresolvedReferences
+from .text import *
+
+__project__ = 'PyHelpers'
+__package__ = 'pyhelpers'
+__description__ = f"An open-source toolkit for facilitating Python users' data manipulation tasks."
+
+__author__ = 'Qian Fu'
+__affiliation__ = 'School of Engineering, University of Birmingham'
+__email__ = 'q.fu@bham.ac.uk'
+
+__copyright__ = f'2019-{datetime.datetime.now().year}, {__author__}'
+
+__version__ = '1.2.18'
