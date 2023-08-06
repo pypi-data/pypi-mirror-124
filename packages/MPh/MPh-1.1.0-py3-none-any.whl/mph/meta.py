821168b65fd91ab2ac417e2236c551c036f3262a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:10:24 +0000

    Add package MPh/1.1.0/MPh-1.1.0-py3-none-any.whl

---
+++ packages/MPh/MPh-1.1.0-py3-none-any.whl/mph/meta.py
@@ -0,0 +1,9 @@
+﻿"""Meta information about the package."""
+
+title     = 'MPh'
+synopsis  = 'Pythonic scripting interface for Comsol Multiphysics'
+version   = '1.1.0'
+date      = '2021–10–20'
+author    = 'John Hennig'
+copyright = '2020, John Hennig'
+license   = 'MIT'
