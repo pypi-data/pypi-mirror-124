4235ec150e8e5050fb56cf2f9cf4aef54e59b64f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:32 +0000

    Add package TurtLSystems/0.0.11/TurtLSystems-0.0.11.tar.gz

---
+++ packages/TurtLSystems/TurtLSystems-0.0.11.tar.gz/TurtLSystems-0.0.11/src/TurtLSystems/__init__.py
@@ -0,0 +1,5 @@
+"""The turtlsystems Python package (https://pypi.org/project/turtlsystems) is a tool and educational
+code toy for generating images and animations of Lindenmayer system (L-system) patterns via turtle graphics."""
+
+from TurtLSystems.core import init, draw, wait, lsystem
+__all__ = ['init', 'draw', 'wait', 'lsystem']
