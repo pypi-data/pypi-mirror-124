a06a104a6d886babe7725392536c5c6862c44e38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:39:58 +0000

    Add package InstagramIG/0.0.1/InstagramIG-0.0.1.tar.gz

---
+++ packages/InstagramIG/InstagramIG-0.0.1.tar.gz/InstagramIG-0.0.1/setup.py
@@ -0,0 +1,48 @@
+import os , codecs
+
+from setuptools import setup, find_packages
+
+pkg = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(pkg, "README.md"), encoding="utf-8") as fh:
+	
+    long_description = "\n" + fh.read()
+
+
+setup(
+
+    name="InstagramIG",
+    
+    version="0.0.1",
+    
+    author="SidraELEzz",
+    
+    author_email="",
+    
+    description = ("Scrape Instagram Profile and login Instagram"),
+    
+    long_description_content_type="text/markdown",
+    
+    long_description=long_description,
+    
+    packages=find_packages(),
+    
+    install_requires=['requests'],
+    
+    keywords=['instagram'],
+    
+    classifiers=[
+    
+        "Development Status :: 1 - Planning",
+        
+        "Intended Audience :: Developers",
+        
+        "Programming Language :: Python :: 3",
+        
+        "Operating System :: Unix",
+        
+        "Operating System :: MacOS :: MacOS X",
+        
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
