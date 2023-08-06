584715766db492e28a8176b6c24863370d840136
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:31 +0000

    Add package simple-file-ops/0.1.3/simple_file_ops-0.1.3-py3-none-any.whl

---
+++ packages/simple-file-ops/simple_file_ops-0.1.3-py3-none-any.whl/fileops/__init__.py
@@ -0,0 +1,10 @@
+"""
+Simple package to do file operations easier
+Read https://pypi.org/project/simple-file-ops/ for more info
+"""
+
+from .jsonops import *
+from .textfiles import *
+
+__version__ = "0.1.3"
+__author__ = "Cube Riser"
