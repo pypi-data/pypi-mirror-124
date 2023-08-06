b630099087eeb8d537dd153602a4e8059ee2aa76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:36:49 +0000

    Add package tedpy/0.1.1/tedpy-0.1.1.tar.gz

---
+++ packages/tedpy/tedpy-0.1.1.tar.gz/tedpy-0.1.1/setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['tedpy']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['httpx>=0.20.0,<0.21.0', 'xmltodict>=0.12.0,<0.13.0']
+
+setup_kwargs = {
+    'name': 'tedpy',
+    'version': '0.1.1',
+    'description': 'Unofficial library for reading from The Energy Detective power meters',
+    'long_description': None,
+    'author': 'rianadon',
+    'author_email': None,
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/rianadon/the-energy-detective-py',
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.5,<4.0',
+}
+
+
+setup(**setup_kwargs)
