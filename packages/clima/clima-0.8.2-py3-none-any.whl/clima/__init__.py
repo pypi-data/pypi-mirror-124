3c0563870d3e2bbf0aa0e642111299286a6eae68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:44:35 +0000

    Add package clima/0.8.2/clima-0.8.2-py3-none-any.whl

---
+++ packages/clima/clima-0.8.2-py3-none-any.whl/clima/__init__.py
@@ -0,0 +1,6 @@
+"""Simple boilerplate for cli scripts"""
+
+from clima import fire
+from clima.core import c, Schema
+from clima.helputils import print_help, HelpString
+from clima.utils import suppress_traceback
