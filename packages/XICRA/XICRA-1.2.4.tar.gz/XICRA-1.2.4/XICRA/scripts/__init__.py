bdda27d69bac769104a0c7dc9dfe200c2821f114
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:14:19 +0000

    Add package XICRA/1.2.4/XICRA-1.2.4.tar.gz

---
+++ packages/XICRA/XICRA-1.2.4.tar.gz/XICRA-1.2.4/XICRA/scripts/__init__.py
@@ -0,0 +1,15 @@
+__all__ = [
+    'fastqc_caller',
+    'multiQC_report',
+    'generate_DE',
+    'RNAbiotype',
+    'mapReads',
+    'cutadapt_caller',
+    'mirtop_caller',
+    'optimir_caller',
+    'miraligner_caller',
+    'MINTMap_caller'
+    
+]
+
+from XICRA.scripts import *
