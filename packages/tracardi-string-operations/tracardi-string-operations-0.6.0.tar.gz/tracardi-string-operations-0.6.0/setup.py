a528828dc98a2e435779a91110d1d7f91d1a2303
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:43 +0000

    Add package tracardi-string-operations/0.6.0/tracardi-string-operations-0.6.0.tar.gz

---
+++ packages/tracardi-string-operations/tracardi-string-operations-0.6.0.tar.gz/tracardi-string-operations-0.6.0/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='tracardi-string-operations',
+    version='0.6.0',
+    description='This plug-in is to make a string operations like: lowercase remove spaces split and other',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='Patryk Migaj',
+    packages=['tracardi_string_operations'],
+    install_requires=[
+        'tracardi-plugin-sdk>=0.6.22'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ],
+    keywords=['tracardi', 'plugin'],
+    include_package_data=True,
+    python_requires=">=3.8",
+)
