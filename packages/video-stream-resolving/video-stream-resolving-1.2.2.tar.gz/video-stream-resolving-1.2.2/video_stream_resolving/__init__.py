bd24c5b45e2198b4311b9d56774bd8a98fb3c16e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:38:18 +0000

    Add package video-stream-resolving/1.2.2/video-stream-resolving-1.2.2.tar.gz

---
+++ packages/video-stream-resolving/video-stream-resolving-1.2.2.tar.gz/video-stream-resolving-1.2.2/video_stream_resolving/__init__.py
@@ -0,0 +1,4 @@
+from .streamtape import resolve as resolve_streamtape
+from .cinemaemas import resolve as resolve_cinemaemas
+from .fa_efek_stream import resolve as resolve_fa_efek_stream
+from .jeniusplay_com import resolve as resolve_jeniusplay_com
