002df8f115137236211a0fd741c963edfb590204
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:40 +0000

    Add package compliantdynamodb/0.0.8/compliantdynamodb-0.0.8-py3-none-any.whl

---
+++ packages/compliantdynamodb/compliantdynamodb-0.0.8-py3-none-any.whl/compliant_dynamodb/_jsii/__init__.py
@@ -0,0 +1,24 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+import aws_cdk.aws_backup._jsii
+import aws_cdk.aws_dynamodb._jsii
+import aws_cdk.core._jsii
+import constructs._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "compliantdynamodb", "0.0.8", __name__[0:-6], "compliantdynamodb@0.0.8.jsii.tgz"
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
