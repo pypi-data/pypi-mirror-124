463fe5c235efd454419da713fa9b97d5a9138e97
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:05 +0000

    Add package satlas2/0.0.1a0/satlas2-0.0.1a0.tar.gz

---
+++ packages/satlas2/satlas2-0.0.1a0.tar.gz/satlas2-0.0.1a0/src/satlas2/__init__.py
@@ -0,0 +1,14 @@
+from .core import *
+from . import core
+
+from satlas2.models import *
+import satlas2.models
+
+from .overwrite import *
+from . import overwrite
+
+from .plotting import *
+from . import plotting
+
+from . import _version
+__version__ = _version.version
