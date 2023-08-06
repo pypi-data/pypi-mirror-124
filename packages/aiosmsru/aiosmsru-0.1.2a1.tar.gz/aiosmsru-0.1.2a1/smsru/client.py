50ae7514aa4d198003da6f6bd43977e9132a6fc1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:29:22 +0000

    Add package aiosmsru/0.1.2a1/aiosmsru-0.1.2a1.tar.gz

---
+++ packages/aiosmsru/aiosmsru-0.1.2a1.tar.gz/aiosmsru-0.1.2a1/smsru/client.py
@@ -0,0 +1,216 @@
+from typing import Optional, Union
+import sys
+
+from smsru.models import SMSruWithBalance, SMSruBase
+from smsru.models.limits import SMSruLimit, SMSruFreeLimit
+from smsru.models.senders import SMSruSenders
+from smsru.models.stoplist import SMSruStoplist
+
+if sys.version_info >= (3, 9):
+    from collections.abc import Sequence
+else:
+    from typing import Sequence
+from httpx import Client, Response
+
+from smsru.models.sms import (
+    SMSruSendSmsResponse,
+    SMSruCheckSmsResponse,
+    SMSruSmsCostResponse,
+)
+
+
+class SMSru:
+    def __init__(self, api_id: str, domain: str = "sms.ru"):
+        self._api_id = api_id
+        self._domain = domain
+        self._client: Optional[Client] = None
+
+    @property
+    def client(self) -> Client:
+        if not self._client:
+            self._client = Client()
+        return self._client
+
+    def _request(self, endpoint: str, params: dict = None) -> Response:
+        if not params:
+            params = {}
+        params.update({"api_id": self._api_id, "json": 1, "partner_id": 331687})
+        response = self.client.get(f"https://{self._domain}/{endpoint}", params=params)
+        return response
+
+    def send_sms(
+        self, recipients: Union[str, Sequence[str]], messages: Union[str, Sequence[str]], **kwargs
+    ) -> SMSruSendSmsResponse:
+        params = {}
+        if (  # More than one recipient
+            isinstance(recipients, Sequence)
+            and not isinstance(recipients, str)
+            and len(recipients) > 1
+        ):
+            if (  # More than one message
+                isinstance(messages, Sequence)
+                and not isinstance(messages, str)
+                and len(messages) > 1
+            ):
+                if len(recipients) == len(messages):
+                    for i, recipient in enumerate(recipients):
+                        params[f"to[{recipient}]"] = messages[i]
+                else:
+                    raise ValueError(
+                        "Sequences of recipients and messages must be of the same length"
+                    )
+            elif (  # One message for all recipients
+                isinstance(messages, str)
+                or isinstance(messages, Sequence)
+                and len(messages) == 1
+            ):
+                if isinstance(messages, Sequence) and not isinstance(messages, str):
+                    messages = messages[0]
+                params["to"] = ",".join(recipients)
+                params["msg"] = messages
+            else:
+                raise ValueError(
+                    "Messages must be str (message) or sequence of str (messages)"
+                )
+        elif (  # Only one recipient
+            isinstance(recipients, str)
+            or isinstance(recipients, Sequence)
+            and len(recipients) == 1
+        ):
+            if (
+                isinstance(messages, str)
+                or isinstance(messages, Sequence)
+                and len(messages) == 1
+            ):
+                if isinstance(messages, Sequence) and not isinstance(messages, str):
+                    messages = messages[0]
+                params["to"] = recipients
+                params["msg"] = messages
+            else:
+                raise ValueError(
+                    "If there is only one recipient, then there can be no more than one message"
+                )
+        else:
+            raise ValueError(
+                "Recipients must be str (phone number) or sequence of str (phone numbers)"
+            )
+
+        params.update(kwargs)
+
+        response = self._request("sms/send", params)
+
+        return SMSruSendSmsResponse(**response.json())
+
+    def check_sms(self, ids: Union[str, Sequence[str]]) -> SMSruCheckSmsResponse:
+        if isinstance(ids, str):
+            ids = [ids]
+        if not isinstance(ids, Sequence):
+            raise ValueError("ids must be str or sequence of str")
+
+        params = {"sms_id": ",".join(ids)}
+
+        response = self._request("sms/status", params)
+
+        return SMSruCheckSmsResponse(**response.json())
+
+    def sms_cost(
+        self, recipients: Union[str, Sequence[str]], messages: Union[str, Sequence[str]], **kwargs
+    ) -> SMSruSmsCostResponse:
+        params = {}
+        if (  # More than one recipient
+            isinstance(recipients, Sequence)
+            and not isinstance(recipients, str)
+            and len(recipients) > 1
+        ):
+            if (  # More than one message
+                isinstance(messages, Sequence)
+                and not isinstance(messages, str)
+                and len(messages) > 1
+            ):
+                if len(recipients) == len(messages):
+                    for i, recipient in enumerate(recipients):
+                        params[f"to[{recipient}]"] = messages[i]
+                else:
+                    raise ValueError(
+                        "Sequences of recipients and messages must be of the same length"
+                    )
+            elif (  # One message for all recipients
+                isinstance(messages, str)
+                or isinstance(messages, Sequence)
+                and len(messages) == 1
+            ):
+                if isinstance(messages, Sequence) and not isinstance(messages, str):
+                    messages = messages[0]
+                params["to"] = ",".join(recipients)
+                params["msg"] = messages
+            else:
+                raise ValueError(
+                    "Messages must be str (message) or sequence of str (messages)"
+                )
+        elif (  # Only one recipient
+            isinstance(recipients, str)
+            or isinstance(recipients, Sequence)
+            and len(recipients) == 1
+        ):
+            if (
+                isinstance(messages, str)
+                or isinstance(messages, Sequence)
+                and len(messages) == 1
+            ):
+                if isinstance(messages, Sequence) and not isinstance(messages, str):
+                    messages = messages[0]
+                params["to"] = recipients
+                params["msg"] = messages
+            else:
+                raise ValueError(
+                    "If there is only one recipient, then there can be no more than one message"
+                )
+        else:
+            raise ValueError(
+                "Recipients must be str (phone number) or sequence of str (phone numbers)"
+            )
+
+        params.update(kwargs)
+
+        response = self._request("sms/cost", params)
+
+        return SMSruSmsCostResponse(**response.json())
+
+    def balance(self):
+        response = self._request("my/balance")
+
+        return SMSruWithBalance(**response.json())
+
+    def limit(self):
+        response = self._request("my/limit")
+
+        return SMSruLimit(**response.json())
+
+    def free_limit(self):
+        response = self._request("my/free")
+
+        return SMSruFreeLimit(**response.json())
+
+    def senders(self):
+        response = self._request("my/senders")
+
+        return SMSruSenders(**response.json())
+
+    def check_auth(self, api_id: str = None, login: str = None, password: str = None):
+        if api_id:
+            params = {"api_id": api_id}
+        elif login and password:
+            params = {"login": login, "password": password}
+        else:
+            raise ValueError("You must provide api_id or login and password")
+
+        response = self.client.get(
+            f"https://{self._domain}/auth/check", params=params
+        )
+
+        return SMSruBase(**response.json())
+
+    def stoplist(self):
+        response = self._request("stoplist/get")
+
+        return SMSruStoplist(**response.json())
