56ee43fc51f75ab01be19bd897da3990188c0063
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:39:49 +0000

    Add package notecoin/0.9.24/notecoin-0.9.24-py3-none-any.whl

---
+++ packages/notecoin/notecoin-0.9.24-py3-none-any.whl/notecoin/huobi/history/_download_trades.py
@@ -0,0 +1,50 @@
+from notecoin.huobi.history.const import *
+from notecoin.huobi.history.download import *
+from notecoin.huobi.history.utils import *
+
+data_type = 'trades'
+
+
+def download_daily_spot(start: datetime = None, end: datetime = None, all_symbols: list = None):
+    '''return date is: [start, end)'''
+
+    global data_type
+    b_download_daily_spot(data_type, ['trades'], start, end, all_symbols)
+
+
+def download_daily_future(start: datetime = None, end: datetime = None, all_symbols: list = None):
+    '''return date is: [start, end)'''
+
+    global data_type
+    b_download_daily_future(data_type, ['trades'], start, end, all_symbols)
+
+
+def download_daily_swap(start: datetime = None, end: datetime = None, all_symbols: list = None):
+    '''return date is: [start, end)'''
+
+    global data_type
+    b_download_daily_swap(data_type, ['trades'], start, end, all_symbols)
+
+
+def download_daily_linearswap(start: datetime = None, end: datetime = None, all_symbols: list = None):
+    '''return date is: [start, end)'''
+
+    global data_type
+    b_download_daily_linearswap(data_type, ['trades'], start, end, all_symbols)
+
+
+def download_daily_option(start: datetime = None, end: datetime = None, all_symbols: list = None):
+    '''return date is: [start, end)'''
+
+    global data_type
+    b_download_daily_option(data_type, ['trades'], start, end, all_symbols)
+
+
+if __name__ == "__main__":
+    download_daily_spot(all_symbols=['btcusdt', 'ltcusdt'],
+                        start=datetime(2021, 1, 1),
+                        end=datetime(2021, 2, 1))
+    # download_daily_future()
+    # download_daily_swap()
+    # download_daily_linearswap()
+    # download_daily_option()
