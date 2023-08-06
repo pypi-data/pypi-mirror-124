a323a0ce1e2e86eb9ede9a945d3324aac3013927
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:44 +0000

    Add package bfit/4.16.0/bfit-4.16.0.tar.gz

---
+++ packages/bfit/bfit-4.16.0.tar.gz/bfit-4.16.0/bfit/global_variables.py
@@ -0,0 +1,25 @@
+# Define some global variables needed for setup
+# Derek Fujimoto
+# Aug 2021
+
+from os.path import join
+
+# variables
+__src__ = join('bfit', 'fitting')
+__version__ = '4.16.0'
+
+# keywords used to identify variables
+KEYVARS = { 'B0'    : 'B0 Field (T)', 
+            'BIAS'  : 'Platform Bias (kV)', 
+            'CLFT'  : 'Cryo Lift Read (mm)', 
+            'DUR'   : 'Run Duration (s)', 
+            'ENRG'  : 'Impl. Energy (keV)', 
+            'LAS'   : 'Laser Power (V)', 
+            'NBMR'  : 'NBM Rate (count/s)', 
+            'RATE'  : 'Sample Rate (count/s)', 
+            'RF'    : 'RF Level DAC', 
+            'RUN'   : 'Run Number', 
+            'TEMP'  : 'Temperature (K)', 
+            'TIME'  : 'Start Time', 
+            'YEAR'  : 'Year', 
+          }    
