9f62c213ec91f49c4a89192a56fdd2b0fc122c94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:25:56 +0000

    Add package ansible-gen/0.3.2/ansible-gen-0.3.2.tar.gz

---
+++ packages/ansible-gen/ansible-gen-0.3.2.tar.gz/ansible-gen-0.3.2/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name='ansible-gen',
+    version='0.3.2',
+    packages=['ansible_gen', 'ansible_gen.adapter', 'ansible_gen.adapter.utils', 'ansible_gen.adapter.utils.xml_parse',
+              'ansible_gen.adapter.utils.yang_parse', 'ansible_gen.generator'],
+    include_package_data=True,
+    entry_points={
+        'console_scripts': [
+          'ansible-gen=ansible_gen.ansible_gen:main',
+        ],
+    },
+    url='https://github.com/HuaweiDatacomm/ansible-gen',
+    license='Apache License, Version 2.0',
+    author='frank feng',
+    author_email='frank.fengchong@huawei.com',
+    description='a tool can generate ansible api according yang modules and some input xml files'
+)
