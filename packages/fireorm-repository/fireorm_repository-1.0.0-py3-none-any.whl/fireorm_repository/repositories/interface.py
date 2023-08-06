55eca53f25cb2f0c7e05632dcf371e5d12c6fa55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:06:19 +0000

    Add package fireorm-repository/1.0.0/fireorm_repository-1.0.0-py3-none-any.whl

---
+++ packages/fireorm-repository/fireorm_repository-1.0.0-py3-none-any.whl/fireorm_repository/repositories/interface.py
@@ -0,0 +1,24 @@
+from fireorm.Queries import Query
+from interface import Interface
+
+from fireorm_repository.types.types import T
+
+
+class InterfaceBaseRepository(Interface):
+    def execute_query(
+            self,
+            query: Query,
+    ) -> list[T]:
+        pass
+
+    def get_by_field(
+            self,
+            field: str,
+            operator: str,
+            value: str | int | bool | list,
+            query: Query = None
+    ) -> Query:
+        pass
+
+    def update(self, item: T) -> T:
+        pass
