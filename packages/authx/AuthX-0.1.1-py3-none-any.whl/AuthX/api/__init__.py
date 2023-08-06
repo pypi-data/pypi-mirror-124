059c11cb9e27b9d9e65d44c982b788d590fd8512
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:14:07 +0000

    Add package authx/0.1.1/AuthX-0.1.1-py3-none-any.whl

---
+++ packages/authx/AuthX-0.1.1-py3-none-any.whl/AuthX/api/__init__.py
@@ -0,0 +1,6 @@
+from AuthX.api.users import UsersRepo
+
+"""
+    Get The API Repositories
+"""
+__all__ = ["UsersRepo"]
---
+++ packages/authx/AuthX-0.1.1-py3-none-any.whl/AuthX/services/__init__.py
@@ -0,0 +1,17 @@
+from AuthX.services.admin import AdminService
+from AuthX.services.auth import AuthService
+from AuthX.services.password import PasswordService
+from AuthX.services.search import SearchService
+from AuthX.services.social import SocialService
+
+"""
+    Initialize all services
+"""
+
+__all__ = [
+    "AdminService",
+    "AuthService",
+    "PasswordService",
+    "SearchService",
+    "SocialService",
+]
