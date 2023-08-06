620871a1af5dd096a3f449c230f5d62a196102f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:07:10 +0000

    Add package vancouver.py/0.0.6/vancouver.py-0.0.6.tar.gz

---
+++ packages/vancouver.py/vancouver.py-0.0.6.tar.gz/vancouver.py-0.0.6/vancouver/__init__.py
@@ -0,0 +1,8 @@
+from .main import *
+
+__version__ = "0.0.6"
+
+__all__ = (
+	"File", "Init"
+)
+
---
+++ packages/vancouver.py/vancouver.py-0.0.6.tar.gz/vancouver.py-0.0.6/vancouver/__main__.py
@@ -0,0 +1,9 @@
+from .main import Init
+
+
+if __name__ == "__main__":
+    print("Приветствуем, просьба ввести токен от imgbb")
+
+    Init()
+
+    exit(0)
