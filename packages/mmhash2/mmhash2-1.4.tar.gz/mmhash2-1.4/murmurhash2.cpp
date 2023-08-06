10c864cbd04c47601fdab5db11e658ead1335ac2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:39:54 +0000

    Add package mmhash2/1.4/mmhash2-1.4.tar.gz

---
+++ packages/mmhash2/mmhash2-1.4.tar.gz/mmhash2-1.4/murmurhash2.cpp
@@ -0,0 +1,80 @@
+#include "murmurhash2.hpp"
+
+#define PY_SSIZE_T_CLEAN
+#include <Python.h>
+
+static PyObject* murmurhash64a(PyObject* self, PyObject* args) {
+    char* input;
+    size_t len;
+    unsigned int seed;
+    if (!PyArg_ParseTuple(args, "z#I", &input, &len, &seed)) {
+        return NULL;
+    }
+
+    uint64_t h = MurmurHash64A(input, len, seed);
+
+    return PyLong_FromUnsignedLong(h);
+}
+
+static PyObject* murmurhash64b(PyObject* self, PyObject* args) {
+    char* input;
+    size_t len;
+    unsigned int seed;
+    if (!PyArg_ParseTuple(args, "z#I", &input, &len, &seed)) {
+        return NULL;
+    }
+
+    uint64_t h = MurmurHash64B(input, len, seed);
+
+    return PyLong_FromUnsignedLong(h);
+}
+
+/**
+ * 10进制转16进制.
+ */
+static PyObject* dechex(PyObject* self, PyObject* args) {
+    uint64_t from;
+    if (!PyArg_ParseTuple(args, "k", &from)) {
+        return NULL;
+    }
+
+    std::string buff = dechex(from);
+
+    return Py_BuildValue("s", buff.c_str());
+}
+
+/**
+ * 10进制转62进制
+ */
+static PyObject* base62encode(PyObject* self, PyObject* args) {
+    uint64_t from;
+    if (!PyArg_ParseTuple(args, "k", &from)) {
+        return NULL;
+    }
+
+    std::string buff = base62(from);
+
+    return Py_BuildValue("s", buff.c_str());
+}
+
+static PyMethodDef methods[] = {
+    {"murmurhash64a",(PyCFunction)murmurhash64a,METH_VARARGS,NULL},
+    {"murmurhash64b",(PyCFunction)murmurhash64b,METH_VARARGS,NULL},
+    {"dechex",(PyCFunction)dechex,METH_VARARGS,NULL},
+    {"base62encode",(PyCFunction)base62encode,METH_VARARGS,NULL},
+    {NULL,NULL,0,NULL}
+};
+
+static struct PyModuleDef mmhash2 =
+{
+    PyModuleDef_HEAD_INIT,
+    "mmhash2", /* name of module */
+    "usage: mmhash2.murmurhash64a(input, seed))\n", /* module documentation, may be NULL */
+    -1, /* size of per-interpreter state of the module, or -1 if the module keeps state in global variables. */
+    methods
+};
+
+PyMODINIT_FUNC PyInit_mmhash2(void)
+{
+    return PyModule_Create(&mmhash2);
+}
---
+++ packages/mmhash2/mmhash2-1.4.tar.gz/mmhash2-1.4/setup.py
@@ -0,0 +1,37 @@
+from distutils.core import setup, Extension
+
+setup(
+    name='mmhash2',
+    version='1.4',
+    ext_modules=[
+        Extension('mmhash2',
+                  sources=['murmurhash2.cpp'],
+                  language='c++',),
+    ],
+    data_files=[('include', ['murmurhash2.hpp'])],
+    description="MurmurHash2 For Python3",
+    long_description="""
+murmurhash is a fast hash function ::
+    >>> import mmhash2
+
+    >>> print(mmhash2.murmurhash64a("zhenhao", 0x1234))
+    16299279845682521441
+
+    >>> print(mmhash2.dechex(mmhash2.murmurhash64a("zhenhao", 0x1234)))
+    'e232aca990972561'
+
+    >>> print(mmhash2.base62encode(mmhash2.murmurhash64a("zhenhao", 0x1234)))
+    'jq2Se1vfXaN'
+
+    >>> print(mmhash2.murmurhash64b('zhenhao', 0x1234))
+    4178523214822422458
+
+
+MurmurHash2 http://murmurhash.googlepages.com/
+MurmurHash2, 64-bit versions, by Austin Appleby
+
+Modified by hit_zhenhao@163.com
+
+linux python lib
+"""
+)
