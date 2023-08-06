17f6da25d87b346070ab99078b2a75e6742bf5ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:16 +0000

    Add package fteikpy/2.2.3/fteikpy-2.2.3.tar.gz

---
+++ packages/fteikpy/fteikpy-2.2.3.tar.gz/fteikpy-2.2.3/test/test_eikonal2d.py
@@ -0,0 +1,31 @@
+#%%
+
+import numpy
+import pytest
+from helpers import allclose, eik2d
+
+
+@pytest.mark.parametrize(
+    "sources, tref",
+    (
+        ([0.0, 0.0], 3573.41870888),
+        ([3.5, 3.5], 2462.96106736),
+        ([[0.0, 0.0], [3.5, 3.5]], [3573.41870888, 2462.96106736]),
+    ),
+)
+def test_solve(sources, tref):
+    tt = eik2d.solve(sources, nsweep=2)
+
+    allclose(tref, tt, lambda x: x.grid.sum())
+
+
+@pytest.mark.parametrize(
+    "points, vref",
+    (
+        ([0.0, 0.0], 1.0),
+        ([-1.0, -1.0], numpy.nan),
+        ([[0.0, 0.0], [15.0, 0.0], [15.0, 15.0], [0.0, 15.0],], numpy.ones(4)),
+    ),
+)
+def test_call(points, vref):
+    allclose(vref, points, lambda x: eik2d(x))
