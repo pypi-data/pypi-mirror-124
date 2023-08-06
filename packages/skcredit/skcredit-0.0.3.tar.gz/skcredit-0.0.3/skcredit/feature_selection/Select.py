f3ded66b5287419b9c7554b814519d7dc754d7b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:14:44 +0000

    Add package skcredit/0.0.3/skcredit-0.0.3.tar.gz

---
+++ packages/skcredit/skcredit-0.0.3.tar.gz/skcredit-0.0.3/skcredit/feature_selection/Select.py
@@ -0,0 +1,35 @@
+# coding: utf-8
+
+import warnings
+import numpy  as np
+import pandas as pd
+from sklearn.base import BaseEstimator, TransformerMixin
+np.random.seed(7)
+pd.set_option("max_rows",    None)
+pd.set_option("max_columns", None)
+pd.set_option("display.unicode.east_asian_width" , True)
+pd.set_option("display.unicode.ambiguous_as_wide", True)
+warnings.simplefilter(action="ignore", category=FutureWarning)
+
+
+class Select(BaseEstimator, TransformerMixin):
+    def __init__(self, keep_columns, date_columns):
+        self.keep_columns = keep_columns
+        self.date_columns = date_columns
+
+        self.feature_columns = None
+        self.feature_support = None
+
+    def fit(self, x, y=None):
+        pass
+
+    def transform(self,   x):
+
+        return x[self.keep_columns +
+                 self.date_columns +
+                 self.feature_columns[self.feature_support].tolist()]
+
+    def fit_transform(self, x, y=None, **fit_params):
+        self.fit(x, y)
+
+        return self.transform(x)
---
+++ packages/skcredit/skcredit-0.0.3.tar.gz/skcredit-0.0.3/skcredit/linear_model/LMCreditcard.py
@@ -0,0 +1,39 @@
+# coding:utf-8
+
+import warnings
+import numpy  as np
+import pandas as pd
+np.random.seed(7)
+pd.set_option("max_rows"   , None)
+pd.set_option("max_columns", None)
+pd.set_option("display.unicode.east_asian_width" , True)
+pd.set_option("display.unicode.ambiguous_as_wide", True)
+warnings.simplefilter(action="ignore", category=FutureWarning)
+
+
+class LMCreditcard(object):
+    def __init__(self, keep_columns, date_columns, discrete, lmclassifier, BASE,  PDO,  ODDS):
+        self.keep_columns = keep_columns
+        self.date_columns = date_columns
+        self.discrete, self.lmclassifier = discrete, lmclassifier
+
+        self.B = PDO / np.log(2)
+        self.A = BASE + self.B * np.log(ODDS)
+
+        self.offset_scores = self.A - self.B * self.lmclassifier.coeff["const"]
+        self.credit_scores = dict()
+
+    def show_scorecard(self):
+        tables = dict()
+        tables.update({column: spliter.table for column, spliter in self.discrete.cat_spliter.items()})
+        tables.update({column: spliter.table for column, spliter in self.discrete.num_spliter.items()})
+
+        for column in self.lmclassifier.feature_subsets:
+            table = tables[column][["Column", "Bucket", "WoE"]].copy(deep=True)
+            table["Coefficients"] =   self.lmclassifier.coeff[column]
+            table["PartialScore"] = - self.lmclassifier.coeff[column] * self.B * table["WoE"]
+            table["OffsetScores"] = self.offset_scores
+            self.credit_scores[column] = table
+
+        return pd.concat(self.credit_scores.values())
+
