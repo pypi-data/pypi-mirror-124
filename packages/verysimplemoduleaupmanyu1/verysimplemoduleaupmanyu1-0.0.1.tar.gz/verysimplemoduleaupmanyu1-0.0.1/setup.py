3b894f71b6952867a452ca824b25165733d5adb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:35:56 +0000

    Add package verysimplemoduleaupmanyu1/0.0.1/verysimplemoduleaupmanyu1-0.0.1.tar.gz

---
+++ packages/verysimplemoduleaupmanyu1/verysimplemoduleaupmanyu1-0.0.1.tar.gz/verysimplemoduleaupmanyu1-0.0.1/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.1'
+DESCRIPTION = 'My first Pyton package'
+LONG_DESCRITPTION = 'My first Python package with a slightly lnger description'
+
+# Setting up
+setup(
+    # the name must match the folder name 'versimplemodule'
+    name="verysimplemoduleaupmanyu1",
+    version=VERSION,
+    author="Abhishek Upmanyu",
+    author_email="abhishek2175@gmail.com",
+    description=DESCRIPTION,
+    long_description=LONG_DESCRITPTION,
+    packages=find_packages(),
+    install_requires=[],  # add any additional packages that
+    # needs to be installed along with your package. Eg: 'caer'
+
+    keywords=['python', 'first package'],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Education",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: MacOS :: MacOS X"
+    ]
+)
