68a9d13e39f171824683188e5e8bfec67d89ee44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:15 +0000

    Add package watchcat/0.0.3/watchcat-0.0.3.tar.gz

---
+++ packages/watchcat/watchcat-0.0.3.tar.gz/watchcat-0.0.3/watchcat/__main__.py
@@ -0,0 +1,28 @@
+import sys
+import termcolor
+from watchcat import cli
+from watchcat.config.config import ConfigLoader
+from watchcat.diff_detector.simple import SimpleDiffDetector
+from watchcat.storage.sql_storage import SqlStorage
+
+
+def main():
+    parser = cli.build_parser()
+    args = parser.parse_args()
+
+    config = ConfigLoader(args.config)
+    diff_detector = SimpleDiffDetector()
+    with SqlStorage(args.db) as storage:
+        for resource_id, resource in config.resources.items():
+            new_snapshot = resource.get_filtered()
+            old_snapshot = storage.get(resource_id)
+            storage.set(new_snapshot)
+            if old_snapshot is not None:
+                if diff_detector.has_update(old_snapshot, new_snapshot):
+                    message = f"{resource.title} has updated!\n{diff_detector.diff(old_snapshot, new_snapshot)}"
+                    resource.notifier.send(message)
+                    termcolor.cprint(f"update found for {resource_id}", file=sys.stderr, color="green")
+                else:
+                    termcolor.cprint(f"no update found for {resource_id}", file=sys.stderr)
+            else:
+                termcolor.cprint(f"first fetch for {resource_id}", file=sys.stderr, color="yellow")
---
+++ packages/watchcat/watchcat-0.0.3.tar.gz/watchcat-0.0.3/watchcat/info.py
@@ -0,0 +1,6 @@
+name = "watchcat"
+version = "0.0.3"
+authors = [
+    "shosatojp",
+    "tomy-0000",
+]
