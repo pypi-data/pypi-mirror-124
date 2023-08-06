09d76fc95a6b5b282e83f100222cb3a895177b7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:11 +0000

    Add package tomdistr/0.1/tomdistr-0.1.tar.gz

---
+++ packages/tomdistr/tomdistr-0.1.tar.gz/tomdistr-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='tomdistr',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['tomdistr'],
+      author='Tom Mathew',
+      author_email='something@nothing.com',
+      zip_safe=False)
