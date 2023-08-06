7f752b932f7cb0bc9ea5ad1937a04099c1b1ccc1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:36:17 +0000

    Add package funniestjoke/0.1/funniestjoke-0.1.tar.gz

---
+++ packages/funniestjoke/funniestjoke-0.1.tar.gz/funniestjoke-0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='funniestjoke',
+      version='0.1',
+      description='The funniest joke in the world',
+      url='http://github.com/purushothamkdr453/funniestjoke',
+      author='purushotham reddy',
+      author_email='purushothamkdr453@gmail.com',
+      license='MIT',
+      packages=['funniestjoke'],
+      zip_safe=False)
