ea958d4d18e7bdaaea6cddf6130ef608758cc6f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:37:53 +0000

    Add package nextcord-ext-all/1.0.0/nextcord-ext-all-1.0.0.tar.gz

---
+++ packages/nextcord-ext-all/nextcord-ext-all-1.0.0.tar.gz/nextcord-ext-all-1.0.0/setup.py
@@ -0,0 +1,84 @@
+import re
+import setuptools
+
+
+classifiers = [
+    "Development Status :: 5 - Production/Stable",
+    "Framework :: AsyncIO",
+    "Intended Audience :: Developers",
+    "License :: OSI Approved :: Apache Software License",
+    "Natural Language :: English",
+    "Operating System :: OS Independent",
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 3",
+    "Programming Language :: Python :: 3 :: Only",
+    "Programming Language :: Python :: 3.6",
+    "Programming Language :: Python :: 3.7",
+    "Programming Language :: Python :: 3.8",
+    "Programming Language :: Python :: 3.9",
+    "Programming Language :: Python :: 3.10",
+    "Programming Language :: Python :: Implementation :: CPython",
+    "Topic :: Communications",
+    "Topic :: Documentation",
+    "Topic :: Documentation :: Sphinx",
+    "Topic :: Internet",
+    "Topic :: Software Development",
+    "Topic :: Software Development :: Libraries",
+    "Topic :: Software Development :: Libraries :: Python Modules",
+]
+
+"""with open("requirements.txt") as stream:
+    install_requires = stream.read().splitlines() """
+
+packages = [
+    "nextcord.ext.ipc",
+    "nextcord.ext.alternatives",
+    "nextcord.ext.converters",
+    "nextcord.ext.events",
+    "nextcord.ext.getch",
+    "nextcord.ext.menus",
+    "nextcord.ext.rx",
+    "nextcord.ext.science",
+]
+
+project_urls = {
+    "Issue Tracker": "https://github.com/VincentRPS/nextcord-ext-all/issues",
+    "Source": "https://github.com/VincentRPS/nextcord-ext-all",
+}
+
+_version_regex = r"^version = ('|\")((?:[0-9]+\.)*[0-9]+(?:\.?([a-z]+)(?:\.?[0-9])?)?)\1$"
+
+with open("nextcord/ext/ipc/__init__.py") as stream:
+    match = re.search(_version_regex, stream.read(), re.MULTILINE)
+
+version = match.group(2)
+
+if match.group(3) is not None:
+    try:
+        import subprocess
+
+        process = subprocess.Popen(["git", "rev-list", "--count", "HEAD"], stdout=subprocess.PIPE)
+        out, _ = process.communicate()
+        if out:
+            version += out.decode("utf-8").strip()
+
+        process = subprocess.Popen(["git", "rev-parse", "--short", "HEAD"], stdout=subprocess.PIPE)
+        out, _ = process.communicate()
+        if out:
+            version += "+g" + out.decode("utf-8").strip()
+    except (Exception) as e:
+        pass
+
+
+setuptools.setup(
+    author="Nextcord, Ext-Creators, VincentRPS",
+    classifiers=classifiers,
+    description="A Nextcord extension for all ext additions",
+    license="Apache Software License",
+    name="nextcord-ext-all",
+    packages=packages,
+    project_urls=project_urls,
+    python_requires=">=3.6.0",
+    url="https://github.com/VincentRPS/nextcord-ext-all",
+    version=version,
+)
