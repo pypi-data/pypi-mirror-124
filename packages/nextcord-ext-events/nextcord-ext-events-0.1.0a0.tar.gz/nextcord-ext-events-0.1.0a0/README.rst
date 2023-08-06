62857194efeb1a83c5a818bf1dfa6dfa39a76e30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:39:36 +0000

    Add package nextcord-ext-events/0.1.0a0/nextcord-ext-events-0.1.0a0.tar.gz

---
+++ packages/nextcord-ext-events/nextcord-ext-events-0.1.0a0.tar.gz/nextcord-ext-events-0.1.0a0/README.rst
@@ -0,0 +1,46 @@
+nextcord-ext-events
+====================
+
+|dependencies| |license|
+
+Custom events derived from events dispatched by Discord. 
+
+    ⚠️ Work In Progress!
+
+Installation
+-------------
+
+This extension is currently not in PyPI.
+
+.. code-block:: sh
+
+    $ python3 -m pip install -U git+https://github.com/vincentrps/nextcord-ext-events
+
+
+Usage
+------
+
+An example for when subscribing to the on_member_kick event.
+
+.. code-block:: py
+
+    import nextcord
+    from nextcord.ext import commands, events
+    from nextcord.ext.events import member_kick
+
+
+    class MyBot(commands.Bot, events.EventsMixin):
+
+        async def on_ready(self):
+            print('Logged in!')
+
+        async def on_member_kick(self, member: nextcord.Member, entry: nextcord.AuditLogEntry):
+            print(f'{member} was kicked from {member.guild}!')
+
+
+    bot = MyBot(command_prefix='!', intents=nextcord.Intents.all())
+
+    bot.run("TOKEN")
+
+.. |dependencies| image:: https://img.shields.io/librariesio/github/vincentrps/nextcord-ext-events
+.. |license| image:: https://img.shields.io/pypi/l/nextcord-ext-events.svg
---
+++ packages/nextcord-ext-events/nextcord-ext-events-0.1.0a0.tar.gz/nextcord-ext-events-0.1.0a0/setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup
+import re
+
+"""with open("requirements.txt") as f:
+    requirements = f.read().splitlines()"""
+
+version = "1.2.0"
+with open("nextcord/ext/events/__init__.py") as f:
+    version = re.search(r"^__version__\s*=\s*[\'\"]([^\'\"]*)[\'\"]", f.read(), re.MULTILINE).group(1)
+
+with open("README.rst", encoding="utf-8") as f:
+    readme = f.read()
+
+setup(
+    name="nextcord-ext-events",
+    author="VincentRPS",
+    python_requires=">=3.8.0",
+    url="https://github.com/vincentrps/nextcord-ext-events",
+    version=version,
+    packages=["nextcord.ext.events"],
+    license="Apache Software License",
+    description="Custom events derived from events dispatched by Discord. ",
+    long_description=readme,
+    long_description_content_type="text/x-rst",
+    include_package_data=True,
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: Apache Software License",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.8",
+        "Topic :: Internet",
+        "Topic :: Software Development",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Utilities",
+    ]
+)
