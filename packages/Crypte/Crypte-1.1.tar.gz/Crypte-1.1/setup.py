9c43fbbcc57cbc10086e474ee98a2251e7c3a73e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:00:37 +0000

    Add package Crypte/1.1/Crypte-1.1.tar.gz

---
+++ packages/Crypte/Crypte-1.1.tar.gz/Crypte-1.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setuptools
+
+setuptools.setup(
+	name="Crypte",
+	version="1.1",
+	license="MB INC",
+	authors= ["MB Craft"],
+	author_email="mbcraft@orange.fr",
+	description="Un simple script pour chiffrer un message avec des chiffres et... Voilà...",
+	long_description="Nothing else.",
+	keywords=["Chiffre", "Crypte"],
+	packages=["Crypte"]
+	)
