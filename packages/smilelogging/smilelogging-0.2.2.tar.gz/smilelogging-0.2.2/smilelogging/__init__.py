11cee11bbeeec246a02550c551d86a14636eb952
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:58 +0000

    Add package smilelogging/0.2.2/smilelogging-0.2.2.tar.gz

---
+++ packages/smilelogging/smilelogging-0.2.2.tar.gz/smilelogging-0.2.2/smilelogging/__init__.py
@@ -0,0 +1,8 @@
+from smilelogging.logger import Logger
+import configargparse
+argparser = configargparse.ArgumentParser()
+argparser.add_argument('--project_name', type=str, default='')
+argparser.add_argument('--debug', action="store_true")
+argparser.add_argument('--screen_print', action="store_true")
+argparser.add_argument('--cache_ignore', type=str, default='')
+argparser.add_argument('--note', type=str, default='')
