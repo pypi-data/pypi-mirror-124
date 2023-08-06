067e9ca91c3c7325451154d60ed125594ec723ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:24:05 +0000

    Add package hanzi/1.0/Hanzi-1.0.tar.gz

---
+++ packages/hanzi/Hanzi-1.0.tar.gz/Hanzi-1.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='Hanzi',
+    version='1.0',
+    packages=['hanzi'],
+    url='',
+    license='',
+    author='',
+    author_email='',
+    description='',
+    include_package_data=True
+)
