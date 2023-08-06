3f764a0530b6726c86db13ad8a7ecdd0d0fae650
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:19:59 +0000

    Add package jsonspot/0.0.3/jsonspot-0.0.3.tar.gz

---
+++ packages/jsonspot/jsonspot-0.0.3.tar.gz/jsonspot-0.0.3/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup,find_packages
+
+setup(
+    name='jsonspot',
+    version='0.0.3',
+    author='quniao',
+    author_email='276590980@qq.com',
+    url='https://github.com/jayt1001/jsonspot',
+    description='A project to solve the judgment of multiple returned results of api test',
+    packages=find_packages(),
+    long_description="A project to solve the judgment of multiple returned results of api test",
+    long_description_content_type="text/markdown",
+    install_requires=[],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+
+)
