98f90666ebedfe5a55a3b94853af06298ffa73f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:01:20 +0000

    Add package daschat-base/0.12.1/daschat_base-0.12.1-py3-none-any.whl

---
+++ packages/daschat-base/daschat_base-0.12.1-py3-none-any.whl/daschat_base/__init__.py
@@ -0,0 +1,24 @@
+# type: ignore[attr-defined]
+"""Daschat base fro modules and plugins."""
+
+from pkg_resources import DistributionNotFound, get_distribution
+
+try:
+    __version__ = get_distribution("daschat_base").version
+except DistributionNotFound:
+    __version__ = "(local)"
+
+__author__ = "Daschat.io"
+__email__ = "admin@daschat.io"
+
+try:
+    import cython  # type: ignore
+except ImportError:
+    compiled: bool = False
+else:  # pragma: no cover
+    try:
+        compiled = cython.compiled
+    except AttributeError:
+        compiled = False
+
+__all__ = "__version__", "__author__", "__email__", "compiled"
