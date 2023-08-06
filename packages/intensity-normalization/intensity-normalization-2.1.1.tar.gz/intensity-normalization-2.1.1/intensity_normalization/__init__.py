81167dc50befdfdecab5df7fd0033a1982a30988
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:27:01 +0000

    Add package intensity-normalization/2.1.1/intensity-normalization-2.1.1.tar.gz

---
+++ packages/intensity-normalization/intensity-normalization-2.1.1.tar.gz/intensity-normalization-2.1.1/intensity_normalization/__init__.py
@@ -0,0 +1,13 @@
+"""Top-level package for intensity-normalization."""
+
+__author__ = """Jacob Reinhold"""
+__email__ = "jcreinhold@gmail.com"
+__version__ = "2.1.1"
+
+PEAK = {
+    "last": ["t1", "other", "last"],
+    "largest": ["t2", "flair", "largest"],
+    "first": ["pd", "md", "first"],
+}
+VALID_PEAKS = {m for modalities in PEAK.values() for m in modalities}
+VALID_MODALITIES = VALID_PEAKS - {"last", "largest", "first"}
