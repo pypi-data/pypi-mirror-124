519e05e016ae8fbc002f40ce4d5bec60680154b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:11:51 +0000

    Add package ascgraph/1.3/ascgraph-1.3.tar.gz

---
+++ packages/ascgraph/ascgraph-1.3.tar.gz/ascgraph-1.3/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='ascgraph',
+      version='1.3',
+      description='Ascii graphics in console',
+      packages=['ascgraph'],
+      author_email='farmbsbraw@gmail.com',
+      zip_safe=False)
