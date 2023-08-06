487a1c65f3adc42d46ae3d0fe3cb79ce9a019e1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:02 +0000

    Add package jupyter-releaser/0.9.4/jupyter_releaser-0.9.4.tar.gz

---
+++ packages/jupyter-releaser/jupyter_releaser-0.9.4.tar.gz/jupyter_releaser-0.9.4/jupyter_releaser/actions/publish_release.py
@@ -0,0 +1,20 @@
+# Copyright (c) Jupyter Development Team.
+# Distributed under the terms of the Modified BSD License.
+import os
+
+from jupyter_releaser.util import get_default_branch
+from jupyter_releaser.util import run
+
+release_url = os.environ["release_url"]
+default_branch = get_default_branch()
+
+if release_url:
+    run(f"jupyter-releaser extract-release {release_url}")
+    run(
+        f"jupyter-releaser forwardport-changelog {release_url} --branch {default_branch}"
+    )
+
+run(f"jupyter-releaser publish-assets {release_url}")
+
+if release_url:
+    run(f"jupyter-releaser publish-release {release_url}")
