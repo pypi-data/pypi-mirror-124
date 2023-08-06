433b4dd6ef8fea418868f0d485c47fa6943603a1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:01:35 +0000

    Add package mstr-rest-requests/0.12.5/mstr_rest_requests-0.12.5-py3-none-any.whl

---
+++ packages/mstr-rest-requests/mstr_rest_requests-0.12.5-py3-none-any.whl/mstr/requests/rest/authenticated_session.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+# Copyright 2020 Paul Bailey
+#
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#        https://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+
+from .session import MSTRRESTSession
+
+
+class AuthenticatedMSTRRESTSession(MSTRRESTSession):
+    """A context manager for sessions interacting with the MicroStrategy REST API."""
+
+    def __init__(self, base_url: str, username: str = None, password: str = None):
+        super(AuthenticatedMSTRRESTSession, self).__init__(base_url)
+        self._username = username
+        self._password = password
+
+    def __enter__(self):
+        self.login(self._username, self._password)
+        return self
+
+    def __exit__(self, t, v, tb):
+        self.logout()
