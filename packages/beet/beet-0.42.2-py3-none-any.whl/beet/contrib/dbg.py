ec98a2e2103f781032b12174dd12de5e49ea92d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:22:04 +0000

    Add package beet/0.42.2/beet-0.42.2-py3-none-any.whl

---
+++ packages/beet/beet-0.42.2-py3-none-any.whl/beet/contrib/dbg.py
@@ -0,0 +1,177 @@
+"""Plugin that installs a Jinja extension for easily logging things in commands."""
+
+
+__all__ = [
+    "DbgOptions",
+    "DbgRenderer",
+    "DbgExtension",
+    "get_padding",
+]
+
+
+import json
+from dataclasses import dataclass, field
+from functools import cached_property
+from itertools import cycle
+from typing import Any, List
+
+from jinja2.nodes import Node, Output, TemplateData
+from pydantic import BaseModel
+
+from beet import Context, JinjaExtension
+from beet.core.utils import JsonDict, TextComponent
+
+
+class DbgOptions(BaseModel):
+    command: str = "tellraw @a {payload}"
+
+    preview_digit_width: int = 6
+    preview_line_limit: int = 45
+    preview_padding: int = 2
+
+    payload: List[TextComponent] = [
+        {
+            "text": "",
+            "hoverEvent": {
+                "action": "show_text",
+                "contents": [
+                    "",
+                    {"text": "{{ render_path }} ", "color": "aqua"},
+                    {"text": "({{ mode }})\n\n", "color": "dark_aqua"},
+                    "{{ preview }}",
+                ],
+            },
+        },
+        {"text": "[{{ project_id }}]: ", "color": "gray"},
+        {
+            "text": "{{ target }} for {{ name }} = ",
+            "color": "gold",
+            "extra": ["{{ accessor }}"],
+        },
+    ]
+
+
+def beet_default(ctx: Context):
+    ctx.template.env.add_extension(DbgExtension)
+
+
+def get_padding(pixels: int) -> TextComponent:
+    """Generate a sequence of bold and normal spaces matching the given number of pixels."""
+    if pixels < 12 and pixels not in [4, 5, 8, 9, 10]:
+        raise ValueError(f"Invalid number of pixels {pixels}.")
+
+    regular, bold = divmod(pixels, 4)
+    regular -= bold
+
+    return [{"text": " " * regular}, {"text": " " * bold, "bold": True}]
+
+
+@dataclass
+class DbgRenderer:
+    """Class responsible for formatting the json text of a dbg statement."""
+
+    ctx: Context
+
+    accessors: JsonDict = field(
+        default_factory=lambda: {
+            "score": {"score": {"name": "{{name}}", "objective": "{{target}}"}},
+            "storage": {"storage": "{{target}}", "nbt": "{{name}}"},
+            "entity": {"entity": "{{target}}", "nbt": "{{name}}"},
+            "block": {"block": "{{target}}", "nbt": "{{name}}"},
+        }
+    )
+
+    @cached_property
+    def opts(self) -> DbgOptions:
+        return self.ctx.validate("dbg", DbgOptions)
+
+    def render_preview(self, path: str, lineno: int) -> TextComponent:
+        """Render the preview as a text component."""
+        function = self.ctx.data.functions[path]
+        lines = function.text.splitlines()
+
+        preview_start = max(lineno - 1 - self.opts.preview_padding, 0)
+        preview = lines[preview_start : lineno + self.opts.preview_padding]
+
+        numbers = [
+            str(i + 1) for i in range(preview_start, preview_start + len(preview))
+        ]
+        number_width = max(len(n) for n in numbers)
+
+        output: List[TextComponent] = [""]
+
+        for number, line, color in zip(numbers, preview, cycle(["#dddddd", "gray"])):
+            if len(line) > self.opts.preview_line_limit:
+                line = line[: self.opts.preview_line_limit - 4] + "..."
+
+            padding = number_width - len(number)
+            output.extend(get_padding(padding * self.opts.preview_digit_width + 4))
+            output.append(
+                {
+                    "text": number,
+                    "color": "red" if int(number) == lineno else "dark_red",
+                }
+            )
+
+            output.append({"text": " |  ", "color": "dark_gray"})
+            output.append({"text": f"{line}\n", "color": color})
+
+        return {"text": "", "extra": output}
+
+    def render(self, mode: str, name: str, target: str, lineno: int) -> str:
+        """Return the json text as a string."""
+        path = self.ctx.meta["render_path"]
+        preview = json.dumps(self.render_preview(path, lineno))
+
+        kwargs = {
+            "mode": mode,
+            "name": name,
+            "target": target,
+            "lineno": lineno,
+            "preview": '",' + preview + ',"',
+        }
+
+        accessor = self.ctx.template.render_json(self.accessors[mode], **kwargs)
+
+        return self.ctx.template.render_string(
+            json.dumps(self.opts.payload),
+            accessor='",' + json.dumps(accessor) + ',"',
+            **kwargs,
+        )
+
+
+class DbgExtension(JinjaExtension):
+    """A Jinja extension for easily logging things in commands."""
+
+    tags = {"dbg"}
+
+    def parse(self, parser: Any) -> Node:
+        lineno = next(parser.stream).lineno
+
+        mode = next(parser.stream)
+        if not mode.test_any("name:score", "name:storage", "name:entity", "name:block"):
+            parser.fail(f"Invalid mode {mode.value!r}.", mode.lineno)
+
+        name = parser.parse_expression()
+        parser.stream.expect("comma")
+        target = parser.parse_expression()
+
+        return Output(
+            [
+                self.call_method(
+                    "_dbg_handler",
+                    [TemplateData(mode.value), name, target, TemplateData(lineno)],
+                    lineno=lineno,
+                )
+            ],
+            lineno=lineno,
+        )
+
+    def _dbg_handler(self, mode: str, name: str, target: str, lineno: int) -> str:
+        if self.ctx.meta["render_group"] != "functions":
+            raise TypeError("Debug statements can only be used inside functions.")
+
+        renderer = self.ctx.inject(DbgRenderer)
+        payload = renderer.render(mode, name, target, lineno)
+
+        return renderer.opts.command.format(payload=payload) + "\n"
