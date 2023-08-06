23c4f90b8da34c4710c017c1d80727e5ede14c9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:24:23 +0000

    Add package ob-dj-otp/0.0.9/ob-dj-otp-0.0.9.tar.gz

---
+++ packages/ob-dj-otp/ob-dj-otp-0.0.9.tar.gz/ob-dj-otp-0.0.9/ob_dj_otp/apis/otp/views.py
@@ -0,0 +1,42 @@
+import logging
+
+from django.utils.decorators import method_decorator
+from drf_yasg.utils import swagger_auto_schema
+from rest_framework import mixins, permissions, viewsets
+
+from ob_dj_otp.apis.otp.serializers import (
+    OTPRequestCodeSerializer,
+    OTPVerifyCodeSerializer,
+)
+
+logger = logging.getLogger(__name__)
+
+
+@method_decorator(
+    name="create",
+    decorator=swagger_auto_schema(
+        operation_summary="OTP Auth & Register",
+        operation_description="""
+        OTP Endpoint can be used for authentication or registration.
+
+        *Auth*:
+
+        - Request `POST /{version}/otp/` with `phone_number`
+        The backend will send a verification code for the specified number if a matching user found.
+        - Request `POST /{version}/otp/` with `phone_number` and `verification_code`
+        The backend will validate the code and return access and refresh token
+
+        """,
+        tags=["OTP Auth",],
+        responses={},
+    ),
+)
+class OneTimePairingViewSet(mixins.CreateModelMixin, viewsets.GenericViewSet):
+    permission_classes = [
+        permissions.AllowAny,
+    ]
+
+    def get_serializer_class(self):
+        if "verification_code" in self.request.data.keys():
+            return OTPVerifyCodeSerializer
+        return OTPRequestCodeSerializer
