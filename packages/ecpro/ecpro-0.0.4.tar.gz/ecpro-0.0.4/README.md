5fb0391e91c69aeab11c4b473b91c3217a081b59
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:15:45 +0000

    Add package ecpro/0.0.4/ecpro-0.0.4.tar.gz

---
+++ packages/ecpro/ecpro-0.0.4.tar.gz/ecpro-0.0.4/README.md
@@ -0,0 +1,9 @@
+
+# 打包
+
+
+# 上传
+twine upload dist/*
+
+# 下载
+pip install ecpro -i  https://pypi.python.org/simple/
---
+++ packages/ecpro/ecpro-0.0.4.tar.gz/ecpro-0.0.4/setup.py
@@ -0,0 +1,25 @@
+# !/usr/bin/env python
+# -*- coding:utf-8 -*-
+# @author :libo
+
+from setuptools import setup
+
+setup(
+    name='ecpro',
+    version='0.0.4',
+    description='ecpro image crop utils',
+    long_description='ecpro crop package',
+    author='libo',
+    author_email='6878595@qq.com',
+    url='',
+    license='MIT Licence',
+    keywords='testing ecpro crop',
+    platforms='any',
+    python_requires='>=3.7.*',
+    install_requires=[],
+    classifiers=[
+      "Programming Language :: Python :: 3",
+      "License :: OSI Approved :: MIT License",
+      "Operating System :: OS Independent",
+      ],
+)
