d59964f23ef5341a4205e8865b5d1ae51a262609
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:10:20 +0000

    Add package evt73-distributions/2.0/evt73_distributions-2.0.tar.gz

---
+++ packages/evt73-distributions/evt73_distributions-2.0.tar.gz/evt73_distributions-2.0/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+import os
+
+#print(os.curdir)
+print(os.getcwd()+'/evt73_distributions/README.md')
+helf_path=os.getcwd()+'/evt73_distributions/README.md'
+count_instances=helf_path.count('evt73_distributions/')
+if count_instances!=1:
+      helf_path=helf_path.replace('evt73_distributions/', '')
+            
+help_path=helf_path.replace("\\", '/')
+      
+
+
+
+
+try:
+    import pypandoc
+    long_description = pypandoc.convert_file(helf_path, 'rst')
+except(FileNotFoundError):
+    #long_description = open('README.md').rt,ead()
+    print("Quase deu ruim")
+
+setup(name='evt73_distributions',
+      version='2.0',
+      description='Gaussian and Binomial distributions',
+      packages=['evt73_distributions'],
+      author="Everton Mendes",
+      author_email="evertonmendes73@usp.br",
+      long_description=long_description,
+      zip_safe=False)
