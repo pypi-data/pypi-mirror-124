5d079cae2a273eeebf3a9a2c1a098e8699c788eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:39:12 +0000

    Add package TayTules-pkg-taytek/0.0.6/TayTules_pkg_taytek-0.0.6-py3-none-any.whl

---
+++ packages/TayTules-pkg-taytek/TayTules_pkg_taytek-0.0.6-py3-none-any.whl/TayTools/tools.py
@@ -0,0 +1,30 @@
+import subprocess
+import time
+
+def install(package):
+  try: 
+    print('Checking for package...')
+    __import__(package)
+  except:
+    print('Package not found\nStarting install...')
+    try: subprocess.check_call(['pip', 'install', package])
+    except Exception as err: print(str(err)+'\nCould not install package')
+    time.sleep(3)
+    subprocess.check_call(['clear'])
+  else:
+     print(package + ' was already installed')
+     time.sleep(3)
+     subprocess.check_call(['clear'])
+
+def cmd(command):
+  cmd=command.split()
+  subprocess.check_call(cmd)
+
+def clear(secs=0):
+  time.sleep(secs)
+  subprocess.check_call('clear')
+
+def check_install(package):
+  try: __import__(package)
+  except: return False
+  else: return True
