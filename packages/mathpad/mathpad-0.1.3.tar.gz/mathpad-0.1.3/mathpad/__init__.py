c66bea6d88c76abfe61186dddd5aec0ea949bfdb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:48:55 +0000

    Add package mathpad/0.1.3/mathpad-0.1.3.tar.gz

---
+++ packages/mathpad/mathpad-0.1.3.tar.gz/mathpad-0.1.3/mathpad/__init__.py
@@ -0,0 +1,21 @@
+import sympy
+
+from .physical_quantity import *
+from .physical_quantities import *
+
+from .units import *
+from .solve import solve, Solution
+from .equation import Equation
+from ._quality_of_life import t, g, pi, frac
+from .algebra import subs, simplify, factor, expand
+from .display import tabulate
+
+from .calculus import diff, integral
+from .trigonometry import cos, sin, tan
+from .simulate_dynamic_system import simulate_dynamic_system
+
+
+sympy.init_printing()  # type: ignore
+sympy.printing.printer.Printer.set_global_settings(min=-3, max=4)  # type: ignore
+
+__version__ = "0.1.3"
