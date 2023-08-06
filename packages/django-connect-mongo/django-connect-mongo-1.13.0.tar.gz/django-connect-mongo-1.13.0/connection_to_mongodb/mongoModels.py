6de9fbccf93ea7d9b628fa4391cb3faac25c0a6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:28:21 +0000

    Add package django-connect-mongo/1.13.0/django-connect-mongo-1.13.0.tar.gz

---
+++ packages/django-connect-mongo/django-connect-mongo-1.13.0.tar.gz/django-connect-mongo-1.13.0/connection_to_mongodb/mongoModels.py
@@ -0,0 +1,51 @@
+import threading, queue
+from multiprocessing import Process, Queue
+import datetime
+
+from pymongo import MongoClient
+import copy
+from bson.objectid import ObjectId
+
+import copy
+
+
+def create_connection_to_mongo(db_name, collection_name, url):
+    client = MongoClient(url)
+    return client['db_name']['collection_name']
+
+
+class MongoBaseModel:
+    def __init__(self, **kwargs):
+        # user must defind self.db_name, self.collection_name, self.url, self.fields
+        # default self.create_thread = Process
+
+        self.collection = create_connection_to_mongo(self.db_name, self.collection_name, self.url)
+
+        self.data = copy.deepcopy(kwargs)
+        self.create_thread = Process
+        
+    def create(self, take_result=False):
+        self.validate_data()
+
+        self.data['create_at'] = datetime.datetime.utcnow()
+
+        if take_result:
+            return self.collection.insert_one(self.data)
+        else:
+            self.create_thread(target=self.create_in_thread, args=(self.data,)).start()
+
+    def create_in_thread(self, data):
+        create_connection_to_mongo(self.db_name, self.collection_name, self.url).insert_one(data)
+
+    def validate_data(self):
+        for field in copy.deepcopy(self.data):
+            if field not in self.fields:
+                del self.data[field]
+        
+        # kiem tra xem co field nao required ma bi thieu khong
+        for field in self.fields:
+            config = self.fields[field]
+            if config['required'] and field not in self.data:
+                raise Exception(f"field {field} is required!")
+
+
---
+++ packages/django-connect-mongo/django-connect-mongo-1.13.0.tar.gz/django-connect-mongo-1.13.0/setup.py
@@ -0,0 +1,28 @@
+import pathlib
+from setuptools import setup, find_packages
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+setup(
+    name="django-connect-mongo",
+    version="1.13.0",
+    description="we use this package to work with mongodb easier",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    # url="",
+    author="hieucao192",
+    author_email="hieucaohd@gmail.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.9",
+    ],
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=["pymongo[srv]"],
+)
