125ee8454ab6afb4d18496f2ae6e47b76da7aaa5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:37:52 +0000

    Add package console-images/0.0.4/console_images-0.0.4.tar.gz

---
+++ packages/console-images/console_images-0.0.4.tar.gz/console_images-0.0.4/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+	long_description = fh.read()
+
+requirements = ["colorama", "typing", "pillow"]
+
+setuptools.setup(
+	name="console_images",
+	version="0.0.4",
+    author="LedinecMing",
+	author_email="loliamalexxaxa@gmail.com",
+	description="Colored console images and gifs",
+	long_description=long_description,
+	long_description_content_type="text/markdown",
+	packages=setuptools.find_packages(),
+	install_requires=requirements,
+	classifiers=[
+		"Programming Language :: Python :: 3.8",
+		"License :: OSI Approved :: Apache Software License",
+		"Operating System :: OS Independent",
+	],
+	python_requires='>=3.6',
+)
+
