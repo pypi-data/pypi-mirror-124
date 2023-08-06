4b73e6572c96bf1598d06fa5d1afb798cd3065b4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:37:20 +0000

    Add package dishhq/0.1.0/dishhq-0.1.0.tar.gz

---
+++ packages/dishhq/dishhq-0.1.0.tar.gz/dishhq-0.1.0/setup.py
@@ -0,0 +1,23 @@
+
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="dishhq",                     
+    version="0.1.0",                        
+    author="Dish Team",                    
+    description="Official Python SDK for Dish",
+    long_description=long_description,      
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),   
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],                                    
+    python_requires='>=3.6',           
+    py_modules=["dishhq"],        
+    package_dir={'':'.'},     
+    install_requires=[]                     
+)
