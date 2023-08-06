75460d75acf61c8afff3237e4091a96a127d2547
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:54 +0000

    Add package pytorch-lightning/1.5.0rc1/pytorch_lightning-1.5.0rc1-py3-none-any.whl

---
+++ packages/pytorch-lightning/pytorch_lightning-1.5.0rc1-py3-none-any.whl/pytorch_lightning/plugins/precision/deepspeed_precision.py
@@ -0,0 +1,82 @@
+# Copyright The PyTorch Lightning team.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+from typing import Any, Callable, Optional, Union
+
+from torch import Tensor
+from torch.nn import Module
+from torch.optim import LBFGS, Optimizer
+
+import pytorch_lightning as pl
+from pytorch_lightning.plugins.precision.precision_plugin import PrecisionPlugin
+from pytorch_lightning.utilities import GradClipAlgorithmType
+from pytorch_lightning.utilities.exceptions import MisconfigurationException
+from pytorch_lightning.utilities.model_helpers import is_overridden
+from pytorch_lightning.utilities.warnings import WarningCache
+
+warning_cache = WarningCache()
+
+
+class DeepSpeedPrecisionPlugin(PrecisionPlugin):
+    """Precision plugin for DeepSpeed integration."""
+
+    def __init__(self, precision: int) -> None:
+        super().__init__()
+        self.precision = precision
+
+    def backward(self, model: "pl.LightningModule", closure_loss: Tensor, *args: Any, **kwargs: Any) -> None:
+        if is_overridden("backward", model):
+            warning_cache.warn(
+                "You have overridden the `LightningModule.backward` hook but it will be ignored since DeepSpeed handles"
+                " the backward logic internally."
+            )
+        deepspeed_engine = model.trainer.model
+        deepspeed_engine.backward(closure_loss, *args, **kwargs)
+
+    def _run_backward(self, tensor: Tensor, model: Module, *args: Any, **kwargs: Any) -> None:
+        model.backward(tensor, *args, **kwargs)
+
+    def pre_optimizer_step(
+        self,
+        model: Union["pl.LightningModule", Module],
+        optimizer: Optimizer,
+        optimizer_idx: int,
+        lambda_closure: Callable,
+        **kwargs: Any,
+    ) -> bool:
+        """Hook to do something before each optimizer step."""
+        if isinstance(optimizer, LBFGS):
+            raise MisconfigurationException(
+                f"DeepSpeed and the LBFGS optimizer are not compatible (optimizer {optimizer_idx})."
+            )
+        result = lambda_closure()  # DeepSpeed does not support closures
+        super().pre_optimizer_step(model, optimizer, optimizer_idx, lambda_closure, **kwargs)
+        skipped_backward = result is None
+        # in manual optimization, the closure does not return a value
+        if isinstance(model, pl.LightningModule) and model.automatic_optimization and skipped_backward:
+            raise MisconfigurationException(
+                "Skipping backward by returning `None` from your `training_step` is not supported by `DeepSpeed`"
+            )
+        # DeepSpeed handles the optimizer step internally
+        deepspeed_engine = model.trainer.model if isinstance(model, pl.LightningModule) else model
+        deepspeed_engine.step()
+        return False
+
+    def clip_gradients(
+        self,
+        optimizer: Optimizer,
+        clip_val: Union[int, float],
+        gradient_clip_algorithm: GradClipAlgorithmType = GradClipAlgorithmType.NORM,
+        model: Optional[Module] = None,
+    ) -> None:
+        """DeepSpeed handles gradient clipping internally."""
---
+++ packages/pytorch-lightning/pytorch_lightning-1.5.0rc1-py3-none-any.whl/tests/plugins/test_amp_plugins.py
@@ -0,0 +1,224 @@
+# Copyright The PyTorch Lightning team.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+from unittest import mock
+
+import pytest
+import torch
+
+from pytorch_lightning import Trainer
+from pytorch_lightning.plugins import ApexMixedPrecisionPlugin, NativeMixedPrecisionPlugin
+from pytorch_lightning.plugins.precision import MixedPrecisionPlugin
+from pytorch_lightning.utilities import _TORCH_GREATER_EQUAL_DEV_1_10
+from pytorch_lightning.utilities.exceptions import MisconfigurationException
+from tests.helpers import BoringModel
+from tests.helpers.runif import RunIf
+
+
+class MyNativeAMP(NativeMixedPrecisionPlugin):
+    pass
+
+
+class MyApexPlugin(ApexMixedPrecisionPlugin):
+    pass
+
+
+@mock.patch.dict(
+    os.environ,
+    {
+        "CUDA_VISIBLE_DEVICES": "0,1",
+        "SLURM_NTASKS": "2",
+        "SLURM_JOB_NAME": "SOME_NAME",
+        "SLURM_NODEID": "0",
+        "LOCAL_RANK": "0",
+        "SLURM_PROCID": "0",
+        "SLURM_LOCALID": "0",
+    },
+)
+@mock.patch("torch.cuda.device_count", return_value=2)
+@pytest.mark.parametrize("ddp_backend,gpus", [("ddp", 2), ("ddp2", 2), ("ddp_spawn", 2)])
+@pytest.mark.parametrize(
+    "amp,custom_plugin,plugin_cls",
+    [
+        ("native", False, NativeMixedPrecisionPlugin),
+        ("native", True, MyNativeAMP),
+        pytest.param("apex", False, ApexMixedPrecisionPlugin, marks=RunIf(amp_apex=True)),
+        pytest.param("apex", True, MyApexPlugin, marks=RunIf(amp_apex=True)),
+    ],
+)
+def test_amp_apex_ddp(
+    mocked_device_count, ddp_backend: str, gpus: int, amp: str, custom_plugin: bool, plugin_cls: MixedPrecisionPlugin
+):
+
+    trainer = Trainer(
+        fast_dev_run=True,
+        precision=16,
+        amp_backend=amp,
+        gpus=gpus,
+        strategy=ddp_backend,
+        plugins=[plugin_cls()] if custom_plugin else None,
+    )
+    assert isinstance(trainer.precision_plugin, plugin_cls)
+    if amp == "native":
+        assert not trainer.precision_plugin.is_bfloat16
+
+
+class GradientUnscaleBoringModel(BoringModel):
+    def on_before_optimizer_step(self, *_):
+        norm = torch.nn.utils.clip_grad_norm_(self.parameters(), 2)
+        if not (torch.isinf(norm) or torch.isnan(norm)):
+            assert norm.item() < 15.0
+
+
+@RunIf(min_gpus=2)
+@pytest.mark.parametrize("accum", [1, 2])
+def test_amp_gradient_unscale(tmpdir, accum: int):
+    model = GradientUnscaleBoringModel()
+
+    trainer = Trainer(
+        max_epochs=2,
+        default_root_dir=tmpdir,
+        limit_train_batches=2,
+        limit_test_batches=2,
+        limit_val_batches=2,
+        amp_backend="native",
+        strategy="ddp_spawn",
+        gpus=2,
+        precision=16,
+        track_grad_norm=2,
+        log_every_n_steps=1,
+        accumulate_grad_batches=accum,
+    )
+    trainer.fit(model)
+
+
+@RunIf(min_gpus=1)
+def test_amp_skip_optimizer(tmpdir):
+    """Test that optimizers can be skipped when using amp."""
+
+    class CustomBoringModel(BoringModel):
+        def __init__(self):
+            super().__init__()
+            self.layer1 = torch.nn.Linear(32, 32)
+            self.layer2 = torch.nn.Linear(32, 2)
+
+        def forward(self, x: torch.Tensor):
+            x = self.layer1(x)
+            x = self.layer2(x)
+            return x
+
+        def training_step(self, batch, batch_idx, optimizer_idx):
+            if optimizer_idx == 1:
+                return None
+            output = self(batch)
+            return self.loss(batch, output)
+
+        def configure_optimizers(self):
+            return [
+                torch.optim.SGD(self.layer1.parameters(), lr=0.1),
+                torch.optim.SGD(self.layer2.parameters(), lr=0.1),
+            ]
+
+    trainer = Trainer(default_root_dir=tmpdir, gpus=1, fast_dev_run=1, amp_backend="native", precision=16)
+    model = CustomBoringModel()
+    trainer.fit(model)
+
+
+@RunIf(min_gpus=2, amp_apex=True, special=True)
+@pytest.mark.parametrize("amp_level", ["O2"])
+def test_amp_apex_ddp_fit(amp_level, tmpdir):
+    class CustomBoringModel(BoringModel):
+        def training_step(self, batch, batch_idx):
+            assert self.layer.weight.dtype == torch.float16
+            assert self.trainer.precision_plugin._connected
+            return super().training_step(batch, batch_idx)
+
+    trainer = Trainer(
+        default_root_dir=tmpdir,
+        fast_dev_run=True,
+        precision=16,
+        amp_backend="apex",
+        gpus=2,
+        strategy="ddp",
+        plugins=ApexMixedPrecisionPlugin(amp_level=amp_level),
+    )
+    assert isinstance(trainer.precision_plugin, ApexMixedPrecisionPlugin)
+    model = CustomBoringModel()
+    trainer.fit(model)
+    trainer.test(model)
+
+
+@RunIf(min_gpus=2, amp_apex=True)
+@pytest.mark.parametrize("amp_level", ["O2"])
+def test_amp_apex_ddp_spawn_fit(amp_level, tmpdir):
+
+    trainer = Trainer(
+        default_root_dir=tmpdir,
+        fast_dev_run=True,
+        precision=16,
+        amp_backend="apex",
+        gpus=2,
+        strategy="ddp_spawn",
+        plugins=ApexMixedPrecisionPlugin(amp_level=amp_level),
+    )
+    assert isinstance(trainer.precision_plugin, ApexMixedPrecisionPlugin)
+    model = BoringModel()
+    trainer.fit(model)
+
+
+@pytest.mark.skipif(not _TORCH_GREATER_EQUAL_DEV_1_10, reason="Torch CPU AMP is not available.")
+def test_cpu_amp_precision_context_manager(tmpdir):
+    """Test to ensure that the context manager correctly is set to CPU + bfloat16, and a scaler isn't set."""
+
+    plugin = NativeMixedPrecisionPlugin(precision="bf16", use_cpu=True)
+    assert plugin.use_cpu
+    assert not hasattr(plugin, "scaler")
+    context_manager = plugin.autocast_context_manager()
+    assert isinstance(context_manager, torch.cpu.amp.autocast)
+    assert context_manager.fast_dtype == torch.bfloat16
+
+
+def test_precision_selection_raises(monkeypatch):
+    with pytest.raises(
+        MisconfigurationException, match=r"precision=16, amp_type='apex'\)` but apex AMP not supported on CPU"
+    ):
+        Trainer(amp_backend="apex", precision=16)
+
+    import pytorch_lightning.plugins.precision.native_amp as amp
+
+    monkeypatch.setattr(amp, "_TORCH_GREATER_EQUAL_DEV_1_10", False)
+    with pytest.warns(
+        UserWarning, match=r"precision=16\)` but native AMP is not supported on CPU. Using `precision='bf16"
+    ), pytest.raises(MisconfigurationException, match="must install torch greater or equal to 1.10"):
+        Trainer(precision=16)
+
+    with pytest.raises(MisconfigurationException, match="must install torch greater or equal to 1.10"):
+        Trainer(precision="bf16")
+
+    with pytest.raises(MisconfigurationException, match=r"amp_type='apex', precision='bf16'\)` but it's not supported"):
+        Trainer(amp_backend="apex", precision="bf16")
+
+    with mock.patch("torch.cuda.device_count", return_value=1), pytest.raises(
+        MisconfigurationException, match="Sharded plugins are not supported with apex"
+    ):
+        Trainer(amp_backend="apex", precision=16, gpus=1, accelerator="ddp_fully_sharded")
+
+    import pytorch_lightning.plugins.precision.apex_amp as apex
+
+    monkeypatch.setattr(apex, "_APEX_AVAILABLE", False)
+    with mock.patch("torch.cuda.device_count", return_value=1), pytest.raises(
+        MisconfigurationException, match="asked for Apex AMP but you have not installed it"
+    ):
+        Trainer(amp_backend="apex", precision=16, gpus=1)
