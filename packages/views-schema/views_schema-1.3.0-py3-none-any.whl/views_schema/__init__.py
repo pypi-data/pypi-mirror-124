832725d9335e0408ccfc3c3bc734c516ff70e3b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:48:17 +0000

    Add package views-schema/1.3.0/views_schema-1.3.0-py3-none-any.whl

---
+++ packages/views-schema/views_schema-1.3.0-py3-none-any.whl/views_schema/__init__.py
@@ -0,0 +1,9 @@
+"""
+The classes in this module define the (json) schema which services and tools
+use to communicate.
+"""
+
+from .queryset_manager import *
+from .partitioning import *
+from .docs import *
+
