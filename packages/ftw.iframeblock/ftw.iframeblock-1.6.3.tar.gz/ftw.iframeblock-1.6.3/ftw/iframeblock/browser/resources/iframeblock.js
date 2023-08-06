2ec05d57f06758d6f56eaa41bc41a59bda69c75c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:28:31 +0000

    Add package ftw.iframeblock/1.6.3/ftw.iframeblock-1.6.3.tar.gz

---
+++ packages/ftw.iframeblock/ftw.iframeblock-1.6.3.tar.gz/ftw.iframeblock-1.6.3/ftw/iframeblock/browser/resources/iframeblock.js
@@ -0,0 +1,40 @@
+(function (factory) {
+  if (typeof define === 'function' && define.amd) {
+    require(['jquery', 'iframeResizer'], factory);
+  } else {
+    factory(window.jQuery, window.iFrameResize);
+  }
+}(function ($, iFrameResize) {
+  "use strict";
+  var default_options = {
+    inPageLinks: true,
+  }
+
+  function initIFrame() {
+      var iframe = $(this);
+
+      if (iframe.data('autoSize') === 'True') {
+        var set_options = iframe.data('resizerOptions');
+
+        var autoJump = { onResized: function () {scroll(0, 0);} };
+        if (iframe.data('autoJump') !== 'True') {
+          autoJump = {}
+        }
+
+        var options = $.extend({},default_options,set_options, autoJump);
+        iFrameResize(options, this);
+      }
+
+      iframe.prev().removeClass('loading');
+  }
+
+  // We have to wait for the ready and then add the onload event listener directly on the
+  // iframe because the iframe does not propagate the onload back to document.
+  $(document).ready(function () {
+    $('iframe').on('load', initIFrame);
+
+    $(document).on('onBeforeClose', '.overlay', function () {
+      $('iframe').each(initIFrame);
+    });
+  });
+}));
