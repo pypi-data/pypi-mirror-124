0b55e391e92a94b4738ee3b52e82b7c4185c1299
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:07:07 +0000

    Add package ignoregen/0.6/ignoregen-0.6.tar.gz

---
+++ packages/ignoregen/ignoregen-0.6.tar.gz/ignoregen-0.6/ignoregen/__init__.py
@@ -0,0 +1,7 @@
+##################################################
+# Copyright (C) 2017-2020, All rights reserved.
+##################################################
+
+__project_name__ = "ignoregen"
+__version__ = "0.6"
+__description__ = "Generate Git ignore rules"
