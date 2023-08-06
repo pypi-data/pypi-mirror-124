de9555424632cad63603a056cafdca708141b366
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:53 +0000

    Add package dora-search/0.1.6/dora_search-0.1.6.tar.gz

---
+++ packages/dora-search/dora_search-0.1.6.tar.gz/dora_search-0.1.6/dora/tests/integ/grids/test.py
@@ -0,0 +1,26 @@
+
+# Copyright (c) Facebook, Inc. and its affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the license found in the
+# LICENSE file in the root directory of this source tree.
+
+from dora import Explorer
+import treetable as tt
+
+
+class MyExplorer(Explorer):
+    def get_grid_metrics(self):
+        return [
+            tt.leaf("loss", ".3f"),
+        ]
+
+
+@MyExplorer
+def explorer(launcher):
+    for a in [32, 64, 128]:
+        launcher(a=a)
+
+    launcher.bind_(b=4)
+    launcher.slurm_(mem_per_gpu=20)
+    launcher()
