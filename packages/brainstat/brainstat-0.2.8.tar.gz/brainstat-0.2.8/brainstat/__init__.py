d1ae97fff7b9b42171f29fd396504b337ffd8b70
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:09:33 +0000

    Add package brainstat/0.2.8/brainstat-0.2.8.tar.gz

---
+++ packages/brainstat/brainstat-0.2.8.tar.gz/brainstat-0.2.8/brainstat/__init__.py
@@ -0,0 +1,13 @@
+"""Neuroimaging statistics toolbox."""
+import sys
+import warnings
+
+__version__ = "0.2.8"
+
+
+if sys.version_info[1] == 6:
+    warnings.simplefilter("always", DeprecationWarning)
+    warnings.warn(
+        "Support for Python3.6 has been dropped. Future versions may not install on Python3.6.",
+        DeprecationWarning,
+    )
