ecae3518c942b78cc4ecae722fe36bb4514f2c45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:13:06 +0000

    Add package chemistry-adapters/0.0.2/chemistry_adapters-0.0.2.tar.gz

---
+++ packages/chemistry-adapters/chemistry_adapters-0.0.2.tar.gz/chemistry_adapters-0.0.2/chemistry_adapters/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+#
+# Initialization of the package
+#
+# -----------------------------
+
+
+from chemistry_adapters.amino_acids import AminoAcidAdapter
+
+name='ChemistryAdapters'
