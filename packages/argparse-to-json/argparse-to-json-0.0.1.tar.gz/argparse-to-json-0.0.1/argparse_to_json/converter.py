c54e407127da206f7ed715431a332515ef66c8c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:15:19 +0000

    Add package argparse-to-json/0.0.1/argparse-to-json-0.0.1.tar.gz

---
+++ packages/argparse-to-json/argparse-to-json-0.0.1.tar.gz/argparse-to-json-0.0.1/argparse_to_json/converter.py
@@ -0,0 +1,116 @@
+import argparse
+
+from typing import Optional
+
+
+class Converter:
+    def convert(
+        self,
+        parser: argparse.ArgumentParser,
+        schema: Optional[dict] = None,
+        form: Optional[list] = None
+    ) -> dict:
+        schema = {} if schema is None else schema
+        form = [] if form is None else form
+        self.parse_parser(parser, schema, form)
+        object = {
+            'schema': schema,
+        }
+        if len(form) > 0:
+            object['form'] = form
+        return object
+
+    def parse_parser(self, parser: argparse.ArgumentParser, schema: Optional[dict] = None, form: Optional[list] = None):
+        schema = {} if schema is None else schema
+        form = [] if form is None else form
+        for action in parser._actions[1:]:
+            self.parse_action(action, schema, form)
+        return schema, form
+
+    def parse_action(self, action: argparse.Action, schema: dict, form: list):
+        action_name = type(action).__name__
+        fn = {
+            '_StoreAction': self.parse_store_action,
+            '_StoreConstAction': self.parse_store_const_action,
+            '_StoreTrueAction': self.parse_store_const_action,
+            '_StoreFalseAction': self.parse_store_const_action,
+            '_AppendAction': self.parse_append_action,
+            '_AppendConstAction': self.parse_append_const_action,
+            '_SubParsersAction': self.parse_subparsers_action,
+        }[action_name]
+        fn(action, schema, form)
+
+    def parse_store_action(self, action: argparse.Action, schema: dict, form: list):
+        data = {
+            'type': self.get_type(action),
+        }
+        if action.help:
+            data['description'] = action.help
+        if action.required:
+            data['required'] = action.required
+        if action.choices:
+            data['enum'] = action.choices
+        if action.metavar:
+            data['title'] = action.metavar
+        if isinstance(action.type, argparse.FileType):
+            form.append({
+                'key': action.dest,
+                'type': 'file',
+            })
+        schema[action.dest] = data
+
+    def parse_store_const_action(self, action: argparse.Action, schema: dict, form: list):
+        data = {
+            'type': 'boolean',
+        }
+        if action.help:
+            data['description'] = action.help
+        if action.metavar:
+            data['title'] = action.metavar
+        schema[action.dest] = data
+
+    def parse_append_action(self, action: argparse.Action, schema: dict, form: list):
+        data = {
+            'type': 'array',
+            'items': {
+                'type': self.get_type(action),
+            },
+        }
+        if action.help:
+            data['description'] = action.help
+        if action.metavar:
+            data['title'] = action.metavar
+        schema[action.dest] = data
+
+    def parse_append_const_action(self, action: argparse.Action, schema: dict, form: list):
+        data = {
+            'type': 'array',
+            'items': {
+                'type': 'boolean',
+            },
+        }
+        if action.help:
+            data['description'] = action.help
+        if action.metavar:
+            data['title'] = action.metavar
+        schema[action.dest] = data
+
+    def parse_subparsers_action(self, action: argparse.Action, schema: dict, form: list):
+        form.append({
+            'type': 'selectfieldset',
+            'title': 'Choose command',
+            'items': [{
+                'key': name,
+                'legend': name,
+            } for name in action.choices],
+        })
+        for name, subparser in action.choices.items():
+            subparser_schema, subparser_form = self.parse_parser(subparser)
+            schema[name] = {
+                'type': 'object',
+                'properties': subparser_schema,
+            }
+            form.extend(subparser_form)
+
+    def get_type(self, action):
+        return 'integer' if action.type is int else 'string'
---
+++ packages/argparse-to-json/argparse-to-json-0.0.1.tar.gz/argparse-to-json-0.0.1/tests/test_converter.py
@@ -0,0 +1,197 @@
+import argparse
+import unittest
+
+from argparse_to_json import convert_parser_to_json as convert
+
+
+class TestConverter(unittest.TestCase):
+    def test_empty_parser(self):
+        parser = argparse.ArgumentParser()
+        jsonform = convert(parser)
+        self.assertEqual({
+            'schema': {}
+        }, jsonform)
+
+    def test_positional_argument(self):
+        parser = argparse.ArgumentParser()
+        parser.add_argument('input1')
+        jsonform = convert(parser)
+        self.assertEqual({
+            'schema': {
+                'input1': {
+                    'type': 'string',
+                    'required': True,
+                },
+            },
+        }, jsonform)
+
+    def test_positional_int_argument(self):
+        parser = argparse.ArgumentParser()
+        parser.add_argument('input1', type=int)
+        jsonform = convert(parser)
+        self.assertEqual({
+            'schema': {
+                'input1': {
+                    'type': 'integer',
+                    'required': True,
+                },
+            },
+        }, jsonform)
+
+    def test_positional_argument_with_choices(self):
+        parser = argparse.ArgumentParser()
+        parser.add_argument('input1', choices=['foo', 'bar'])
+        jsonform = convert(parser)
+        self.assertEqual({
+            'schema': {
+                'input1': {
+                    'type': 'string',
+                    'enum': ['foo', 'bar'],
+                    'required': True,
+                },
+            },
+        }, jsonform)
+
+    def test_positional_file_argument(self):
+        parser = argparse.ArgumentParser()
+        parser.add_argument('input1', type=argparse.FileType('r'))
+        jsonform = convert(parser)
+        self.assertEqual({
+            'schema': {
+                'input1': {
+                    'type': 'string',
+                    'required': True,
+                },
+            },
+            'form': [
+                {
+                    'key': 'input1',
+                    'type': 'file',
+                }
+            ],
+        }, jsonform)
+
+    def test_positional_argument_metavar(self):
+        parser = argparse.ArgumentParser()
+        parser.add_argument('input1', metavar='Input')
+        jsonform = convert(parser)
+        self.assertEqual({
+            'schema': {
+                'input1': {
+                    'type': 'string',
+                    'required': True,
+                    'title': 'Input',
+                },
+            },
+        }, jsonform)
+
+    def test_optional_argument(self):
+        parser = argparse.ArgumentParser()
+        parser.add_argument('-i', '--input1')
+        jsonform = convert(parser)
+        self.assertEqual({
+            'schema': {
+                'input1': {
+                    'type': 'string',
+                },
+            },
+        }, jsonform)
+
+    def test_optional_store_const(self):
+        parser = argparse.ArgumentParser()
+        parser.add_argument('-i', '--input1', action='store_const', const=None)
+        jsonform = convert(parser)
+        self.assertEqual({
+            'schema': {
+                'input1': {
+                    'type': 'boolean',
+                },
+            },
+        }, jsonform)
+
+    def test_optional_store_true(self):
+        parser = argparse.ArgumentParser()
+        parser.add_argument('-i', '--input1', action='store_true')
+        jsonform = convert(parser)
+        self.assertEqual({
+            'schema': {
+                'input1': {
+                    'type': 'boolean',
+                },
+            },
+        }, jsonform)
+
+    def test_optional_store_false(self):
+        parser = argparse.ArgumentParser()
+        parser.add_argument('-i', '--input1', action='store_false')
+        jsonform = convert(parser)
+        self.assertEqual({
+            'schema': {
+                'input1': {
+                    'type': 'boolean',
+                },
+            },
+        }, jsonform)
+
+    def test_optional_append(self):
+        parser = argparse.ArgumentParser()
+        parser.add_argument('-i', '--input1', action='append')
+        jsonform = convert(parser)
+        self.assertEqual({
+            'schema': {
+                'input1': {
+                    'type': 'array',
+                    'items': {
+                        'type': 'string',
+                    }
+                },
+            },
+        }, jsonform)
+
+    def test_optional_append_const(self):
+        parser = argparse.ArgumentParser()
+        parser.add_argument('-i', '--input1', action='append_const', const=None)
+        jsonform = convert(parser)
+        self.assertEqual({
+            'schema': {
+                'input1': {
+                    'type': 'array',
+                    'items': {
+                        'type': 'boolean',
+                    }
+                },
+            },
+        }, jsonform)
+
+    def test_subparsers(self):
+        parser = argparse.ArgumentParser()
+        subparsers = parser.add_subparsers()
+        subparsers.add_parser('subparser1')
+        subparsers.add_parser('subparser2')
+        jsonform = convert(parser)
+        self.assertEqual({
+            'schema': {
+                'subparser1': {
+                    'type': 'object',
+                    'properties': {},
+                },
+                'subparser2': {
+                    'type': 'object',
+                    'properties': {},
+                },
+            },
+            'form': [
+                {
+                    'type': 'selectfieldset',
+                    'title': 'Choose command',
+                    'items': [
+                        {
+                            'key': 'subparser1',
+                        },
+                        {
+                            'key': 'subparser2',
+                        }
+                    ],
+                },
+            ],
+        }, jsonform)
