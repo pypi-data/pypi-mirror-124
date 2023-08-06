c1c9eeefef62b6e6fa473eb6d53476ec201e1562
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:37:36 +0000

    Add package multilabel-knn/0.0.2/multilabel_knn-0.0.2-py3-none-any.whl

---
+++ packages/multilabel-knn/multilabel_knn-0.0.2-py3-none-any.whl/multilabel_knn/__init__.py
@@ -0,0 +1,4 @@
+from multilabel_knn.binom_multilabel_graph import * 
+from multilabel_knn.binom_multilabel_knn import * 
+from multilabel_knn.knn import * 
+from multilabel_knn.multilabel_knn import * 
