e4b8d86abd7552032798a39d8101add8bdde1767
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:07:59 +0000

    Add package annotell-openlabel/0.0.8/annotell_openlabel-0.0.8-py3-none-any.whl

---
+++ packages/annotell-openlabel/annotell_openlabel-0.0.8-py3-none-any.whl/annotell/openlabel/models/__init__.py
@@ -0,0 +1,12 @@
+from typing import Union
+
+from .models import *
+
+from .models import Type1 as DataTypes
+from .models import Type2 as NumTypes
+from .models import Type3 as RdfTypes
+from .models import Type4 as StreamTypes
+from .models import Type5 as TextTypes
+from .models import Type6 as VecTypes
+
+DataTypeBase = Union[Text, Num, Vec, Boolean]
