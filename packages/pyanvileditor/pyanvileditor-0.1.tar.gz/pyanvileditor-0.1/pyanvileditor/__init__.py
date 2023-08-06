d5f1267ab58bd3ccb265c5382f7884dc7befb6e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:10:05 +0000

    Add package pyanvileditor/0.1/pyanvileditor-0.1.tar.gz

---
+++ packages/pyanvileditor/pyanvileditor-0.1.tar.gz/pyanvileditor-0.1/pyanvileditor/__init__.py
@@ -0,0 +1,7 @@
+from pyanvileditor.biomes import Biome
+from pyanvileditor.materials import Material
+from pyanvileditor.world import World, Chunk, Block, BlockState
+from pyanvileditor.canvas import Canvas
+from pyanvileditor.schematic import Schematic
+
+import pyanvileditor.nbt
---
+++ packages/pyanvileditor/pyanvileditor-0.1.tar.gz/pyanvileditor-0.1/setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+setup(
+  name = 'pyanvileditor',
+  packages = ['pyanvileditor'],
+  version = '0.1',
+  license='MIT',
+  description = 'Python3 library to load and edit minecraft savegames',
+  author = 'Donovan Allen, Mauro Sbicego',
+  author_email = 'mauro@sbicego.ch',
+  url = 'https://github.com/maurosbicego/PyAnvilEditor',
+  download_url = 'https://github.com/maurosbicego/PyAnvilEditor/archive/refs/tags/v0.1.tar.gz',
+  keywords = ['minecraft', 'anvil', 'editor'],
+  install_requires=[],
+  classifiers=[
+    'Development Status :: 4 - Beta'
+  ],
+)
