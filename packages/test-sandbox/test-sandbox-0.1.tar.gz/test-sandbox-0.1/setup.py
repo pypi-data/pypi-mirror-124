b51912b556200e451aa9146987192f8ba027ba8c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:39:06 +0000

    Add package test-sandbox/0.1/test-sandbox-0.1.tar.gz

---
+++ packages/test-sandbox/test-sandbox-0.1.tar.gz/test-sandbox-0.1/setup.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+from setuptools import setup, find_packages
+
+setup(
+    name='test-sandbox',
+    version='0.1',
+    author='Tector Pro',
+    description='Testing something',
+    packages=find_packages(),
+    classifiers = [
+  	'Development Status :: 5 - Production/Stable',
+  	'Intended Audience :: Education',
+  	'Operating System :: Microsoft :: Windows :: Windows 10',
+  	'License :: OSI Approved :: MIT License',
+  	'Programming Language :: Python :: 3'],
+)
