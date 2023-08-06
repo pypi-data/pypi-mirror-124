2e5df7db397e2ad4c69aab1c60350e89841eec34
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:35:48 +0000

    Add package monacelli-pylog-prefs/0.9.0/monacelli_pylog_prefs-0.9.0-py2.py3-none-any.whl

---
+++ packages/monacelli-pylog-prefs/monacelli_pylog_prefs-0.9.0-py2.py3-none-any.whl/monacelli_pylog_prefs/logger.py
@@ -0,0 +1,33 @@
+import logging
+import logging.config
+import logging.handlers
+
+
+def setup(filename=None, stream_level=logging.WARNING, file_level=logging.DEBUG):
+    handlers = []
+
+    format_stream = "{%(filename)s:%(lineno)d} %(levelname)s - %(message)s"  # console
+    formatter_stream = logging.Formatter(format_stream)
+    stream_handler = logging.StreamHandler()
+    stream_handler.setFormatter(formatter_stream)
+    stream_handler.setLevel(stream_level)
+
+    handlers.append(stream_handler)
+
+    if filename:
+        format_file = "[%(asctime)s] p%(process)s {%(filename)s:%(lineno)d} %(levelname)s - %(message)s"
+        formatter_file = logging.Formatter(format_file)
+        file_handler = logging.handlers.RotatingFileHandler(
+            filename,
+            mode="a",
+        )
+        file_handler.setFormatter(formatter_file)
+        file_handler.setLevel(file_level)
+
+        handlers.append(file_handler)
+
+    logging.basicConfig(
+        level=logging.DEBUG,
+        handlers=handlers,
+    )
+    logging.config.dictConfig({"version": 1, "disable_existing_loggers": True})
