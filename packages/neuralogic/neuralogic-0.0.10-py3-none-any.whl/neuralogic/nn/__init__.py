d9136591fc5e99a1dff8f5f719c46ce6eae93c89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:27 +0000

    Add package neuralogic/0.0.10/neuralogic-0.0.10-py3-none-any.whl

---
+++ packages/neuralogic/neuralogic-0.0.10-py3-none-any.whl/neuralogic/nn/__init__.py
@@ -0,0 +1,49 @@
+from typing import Optional
+
+from neuralogic.core.builder import Backend
+from neuralogic.nn.base import AbstractEvaluator
+from neuralogic.core import Template
+from neuralogic.core.settings import Settings
+
+
+def get_neuralogic_layer(backend: Backend, native_backend_models: bool = False):
+    if backend == Backend.DYNET:
+        from neuralogic.nn.dynet import NeuraLogic  # type: ignore
+
+        return NeuraLogic
+    if backend == Backend.JAVA:
+        from neuralogic.nn.java import NeuraLogic  # type: ignore
+
+        return NeuraLogic
+    if backend == Backend.PYG:
+        from neuralogic.nn.native.torch import NeuraLogic
+
+        return NeuraLogic
+    raise NotImplementedError
+
+
+def get_evaluator(
+    backend: Backend,
+    template: Template,
+    settings: Optional[Settings] = None,
+    *,
+    native_backend_models=False,
+):
+    if settings is None:
+        if template is not None:
+            settings = template.java_factory.settings
+        else:
+            settings = Settings()
+
+    if backend == Backend.DYNET:
+        from neuralogic.nn.evaluators.dynet import DynetEvaluator
+
+        return DynetEvaluator(template, settings)
+    if backend == Backend.JAVA:
+        from neuralogic.nn.evaluators.java import JavaEvaluator
+
+        return JavaEvaluator(template, settings)
+    if backend == Backend.PYG:
+        from neuralogic.nn.evaluators.torch import TorchEvaluator
+
+        return TorchEvaluator(template, settings)
