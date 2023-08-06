ba03d9bd990ae3e7b9e44805140ec548328cba60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:20:24 +0000

    Add package ipyflow/0.0.80/ipyflow-0.0.80-py2.py3-none-any.whl

---
+++ packages/ipyflow/ipyflow-0.0.80-py2.py3-none-any.whl/nbplusplus/install.py
@@ -0,0 +1,9 @@
+# -*- coding: future_annotations -*-
+import sys
+
+from nbplusplus.kernel.install import main
+
+
+# this is just a pointer to nbplusplus.kernel.install
+if __name__ == '__main__':
+    sys.exit(main())
