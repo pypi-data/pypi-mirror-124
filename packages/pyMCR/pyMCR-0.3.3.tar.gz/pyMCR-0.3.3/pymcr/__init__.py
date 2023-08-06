e62469eef1cc8c5b65c7cc52e8738a21262e635a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:22:14 +0000

    Add package pyMCR/0.3.3/pyMCR-0.3.3.tar.gz

---
+++ packages/pyMCR/pyMCR-0.3.3.tar.gz/pyMCR-0.3.3/pymcr/__init__.py
@@ -0,0 +1,8 @@
+""" pyMCR: Pythonic Multivariate Curve Resolution - Alternating Least Squares """
+from . import mcr
+from . import constraints
+from . import metrics
+from . import regressors
+from . import condition
+
+from ._version import __version__
