184d1f01aa8c138ff33d920ccc3fabe31122775f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:06:15 +0000

    Add package heroicons/1.4.0/heroicons-1.4.0.tar.gz

---
+++ packages/heroicons/heroicons-1.4.0.tar.gz/heroicons-1.4.0/src/heroicons/_compat.py
@@ -0,0 +1,28 @@
+import sys
+from typing import IO
+
+if sys.version_info < (3, 9):
+
+    def str_removeprefix(self: str, prefix: str) -> str:
+        if self.startswith(prefix):
+            return self[len(prefix) :]
+        else:  # pragma: no cover
+            return self[:]
+
+
+else:
+    str_removeprefix = str.removeprefix
+
+
+if sys.version_info >= (3, 9) or sys.version_info < (3, 7):
+    if sys.version_info >= (3, 9):
+        from importlib.resources import files
+    else:
+        from importlib_resources import files
+
+    def open_binary(pkg: str, filename: str) -> IO[bytes]:
+        return (files(pkg) / filename).open("rb")
+
+
+else:
+    from importlib.resources import open_binary  # noqa: F401
