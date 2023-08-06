e63ee877f1e1ca3afed3847d7c0d373a754f2cd0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:28:26 +0000

    Add package hondana/0.4.6/Hondana-0.4.6-py3-none-any.whl

---
+++ packages/hondana/Hondana-0.4.6-py3-none-any.whl/hondana/types/common.py
@@ -0,0 +1,140 @@
+"""
+The MIT License (MIT)
+
+Copyright (c) 2021-Present AbstractUmbra
+
+Permission is hereby granted, free of charge, to any person obtaining a
+copy of this software and associated documentation files (the "Software"),
+to deal in the Software without restriction, including without limitation
+the rights to use, copy, modify, merge, publish, distribute, sublicense,
+and/or sell copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+DEALINGS IN THE SOFTWARE.
+"""
+
+from __future__ import annotations
+
+from typing import Literal, TypedDict
+
+
+__all__ = (
+    "PublicationDemographic",
+    "ContentRating",
+    "LanguageCode",
+    "LocalizedString",
+    "LocalisedString",
+)
+
+PublicationDemographic = Literal["shounen", "shoujo", "josei", "seinen"]
+ContentRating = Literal["safe", "suggestive", "erotica", "pornographic"]
+LanguageCode = Literal[
+    "en",
+    "ja",
+    "pl",
+    "sh",
+    "nl",
+    "it",
+    "ru",
+    "de",
+    "hu",
+    "fr",
+    "fi",
+    "vi",
+    "el",
+    "bg",
+    "es",
+    "pt-br",
+    "pt",
+    "sv",
+    "ar",
+    "da",
+    "zh",
+    "bn",
+    "ro",
+    "cs",
+    "mn",
+    "tr",
+    "id",
+    "ko",
+    "es-la",
+    "fa",
+    "ms",
+    "th",
+    "ca",
+    "tl",
+    "zh-hk",
+    "uk",
+    "my",
+    "lt",
+    "he",
+    "hi",
+    "no",
+]
+
+LocalizedString = TypedDict(
+    "LocalizedString",
+    {
+        "en": str,
+        "ja": str,
+        "pl": str,
+        "sh": str,
+        "nl": str,
+        "it": str,
+        "ru": str,
+        "de": str,
+        "hu": str,
+        "fr": str,
+        "fi": str,
+        "vi": str,
+        "el": str,
+        "bg": str,
+        "es": str,
+        "pt-br": str,
+        "pt": str,
+        "sv": str,
+        "ar": str,
+        "da": str,
+        "zh": str,
+        "bn": str,
+        "ro": str,
+        "cs": str,
+        "mn": str,
+        "tr": str,
+        "id": str,
+        "ko": str,
+        "es-la": str,
+        "fa": str,
+        "ms": str,
+        "th": str,
+        "ca": str,
+        "tl": str,
+        "zh-hk": str,
+        "uk": str,
+        "my": str,
+        "lt": str,
+        "he": str,
+        "hi": str,
+        "no": str,
+    },
+    total=False,
+)
+"""
+Examples
+---------
+A localised string: ::
+
+    {"en": "An english string"}
+    {"fr": "Une corde française"}
+
+"""
+LocalisedString = LocalizedString
