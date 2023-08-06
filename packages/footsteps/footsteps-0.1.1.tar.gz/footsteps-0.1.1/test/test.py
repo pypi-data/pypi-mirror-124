701499f719c592ad4430c6f7137a8807d2d14436
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:25:34 +0000

    Add package footsteps/0.1.1/footsteps-0.1.1.tar.gz

---
+++ packages/footsteps/footsteps-0.1.1.tar.gz/footsteps-0.1.1/test/test.py
@@ -0,0 +1,34 @@
+import unittest
+import subprocess
+import time
+
+from subprocess import PIPE, Popen
+
+import os.path
+
+class FootstepsTestCase(unittest.TestCase):
+    def testFootsteps(self):
+        output = subprocess.Popen(["python", "test/example_program.py", "horseradish"], stdin=PIPE, stdout=PIPE).communicate(b"my_results_name")
+        
+        self.assertTrue(os.path.exists("results/my_results_name/info.txt"))
+        
+        with open("results/my_results_name/info.txt", "r") as f:
+            print("Current info format:\n=========================")
+            print(f.read())
+        
+    def testFootstepsFallbackPath(self):
+        output = subprocess.Popen(["python", "test/example_program.py", "horseradish"], stdin=PIPE, stdout=PIPE).communicate(b"preexisting_results_name")
+        output = subprocess.Popen(["python", "test/example_program.py", "horseradish"], stdin=PIPE, stdout=PIPE).communicate(b"preexisting_results_name")
+        output = subprocess.Popen(["python", "test/example_program.py", "horseradish"], stdin=PIPE, stdout=PIPE).communicate(b"preexisting_results_name")
+        
+        self.assertTrue(os.path.exists("results/preexisting_results_name(2)/info.txt"))
+        
+    def testNeedsGitDir(self):
+        os.chdir("..")
+        try:
+            import footsteps
+            raise Exception("Should have failed")
+        except Exception as e:
+            self.assertTrue(str(e) == "code that uses footsteps needs to be run in a git directory to record the git hash assosciated with this experiment")
+        
+        
