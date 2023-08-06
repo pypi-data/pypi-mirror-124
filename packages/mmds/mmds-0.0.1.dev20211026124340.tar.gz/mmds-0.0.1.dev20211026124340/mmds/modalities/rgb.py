54ae542048a33e284bac65c4de27a0c3c0f3430d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:13:34 +0000

    Add package mmds/0.0.1.dev20211026124340/mmds-0.0.1.dev20211026124340.tar.gz

---
+++ packages/mmds/mmds-0.0.1.dev20211026124340.tar.gz/mmds-0.0.1.dev20211026124340/mmds/modalities/rgb.py
@@ -0,0 +1,37 @@
+import attr
+import warnings
+from typing import Callable
+from mmds.exceptions import PackageNotFoundError
+
+try:
+    from PIL import Image
+except:
+    raise PackageNotFoundError("pillow", by="rgb modality.")
+
+from .modality import Modality
+
+
+dumb_image = Image.new("RGB", (32, 32))
+
+
+@attr.define
+class RgbModality(Modality):
+    """A rgb modality for image."""
+
+    transform: Callable
+
+    def _fetch_impl(self, *, info={}):
+        return self.transform(self._load_pil(self.path))
+
+    @staticmethod
+    def _load_pil(path):
+        if path is None:
+            return dumb_image
+
+        try:
+            image = Image.open(path)
+        except:
+            warnings.warn(f"Open {path} failed, use an empty picture instead.")
+            image = dumb_image
+
+        return image
