3b6e0717b92bce66bbfbe39b90794abeda243372
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:15:14 +0000

    Add package fastrlock/0.7/fastrlock-0.7.tar.gz

---
+++ packages/fastrlock/fastrlock-0.7.tar.gz/fastrlock-0.7/lockbench.py
@@ -0,0 +1,127 @@
+
+from threading import Thread
+from threading import RLock
+from fastrlock.rlock import FastRLock as FLock
+
+
+def lock_unlock(l):
+    l.acquire()
+    l.release()
+    l.acquire()
+    l.release()
+    l.acquire()
+    l.release()
+    l.acquire()
+    l.release()
+    l.acquire()
+    l.release()
+
+
+def reentrant_lock_unlock(l):
+    l.acquire()
+    l.acquire()
+    l.acquire()
+    l.acquire()
+    l.acquire()
+    l.release()
+    l.release()
+    l.release()
+    l.release()
+    l.release()
+
+
+def mixed_lock_unlock(l):
+    l.acquire()
+    l.release()
+    l.acquire()
+    l.acquire()
+    l.release()
+    l.acquire()
+    l.release()
+    l.acquire()
+    l.release()
+    l.release()
+
+
+def context_manager(l):
+    with l: pass
+    with l:
+        with l:
+            with l: pass
+            with l: pass
+        with l:
+            with l: pass
+        with l:
+            with l: pass
+            with l: pass
+    with l: pass
+    with l:
+        with l:
+            with l: pass
+            with l: pass
+            with l:
+                with l: pass
+    with l: pass
+
+
+def lock_unlock_nonblocking(l):
+    if l.acquire(False):
+        l.release()
+    if l.acquire(False):
+        l.release()
+    if l.acquire(False):
+        l.release()
+    if l.acquire(False):
+        l.release()
+    if l.acquire(False):
+        l.release()
+
+
+def threaded(l, test_func, tcount=10):
+    threads = [ Thread(target=test_func, args=(l,)) for _ in range(tcount) ]
+    for thread in threads:
+        thread.start()
+    for thread in threads:
+        thread.join()
+
+
+if __name__ == '__main__':
+    functions = [
+        lock_unlock,
+        reentrant_lock_unlock,
+        mixed_lock_unlock,
+        lock_unlock_nonblocking,
+        context_manager,
+        ]
+
+    import sys
+    from timeit import Timer
+    from functools import partial
+
+    repeat_count = 100000
+    repeat_count_t = 1000
+
+    rlock, flock = ('threading.RLock', RLock()), ('FastRLock', FLock())
+    locks = []
+    args = sys.argv[1:]
+    if 'rlock' in args:
+        locks.append(rlock)
+    if 'flock' in args:
+        locks.append(flock)
+    if not locks:
+        locks = [rlock, flock]
+    for _ in range(args.count('quick')):
+        repeat_count = max(10, repeat_count // 100)
+        repeat_count_t = max(5, repeat_count_t // 10)
+
+    for name, lock in locks:
+        print('Testing %s' % name)
+        print("sequential (x%d):" % repeat_count)
+        for function in functions:
+            timer = Timer(partial(function, lock))
+            print('%-25s: %9.2f msec' % (function.__name__, max(timer.repeat(repeat=4, number=repeat_count)) * 1000.0))
+
+        print("threaded 10T (x%d):" % repeat_count_t)
+        for function in functions:
+            timer = Timer(partial(threaded, lock, function))
+            print('%-25s: %9.2f msec' % (function.__name__, max(timer.repeat(repeat=4, number=repeat_count_t)) * 1000.0))
