562e51b9f0729a2bd727ad146a36ae50718a32e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:36:52 +0000

    Add package cloudrail-knowledge/0.0.161/cloudrail_knowledge-0.0.161-py3-none-any.whl

---
+++ packages/cloudrail-knowledge/cloudrail_knowledge-0.0.161-py3-none-any.whl/cloudrail/knowledge/drift_detection/azure_environment_context_drift_detector.py
@@ -0,0 +1,13 @@
+from cloudrail.knowledge.context.mergeable import Mergeable
+from cloudrail.knowledge.drift_detection.base_environment_context_drift_detector import BaseEnvironmentContextDriftDetector
+
+
+class AzureEnvironmentContextDriftDetector(BaseEnvironmentContextDriftDetector):
+
+    @classmethod
+    def supported_drift_resource(cls, mergeable: Mergeable):
+        pass
+
+    @classmethod
+    def convert_to_drift_detection_object(cls, mergeable: Mergeable):
+        return mergeable.to_drift_detection_object()
