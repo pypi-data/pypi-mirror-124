e73c4e9103d6057a48524d519bb9e45f37716703
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:49 +0000

    Add package pkg-cli/3.1.0/pkg-cli-3.1.0.tar.gz

---
+++ packages/pkg-cli/pkg-cli-3.1.0.tar.gz/pkg-cli-3.1.0/pkg/commands.py
@@ -0,0 +1,33 @@
+from .decorators import setup_file_required
+from configparser import ConfigParser
+from .utils import python
+from . import PROJECT_DIR
+
+
+def init(args):
+    config = ConfigParser()
+    config['metadata'] = {
+        'name': '<package_name>',
+        'version': '0.1.0',
+    }
+    config['options'] = {
+        'packages': 'find:'
+    }
+
+    file = PROJECT_DIR / 'setup.cfg'
+    if file.exists() and file.is_file():
+        print('Warning: The "setup.cfg" file exists!!')
+        return
+
+    with open(str(file), 'w') as outfile:
+        config.write(outfile)
+
+    if not (PROJECT_DIR / 'setup.py').exists():
+        (PROJECT_DIR / 'setup.py').touch()
+        (PROJECT_DIR / 'setup.py').write_text('from setuptools import setup\n\nsetup()\n')
+
+
+@setup_file_required
+def publish(args):
+    python('-m', 'build')
+    python('-m', 'twine', 'upload', 'dist/*')
