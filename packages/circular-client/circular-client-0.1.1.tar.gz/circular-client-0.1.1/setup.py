14f8f2c3f29815127a2a4398e4ea0a258152835d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:28:37 +0000

    Add package circular-client/0.1.1/circular-client-0.1.1.tar.gz

---
+++ packages/circular-client/circular-client-0.1.1.tar.gz/circular-client-0.1.1/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['circular_client']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['python-dateutil>=2.8.2,<3.0.0', 'requests>=2.23.0,<3.0.0']
+
+setup_kwargs = {
+    'name': 'circular-client',
+    'version': '0.1.1',
+    'description': '',
+    'long_description': None,
+    'author': 'CircularNetwork',
+    'author_email': 'info@circularnetwork.io',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
