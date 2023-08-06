2711060e6b67b3b999e65eef097fd45fccf6e06e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:14:02 +0000

    Add package instrumentum/0.6.0/instrumentum-0.6.0.tar.gz

---
+++ packages/instrumentum/instrumentum-0.6.0.tar.gz/instrumentum-0.6.0/src/instrumentum/__init__.py
@@ -0,0 +1,9 @@
+# read version from installed package
+from importlib.metadata import version
+import logging
+
+logging.basicConfig(format='%(asctime)s | %(levelname)s | %(message)s', 
+                    datefmt='%y-%m-%d %H:%M')
+
+__version__ = version("instrumentum")
+
