6d361e028cc509d91a2f1100e4e43111c50a51e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:45:40 +0000

    Add package hdl-registers/2.0.1/hdl_registers-2.0.1.tar.gz

---
+++ packages/hdl-registers/hdl_registers-2.0.1.tar.gz/hdl_registers-2.0.1/hdl_registers/about.py
@@ -0,0 +1,61 @@
+# --------------------------------------------------------------------------------------------------
+# Copyright (c) Lukas Vik. All rights reserved.
+#
+# This file is part of the hdl_registers project.
+# https://hdl-registers.com
+# https://gitlab.com/tsfpga/hdl_registers
+# --------------------------------------------------------------------------------------------------
+
+
+def get_slogan():
+    return (
+        "The hdl_registers project is a HDL register generator "
+        "fast enough to be run in real time."
+    )
+
+
+def get_readme_rst(include_website_link):
+    """
+    Get the complete README.rst (to be used on website and in PyPI release).
+    RST file inclusion in README.rst does not work on gitlab unfortunately, hence this
+    cumbersome handling where the README is duplicated in two places.
+
+    Arguments:
+        include_website_link (bool): Include a link to the website in README.
+    """
+
+    extra_rst = (
+        "**See documentation on the website**: https://hdl-registers.com\n"
+        if include_website_link
+        else ""
+    )
+
+    readme_rst = f"""\
+About ``hdl_registers``
+=======================
+
+|pic_website| |pic_gitlab| |pic_gitter| |pic_license|
+
+.. |pic_website| image:: https://hdl-registers.com/badges/website.svg
+  :alt: Website
+  :target: https://hdl-registers.com
+
+.. |pic_gitlab| image:: https://hdl-registers.com/badges/gitlab.svg
+  :alt: Gitlab
+  :target: https://gitlab.com/tsfpga/hdl_registers
+
+.. |pic_gitter| image:: https://badges.gitter.im/owner/repo.png
+  :alt: Gitter
+  :target: https://gitter.im/tsfpga/tsfpga
+
+.. |pic_license| image:: https://hdl-registers.com/badges/license.svg
+  :alt: License
+  :target: https://hdl-registers.com/license_information.html
+
+{get_slogan()}
+
+{extra_rst}
+TBC...
+"""
+
+    return readme_rst
