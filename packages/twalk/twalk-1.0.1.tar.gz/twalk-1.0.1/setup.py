4b1e8b7eef5d7225d56c10fc4b60c1691f833571
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:58 +0000

    Add package twalk/1.0.1/twalk-1.0.1.tar.gz

---
+++ packages/twalk/twalk-1.0.1.tar.gz/twalk-1.0.1/setup.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+modules = \
+['twalk']
+entry_points = \
+{'console_scripts': ['cli_command_name = twalk:main']}
+
+setup_kwargs = {
+    'name': 'twalk',
+    'version': '1.0.1',
+    'description': 'Condense a directory tree into a single txt file or extract it from one',
+    'long_description': None,
+    'author': 'Ovsyanka',
+    'author_email': 'szmiev2000@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/Ovsyanka83/twalk',
+    'py_modules': modules,
+    'entry_points': entry_points,
+    'python_requires': '>=3.6,<4.0',
+}
+
+
+setup(**setup_kwargs)
