778acc171e53ffe20f4d150621faee1e857fe242
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:14 +0000

    Add package api-manager-verifier/0.1.15/api_manager_verifier-0.1.15-py2.py3-none-any.whl

---
+++ packages/api-manager-verifier/api_manager_verifier-0.1.15-py2.py3-none-any.whl/data_verifier/exceptions/exception.py
@@ -0,0 +1,17 @@
+class VerifierRequestException(Exception):
+    pass
+
+class BackendError(Exception):
+    pass
+
+class SessionRetrieveException(Exception):
+    pass
+
+class VerificationError(Exception):
+    pass
+
+class NotFromPCM(Exception):
+    pass
+
+class EmptyDataFound(Exception):
+    pass
