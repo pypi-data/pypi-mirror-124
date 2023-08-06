cb30bb287d85dd7496baea085d1548b5ae2c1278
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:31:11 +0000

    Add package asgi-correlation-id/0.1.1/asgi-correlation-id-0.1.1.tar.gz

---
+++ packages/asgi-correlation-id/asgi-correlation-id-0.1.1.tar.gz/asgi-correlation-id-0.1.1/README.md
@@ -0,0 +1,8 @@
+[![test](https://github.com/snok/asgi-correlation-id/actions/workflows/test.yml/badge.svg)](https://github.com/snok/asgi-correlation-id/actions/workflows/test.yml)
+[![codecov](https://codecov.io/gh/snok/asgi-correlation-id/branch/master/graph/badge.svg?token=1aXlWPm2gb)](https://codecov.io/gh/snok/asgi-correlation-id)
+[![pypi](https://pypi.org/pypi/asgi-correlation-id)](https://github.com/snok/asgi-correlation-id)
+
+
+# ASGI Correlation ID middleware
+
+This is a rewrite of [django-guid](django-guid) for ASGI apps.
---
+++ packages/asgi-correlation-id/asgi-correlation-id-0.1.1.tar.gz/asgi-correlation-id-0.1.1/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['asgi_correlation_id']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'asgi-correlation-id',
+    'version': '0.1.1',
+    'description': 'Middleware that propagates HTTP header correlation IDs to project logs',
+    'long_description': '[![test](https://github.com/snok/asgi-correlation-id/actions/workflows/test.yml/badge.svg)](https://github.com/snok/asgi-correlation-id/actions/workflows/test.yml)\n[![codecov](https://codecov.io/gh/snok/asgi-correlation-id/branch/master/graph/badge.svg?token=1aXlWPm2gb)](https://codecov.io/gh/snok/asgi-correlation-id)\n[![pypi](https://pypi.org/pypi/asgi-correlation-id)](https://github.com/snok/asgi-correlation-id)\n\n\n# ASGI Correlation ID middleware\n\nThis is a rewrite of [django-guid](django-guid) for ASGI apps.\n',
+    'author': 'Jonas Krüger Svensson',
+    'author_email': 'jonas-ks@hotmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': 'https://github.com/snok/asgi-correlation-id',
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
