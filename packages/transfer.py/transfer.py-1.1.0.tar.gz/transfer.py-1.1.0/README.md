af185da3a82809ee8b96c724f9aaab0df62e6adf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:25 +0000

    Add package transfer.py/1.1.0/transfer.py-1.1.0.tar.gz

---
+++ packages/transfer.py/transfer.py-1.1.0.tar.gz/transfer.py-1.1.0/README.md
@@ -0,0 +1,3 @@
+hello, you may use this program
+
+it was originally intended for transfer of AIs between computers
---
+++ packages/transfer.py/transfer.py-1.1.0.tar.gz/transfer.py-1.1.0/main.c
@@ -0,0 +1,28 @@
+#include <Python.h>
+#include <marshal.h>
+PyObject * sort_fputs(PyObject * self, PyObject *args) {
+  PyObject *to_write;
+  char *where;
+  if(!PyArg_ParseTuple(args, "Os", &to_write, &where)) {
+    return NULL;
+  }
+  FILE *f = fopen(where, "r");
+  PyMarshal_WriteObjectToFile(to_write, f, 3);
+  return NULL;
+}
+static PyMethodDef FputsMethods[] = {
+    {"write_obj", sort_fputs, METH_VARARGS, "writes object"},
+    {NULL, NULL, 0, NULL}
+};
+
+static struct PyModuleDef fputsmodule = {
+    PyModuleDef_HEAD_INIT,
+    "write_obj",
+    "writes object",
+    -1,
+    FputsMethods
+};
+
+PyMODINIT_FUNC PyInit_fputs(void) {
+    return PyModule_Create(&fputsmodule);
+}
