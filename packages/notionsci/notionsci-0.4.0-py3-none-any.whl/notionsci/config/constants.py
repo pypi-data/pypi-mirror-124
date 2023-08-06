b5c517910d686a567b95d33c02a64c69555f8c2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:26:30 +0000

    Add package notionsci/0.4.0/notionsci-0.4.0-py3-none-any.whl

---
+++ packages/notionsci/notionsci-0.4.0-py3-none-any.whl/notionsci/config/constants.py
@@ -0,0 +1,8 @@
+CONFIG_VERSION = 1
+APP_NAME = "notionsci"
+DEFAULT_PROFLE = "default"
+OVERRIDE_CONFIG_NAME = "config.yml"
+
+TEMPLATE_ZOTERO = 'https://efficacious-alarm-7cc.notion.site/Zotero-Library-908bf67ef61048b79a1a10dfa5826302'
+
+DEV_TESTS_PAGE = "https://www.notion.so/NotionSci-Tests-22ecab6188d147ef83fa455e2694395b"
