075f76fe929c60819b54cea0060daa90acd47bb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:30:47 +0000

    Add package weboa/0.6.8/weboa-0.6.8.tar.gz

---
+++ packages/weboa/weboa-0.6.8.tar.gz/weboa-0.6.8/weboa/__init__.py
@@ -0,0 +1,9 @@
+__VERSION__ = "0.6.8"
+
+import os
+import json
+from . import prepare
+from .project import *
+from .utils import *
+
+__VERSION__ = __VERSION__
---
+++ packages/weboa/weboa-0.6.8.tar.gz/weboa-0.6.8/weboa_res/pwa/serviceWorker.js
@@ -0,0 +1,18 @@
+const staticDevCoffee = "weboa-pwa-app"
+const assets = ["/","/css/styles.css"]
+
+self.addEventListener("install", installEvent => {
+  installEvent.waitUntil(
+    caches.open(staticDevCoffee).then(cache => {
+      cache.addAll(assets)
+    })
+  )
+})
+
+self.addEventListener("fetch", fetchEvent => {
+  fetchEvent.respondWith(
+    caches.match(fetchEvent.request).then(res => {
+      return res || fetch(fetchEvent.request)
+    })
+  )
+})
