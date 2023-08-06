9449f1dc503f2c0a672ac3df3ff4d557e47b9d3f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:06:10 +0000

    Add package namu/0.0.4/namu-0.0.4.tar.gz

---
+++ packages/namu/namu-0.0.4.tar.gz/namu-0.0.4/namu/main.py
@@ -0,0 +1,15 @@
+from .namu import run
+
+def interpreter():
+    """나무 인프리터를 실행합니다."""
+    while True:
+        text = input('나무 > ')
+        if text.strip() == '': continue
+        result, error = run('<입력>', text)
+        
+        if error: print(error.as_string())
+        elif result:
+            if hasattr(result, 'elements') and len(result.elements) == 1:
+                print(repr(result.elements[0]))
+            else:
+                print(repr(result))
