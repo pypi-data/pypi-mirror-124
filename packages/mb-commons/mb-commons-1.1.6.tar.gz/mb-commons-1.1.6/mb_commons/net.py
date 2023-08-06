d98072ea1d60851e22f7e4baefa9892f950cce8b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:37 +0000

    Add package mb-commons/1.1.6/mb-commons-1.1.6.tar.gz

---
+++ packages/mb-commons/mb-commons-1.1.6.tar.gz/mb-commons-1.1.6/mb_commons/net.py
@@ -0,0 +1,12 @@
+import socket
+import time
+
+
+def check_port(ip: str, port: int, attempts=3, sleep_seconds=1) -> bool:
+    for _ in range(attempts):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        res = sock.connect_ex((ip, port)) == 0
+        if res:
+            return True
+        time.sleep(sleep_seconds)
+    return False
