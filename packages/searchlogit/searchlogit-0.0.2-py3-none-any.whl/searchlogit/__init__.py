5341a07fa6505905cbac44ae7b2ec8b092316854
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:14:36 +0000

    Add package searchlogit/0.0.2/searchlogit-0.0.2-py3-none-any.whl

---
+++ packages/searchlogit/searchlogit-0.0.2-py3-none-any.whl/searchlogit/__init__.py
@@ -0,0 +1,6 @@
+from .mixed_logit import MixedLogit
+from .multinomial_logit import MultinomialLogit
+from .latent_class_model import LatentClassModel
+from .latent_class_model_mixed import LatentClassModelMixed
+from .search import Search
+from ._device import device
