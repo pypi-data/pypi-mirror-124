73d5ef013b29c156803cee641c8303dd51707322
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:40:55 +0000

    Add package libcovebods/0.12.0/libcovebods-0.12.0.tar.gz

---
+++ packages/libcovebods/libcovebods-0.12.0.tar.gz/libcovebods-0.12.0/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='libcovebods',
+    version='0.12.0',
+    author='Open Data Services',
+    author_email='code@opendataservices.coop',
+    url='https://github.com/openownership/lib-cove-bods',
+    description='A data review library',
+    packages=find_packages(),
+    long_description='A data review library',
+    python_requires='>=3.6',
+    install_requires=[
+        'python-dateutil',
+        'Django>2.2,<2.3',
+        'flattentool>=0.5.0',
+        'libcove>=0.22.0',
+        'libcoveweb>=0.21.0'
+    ],
+    extras_require={
+        'dev': ['pytest', 'flake8']
+    },
+    classifiers=[
+            'License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)',
+    ],
+    entry_points='''[console_scripts]
+libcovebods = libcovebods.cli.__main__:main''',
+    include_package_data=True,
+)
