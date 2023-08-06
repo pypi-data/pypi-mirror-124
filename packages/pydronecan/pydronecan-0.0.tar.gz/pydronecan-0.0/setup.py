16b625f842c70114b9d9a915f18e24842de4edb9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:45 +0000

    Add package pydronecan/0.0/pydronecan-0.0.tar.gz

---
+++ packages/pydronecan/pydronecan-0.0.tar.gz/pydronecan-0.0/setup.py
@@ -0,0 +1,19 @@
+# encoding: utf8
+
+from setuptools import setup
+
+
+setup(
+    author='Pavel Kirienko, Ben Dyer',
+    author_email='dronecan.devel@gmail.com',
+    description='Python implementation of the DroneCAN protocol stack',
+    install_requires=['dronecan'],
+    long_description='Use `dronecan <https://pypi.python.org/pypi/dronecan/>`_ instead.',
+    maintainer='UNKNOWN',
+    maintainer_email='UNKNOWN',
+    name='pydronecan',
+    platforms=['all'],
+    url='https://pypi.python.org/pypi/dronecan/',
+    version='0.0',
+    zip_safe=False,
+)
