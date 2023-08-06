4e151ef065b1bee62f7cc1a4c5bc5ae00bbd744a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:10:32 +0000

    Add package dask-bigquery/2021.10.1/dask-bigquery-2021.10.1.tar.gz

---
+++ packages/dask-bigquery/dask-bigquery-2021.10.1.tar.gz/dask-bigquery-2021.10.1/dask_bigquery/__init__.py
@@ -0,0 +1,3 @@
+from .core import read_gbq
+
+__version__ = "2021.10.1"
---
+++ packages/dask-bigquery/dask-bigquery-2021.10.1.tar.gz/dask-bigquery-2021.10.1/setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="dask-bigquery",
+    version="2021.10.1",
+    description="Dask + BigQuery intergration",
+    license="BSD",
+    packages=["dask_bigquery"],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    python_requires=">=3.7",
+    install_requires=open("requirements.txt").read().strip().split("\n"),
+    extras_require={
+        "test": ["pytest", "pandas-gbq", "distributed", "google-auth>=1.30.0"]
+    },
+    include_package_data=True,
+    zip_safe=False,
+)
