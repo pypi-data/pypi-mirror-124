4846fe31158dd07dbef64a586912dc7d1c2c0089
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:14:45 +0000

    Add package validata-core/0.8.3/validata_core-0.8.3-py3-none-any.whl

---
+++ packages/validata-core/validata_core-0.8.3-py3-none-any.whl/validata_core/custom_checks/__init__.py
@@ -0,0 +1,23 @@
+from .cohesive_columns_value import CohesiveColumnsValue
+from .compare_columns_value import CompareColumnsValue
+from .french_siren_value import FrenchSirenValue
+from .french_siret_value import FrenchSiretValue
+from .missing_required_header import MissingRequiredHeader  # noqa
+from .nomenclature_actes_value import NomenclatureActesValue
+from .opening_hours_value import OpeningHoursValue
+from .phone_number_value import PhoneNumberValue
+from .sum_columns_value import SumColumnsValue
+from .year_interval_value import YearIntervalValue
+
+# Please keep the below dict up-to-date
+available_checks = {
+    "cohesive-columns-value": CohesiveColumnsValue,
+    "compare-columns-value": CompareColumnsValue,
+    "french-siren-value": FrenchSirenValue,
+    "french-siret-value": FrenchSiretValue,
+    "nomenclature-actes-value": NomenclatureActesValue,
+    "opening-hours-value": OpeningHoursValue,
+    "phone-number-value": PhoneNumberValue,
+    "sum-columns-value": SumColumnsValue,
+    "year-interval-value": YearIntervalValue,
+}
