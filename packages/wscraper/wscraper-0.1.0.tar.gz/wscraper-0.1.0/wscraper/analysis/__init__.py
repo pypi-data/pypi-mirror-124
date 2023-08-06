01ce8ec58c7e2baf75b5b7798f5659c99bb208c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:31:22 +0000

    Add package wscraper/0.1.0/wscraper-0.1.0.tar.gz

---
+++ packages/wscraper/wscraper-0.1.0.tar.gz/wscraper-0.1.0/wscraper/analysis/__init__.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2021 T.Furukawa
+# This software is released under the MIT License, see LICENSE.
+
+
+from .parser import *
+from .page_iterator import *
+from .both_iterator import *
+from .entry_iterator import *
+from .redirection_iterator import *
