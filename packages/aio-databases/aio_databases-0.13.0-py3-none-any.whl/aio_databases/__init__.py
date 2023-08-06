92f5f1cf96bf9ef18e7fc35c233dc1b281678dc1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:15:36 +0000

    Add package aio-databases/0.13.0/aio_databases-0.13.0-py3-none-any.whl

---
+++ packages/aio-databases/aio_databases-0.13.0-py3-none-any.whl/aio_databases/__init__.py
@@ -0,0 +1,18 @@
+"""Async support for various databases."""
+
+from __future__ import annotations
+
+import contextvars
+import logging
+
+
+__version__ = '0.13.0'
+
+
+logger: logging.Logger = logging.getLogger('aio-databases')
+logger.addHandler(logging.NullHandler())
+
+current_conn = contextvars.ContextVar('aio-db-ses', default=None)
+
+
+from .database import Database  # noqa
