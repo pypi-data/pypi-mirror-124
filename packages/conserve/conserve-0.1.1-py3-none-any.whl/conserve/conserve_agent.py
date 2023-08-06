abbfc93efde3cfb3b23a64d1f9747b75ed1b6ce9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:15:20 +0000

    Add package conserve/0.1.1/conserve-0.1.1-py3-none-any.whl

---
+++ packages/conserve/conserve-0.1.1-py3-none-any.whl/conserve/conserve_agent.py
@@ -0,0 +1,25 @@
+import os
+import sys
+import random
+import math
+from tqdm import tqdm
+import numpy as np
+import pandas as pd
+import warnings
+warnings.filterwarnings("ignore")
+
+
+class Agent:
+    """
+
+    """
+    def __init__(self):
+        pass
+
+
+class ConserveAgent(Agent):
+    """
+
+    """
+    def __init__(self):
+        super().__init__()
