e609decff5462e0c9d01e991b34aeb152691d05f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:57 +0000

    Add package st-btn-select/0.1.2/st_btn_select-0.1.2.tar.gz

---
+++ packages/st-btn-select/st_btn_select-0.1.2.tar.gz/st_btn_select-0.1.2/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+setuptools.setup(
+    name="st_btn_select",
+    version="0.1.2",
+    author="0phoff",
+    description="Streamlit Button Selection Component",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url="https://github.com/0phoff/st-btn-select",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    classifiers=[],
+    license="MIT",
+    license_files=('LICENSE'),
+    python_requires=">=3.6",
+    install_requires=[
+        "streamlit >= 0.63",
+    ],
+)
