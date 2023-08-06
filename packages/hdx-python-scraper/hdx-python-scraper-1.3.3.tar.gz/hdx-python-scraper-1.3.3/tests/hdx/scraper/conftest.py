0e4cea178aab46c962d5223f4f8efab3dc679eaa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:21:11 +0000

    Add package hdx-python-scraper/1.3.3/hdx-python-scraper-1.3.3.tar.gz

---
+++ packages/hdx-python-scraper/hdx-python-scraper-1.3.3.tar.gz/hdx-python-scraper-1.3.3/tests/hdx/scraper/conftest.py
@@ -0,0 +1,31 @@
+"""Global fixtures"""
+from os.path import join
+
+import pytest
+from hdx.hdx_configuration import Configuration
+from hdx.hdx_locations import Locations
+from hdx.location.country import Country
+
+
+@pytest.fixture(scope="session")
+def configuration():
+    Configuration._create(
+        hdx_read_only=True,
+        hdx_site="prod",
+        user_agent="test",
+        project_config_yaml=join("tests", "config", "project_configuration.yml"),
+    )
+    Locations.set_validlocations(
+        [
+            {"name": "afg", "title": "Afghanistan"},
+            {"name": "phl", "title": "Philippines"},
+            {"name": "pse", "title": "State of Palestine"},
+        ]
+    )
+    Country.countriesdata(use_live=False)
+    return Configuration.read()
+
+
+@pytest.fixture(scope="session")
+def fixtures():
+    return join("tests", "fixtures")
