d4f0690a062f72830f9291984b86af871d322d2a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:25:57 +0000

    Add package tiled/0.1.0a43/tiled-0.1.0a43.tar.gz

---
+++ packages/tiled/tiled-0.1.0a43.tar.gz/tiled-0.1.0a43/tiled/examples/toy_authentication.py
@@ -0,0 +1,30 @@
+"""
+See example_configs/toy_authentication.yml for
+server configuration that runs this example.
+"""
+import numpy
+
+from tiled.readers.array import ArrayAdapter
+from tiled.trees.in_memory import SimpleAccessPolicy, Tree
+from tiled.utils import SpecialUsers
+
+# Specify which entries each user is allowed to use.
+# SpecialUsers.public is a sentinel that means anyone can access.
+access_policy = SimpleAccessPolicy(
+    {
+        SpecialUsers.public: ["A"],
+        "alice": ["A", "B"],
+        "bob": ["A", "C"],
+        "cara": SimpleAccessPolicy.ALL,
+    }
+)
+# Make a Tree with a couple arrays in it.
+tree = Tree(
+    {
+        "A": ArrayAdapter.from_array(10 * numpy.ones((10, 10))),
+        "B": ArrayAdapter.from_array(20 * numpy.ones((10, 10))),
+        "C": ArrayAdapter.from_array(30 * numpy.ones((10, 10))),
+        "D": ArrayAdapter.from_array(30 * numpy.ones((10, 10))),
+    },
+    access_policy=access_policy,
+)
