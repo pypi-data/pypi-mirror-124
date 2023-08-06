15c0906548f7b612209a076ee0e50ad34eef4b68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:07:06 +0000

    Add package kolibri-ml/1.0.44/kolibri_ml-1.0.44-py3-none-any.whl

---
+++ packages/kolibri-ml/kolibri_ml-1.0.44-py3-none-any.whl/kolibri/__init__.py
@@ -0,0 +1,4 @@
+from kolibri.config import ModelConfig
+from kolibri.model_loader import ModelLoader
+from kolibri.model_trainer import ModelTrainer
+from kolibri.task.text.intents import domains
---
+++ packages/kolibri-ml/kolibri_ml-1.0.44-py3-none-any.whl/kolibri/utils/common.py
@@ -0,0 +1,65 @@
+import os
+import numpy as np
+
+def construct_learner_name(fold, repeat, repeats):
+    repeat_str = f"_repeat_{repeat}" if repeats > 1 else ""
+    return f"learner_fold_{fold}{repeat_str}"
+
+
+def is_tree(estimator):
+    from sklearn.tree import BaseDecisionTree
+    from sklearn.ensemble._forest import BaseForest
+
+    if "final_estimator" in estimator.get_params():
+        estimator = estimator.final_estimator
+    if "base_estimator" in estimator.get_params():
+        estimator = estimator.base_estimator
+    if isinstance(estimator, BaseForest) or isinstance(estimator, BaseDecisionTree):
+        return True
+
+
+
+def learner_name_to_fold_repeat(name):
+    fold, repeat = None, None
+    arr = name.split("_")
+    fold = int(arr[2])
+    if "repeat" in name:
+        repeat = int(arr[4])
+    return fold, repeat
+
+
+def get_fold_repeat_cnt(model_path):
+    training_logs = [f for f in os.listdir(model_path) if "_training.log" in f]
+    fold_cnt, repeat_cnt = 0, 0
+    for fname in training_logs:
+        fold, repeat = learner_name_to_fold_repeat(fname)
+        if fold is not None:
+            fold_cnt = max(fold_cnt, fold)
+        if repeat is not None:
+            repeat_cnt = max(repeat_cnt, repeat)
+
+    fold_cnt += 1  # counting from 0
+    repeat_cnt += 1
+
+    return fold_cnt, repeat_cnt
+
+
+def get_learners_names(model_path):
+    postfix = "_training.log"
+    learner_names = [
+        f.repleace(postfix, "") for f in os.listdir(model_path) if postfix in f
+    ]
+    return learner_names
+
+
+def estimate_weight(error):
+    """
+    to estimate weights for base estimators based on the error rates
+    :param error: error rates
+    :return:
+    """
+    if error < 0.0000001:
+        error = 0.0000001
+    if error == 1:
+        error = 0.9999999
+    return 0.5*np.log((1-error)/error)
