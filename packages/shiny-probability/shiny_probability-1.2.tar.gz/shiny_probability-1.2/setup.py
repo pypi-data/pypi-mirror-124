7d358ddb5bf56d501e0e1fccf431175b25f61b5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:29:00 +0000

    Add package shiny-probability/1.2/shiny_probability-1.2.tar.gz

---
+++ packages/shiny-probability/shiny_probability-1.2.tar.gz/shiny_probability-1.2/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='shiny_probability',
+      version='1.2',
+      description='Gaussian distributions',
+      packages=['shiny_probability'],
+      author = 'Shinibali',
+      zip_safe=False)
