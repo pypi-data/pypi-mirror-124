a874dc207d608352c6d0ffbc1ed9d32fbf9b24d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:42:16 +0000

    Add package scripting-tools/0.1.0/scripting_tools-0.1.0.tar.gz

---
+++ packages/scripting-tools/scripting_tools-0.1.0.tar.gz/scripting_tools-0.1.0/README.md
@@ -0,0 +1,4 @@
+<h2> A package which contains a bunch of small helpful devtools while scripting </h2>
+
+> To install this package :\
+`pip install -i https://test.pypi.org/simple/ scripting-tools==0.1.5` (old)
---
+++ packages/scripting-tools/scripting_tools-0.1.0.tar.gz/scripting_tools-0.1.0/src/scripting_tools/__init__.py
@@ -0,0 +1,10 @@
+"""
+A package which contains a bunch of small helpful devtools while scripting
+"""
+
+from .argsguard import args_guard
+from .parse import parse_int, parse_float
+from .ostools import *
+from .sysargs import args_to_dictionary
+from . import colorama_ext
+from .progression_bar import progress_bar
