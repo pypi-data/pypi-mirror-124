3e82f84205b095ef0e370da320516f9e66453974
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:26 +0000

    Add package menu.py/0.3.4/menu.py-0.3.4-py3-none-any.whl

---
+++ packages/menu.py/menu.py-0.3.4-py3-none-any.whl/menu/__init__.py
@@ -0,0 +1,37 @@
+"""
+menu.py
+=============================================
+A simple python package for in-terminal menu creation on Windows
+
+copyright (c) 2021-present deadyeti
+
+MIT License, see LICENSE for more details.
+"""
+
+# import functions
+#from .menu import generate
+#from .menu import terminate
+
+# import classes
+#from .menu import cprint
+#from .menu import eprint
+
+# import modules
+from .menu import *
+from .functions.menuinput import *
+from .functions.menuprint import *
+from .classes.theme import formatting
+
+__all__ = [
+    'menu',
+    'menuprint',
+    'menuprint_error',
+    'menuinput',
+    'formatting'
+]
+
+__title__ = 'menu'
+__author__ = 'deadyeti'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2021-present deadyeti'
+__version__ = version
