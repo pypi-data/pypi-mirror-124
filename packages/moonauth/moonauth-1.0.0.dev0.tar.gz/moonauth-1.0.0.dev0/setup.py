be7208db820437e1b96c139e9d34bffdb6bd8bb6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:31:15 +0000

    Add package moonauth/1.0.0.dev0/moonauth-1.0.0.dev0.tar.gz

---
+++ packages/moonauth/moonauth-1.0.0.dev0.tar.gz/moonauth-1.0.0.dev0/setup.py
@@ -0,0 +1,25 @@
+import setuptools
+
+long_description = open("README.md").read()
+required = [""] # Comma seperated dependent libraries name
+
+setuptools.setup(
+    name="moonauth",
+    version="1.0.0dev",
+    author="Dominik Gralka",
+    author_email="dominik@gralka.info",
+    license="Do No Harm",
+    description="A Python wrapper for the Moonauth RESTful API",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/moonauth/SDK-NodeJS",
+    #packages = ['moonauth-sdk'],
+    # project_urls is optional
+    project_urls={
+        "Bug Tracker": "https://github.com/moonauth/SDK-Python/issues",
+    },
+    key_words="moonauth, api, wrapper, moon, authentication, passwordless",
+    install_requires=required,
+    packages=setuptools.find_packages(where="src"),
+    python_requires=">=3.6",
+)
