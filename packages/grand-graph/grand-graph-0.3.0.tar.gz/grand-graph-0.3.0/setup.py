151df19e04732c1c74e0951d69f02fcc8e046c20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:59 +0000

    Add package grand-graph/0.3.0/grand-graph-0.3.0.tar.gz

---
+++ packages/grand-graph/grand-graph-0.3.0.tar.gz/grand-graph-0.3.0/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+    name="grand-graph",
+    version="0.3.0",
+    author="Jordan Matelsky",
+    author_email="opensource@matelsky.com",
+    description="Graph database wrapper for non-graph datastores",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/aplbrain/grand",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "networkx>=2.4",
+        "numpy",
+        "pandas",
+    ],
+    extra_requires={
+        "sql": ["SQLAlchemy>=1.3"],
+        "dynamodb": ["boto3"],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
