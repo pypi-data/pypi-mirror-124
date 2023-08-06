a8868ecc568de9d1dcf97d7385b208c2f6fbf4e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:27:15 +0000

    Add package pytinyrenderer/0.0.13/pytinyrenderer-0.0.13.tar.gz

---
+++ packages/pytinyrenderer/pytinyrenderer-0.0.13.tar.gz/pytinyrenderer-0.0.13/python/CMakeFiles/pytinyrenderer.dir/DependInfo.cmake
@@ -0,0 +1,34 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/erwincoumans/dev/tinyrenderer/geometry.cpp" "/home/erwincoumans/dev/tinyrenderer/python/CMakeFiles/pytinyrenderer.dir/__/geometry.cpp.o"
+  "/home/erwincoumans/dev/tinyrenderer/model.cpp" "/home/erwincoumans/dev/tinyrenderer/python/CMakeFiles/pytinyrenderer.dir/__/model.cpp.o"
+  "/home/erwincoumans/dev/tinyrenderer/our_gl.cpp" "/home/erwincoumans/dev/tinyrenderer/python/CMakeFiles/pytinyrenderer.dir/__/our_gl.cpp.o"
+  "/home/erwincoumans/dev/tinyrenderer/tgaimage.cpp" "/home/erwincoumans/dev/tinyrenderer/python/CMakeFiles/pytinyrenderer.dir/__/tgaimage.cpp.o"
+  "/home/erwincoumans/dev/tinyrenderer/tinyrenderer.cpp" "/home/erwincoumans/dev/tinyrenderer/python/CMakeFiles/pytinyrenderer.dir/__/tinyrenderer.cpp.o"
+  "/home/erwincoumans/dev/tinyrenderer/python/pytinyrenderer.cc" "/home/erwincoumans/dev/tinyrenderer/python/CMakeFiles/pytinyrenderer.dir/pytinyrenderer.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "_USE_MATH_DEFINES"
+  "pytinyrenderer_EXPORTS"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "python/.."
+  "python/../third_party/pybind11/include"
+  "/usr/include/python3.6m"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
