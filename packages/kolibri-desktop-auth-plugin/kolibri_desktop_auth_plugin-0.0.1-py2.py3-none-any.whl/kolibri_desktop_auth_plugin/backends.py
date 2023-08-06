78c69c47a7f28ac24249c21a46db0dea7bf111aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:08:03 +0000

    Add package kolibri-desktop-auth-plugin/0.0.1/kolibri_desktop_auth_plugin-0.0.1-py2.py3-none-any.whl

---
+++ packages/kolibri-desktop-auth-plugin/kolibri_desktop_auth_plugin-0.0.1-py2.py3-none-any.whl/kolibri_desktop_auth_plugin/backends.py
@@ -0,0 +1,40 @@
+from gi.repository import Gio
+
+from .models import DesktopUser
+
+
+DBUS_ID = "org.learningequality.Kolibri.Daemon"
+DBUS_PATH = "/" + DBUS_ID.replace(".", "/") + "/Private"
+IFACE = DBUS_ID + ".Private"
+
+
+class TokenAuthBackend:
+    def _get_user_details(self, token):
+        bus = Gio.bus_get_sync(Gio.BusType.SESSION, None)
+        proxy = Gio.DBusProxy.new_sync(
+            bus, 0, None, DBUS_ID, DBUS_PATH, IFACE, None
+        )
+
+        try:
+            details = proxy.CheckLoginToken("(s)", token)
+        except Exception:
+            return None
+
+        return details
+
+    def authenticate(self, request, token=None, **kwargs):
+        user_details = self._get_user_details(token)
+        if not user_details:
+            return None
+
+        if "user_id" not in user_details:
+            return None
+
+        user = DesktopUser.objects.get_or_create(**user_details)
+        return user.user
+
+    def get_user(self, user_id):
+        try:
+            return DesktopUser.objects.get(user__pk=user_id).user
+        except DesktopUser.DoesNotExist:
+            return None
