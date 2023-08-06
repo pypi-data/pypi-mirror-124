9332140843c4e0d64d6dd12b476c10b28b640058
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:29:10 +0000

    Add package scikit-weak/0.1.5a1/scikit-weak-0.1.5a1.tar.gz

---
+++ packages/scikit-weak/scikit-weak-0.1.5a1.tar.gz/scikit-weak-0.1.5a1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+   name='scikit-weak',
+   version='0.1.5a1',
+   author='Andrea Campagner',
+   author_email='a.campagner@campus.unimib.it',
+   packages=find_packages(include=['scikit_weak', 'scikit_weak.*']),
+   url='https://pypi.org/project/scikit-weak/',
+   license='LICENSE.txt',
+   description='A package featuring utilities and algorithms for weakly supervised ML.',
+   long_description=open('README.md').read(),
+   install_requires=[
+       "scikit-learn",
+       "pandas",
+       "numpy",
+       "scipy"
+   ],
+)
