af91419d6276715c4136e144269bb9978f5df1c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:26:17 +0000

    Add package pytinyrenderer/0.0.12/pytinyrenderer-0.0.12.tar.gz

---
+++ packages/pytinyrenderer/pytinyrenderer-0.0.12.tar.gz/pytinyrenderer-0.0.12/python/CMakeFiles/pytinyrenderer.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pytinyrenderer.dir/__/TinyRenderer.cpp.o"
+  "CMakeFiles/pytinyrenderer.dir/__/geometry.cpp.o"
+  "CMakeFiles/pytinyrenderer.dir/__/model.cpp.o"
+  "CMakeFiles/pytinyrenderer.dir/__/our_gl.cpp.o"
+  "CMakeFiles/pytinyrenderer.dir/__/tgaimage.cpp.o"
+  "CMakeFiles/pytinyrenderer.dir/pytinyrenderer.cpp.o"
+  "libpytinyrenderer.pdb"
+  "libpytinyrenderer.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pytinyrenderer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
