06cd1ab9b30fd529e55884cdf488efa18dd6d3d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:20:35 +0000

    Add package search-me-server/1.0.1.post0/search_me_server-1.0.1.post0-py3-none-any.whl

---
+++ packages/search-me-server/search_me_server-1.0.1.post0-py3-none-any.whl/search_me_server/__init__.py
@@ -0,0 +1,8 @@
+from .api import SearchMeServer
+
+__author__ = "Michael R. Kisel"
+__license__ = "MIT"
+__version__ = "1.0.1r"
+__maintainer__ = "Michael R. Kisel"
+__email__ = "deploy-me@yandex.ru"
+__status__ = "Stable"
