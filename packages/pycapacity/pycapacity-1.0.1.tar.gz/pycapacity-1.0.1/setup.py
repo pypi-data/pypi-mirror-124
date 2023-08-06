ffcd00e05ce2d49af0fc349cd5a0e12285e0a7ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 11:46:42 +0000

    Add package pycapacity/1.0.1/pycapacity-1.0.1.tar.gz

---
+++ packages/pycapacity/pycapacity-1.0.1.tar.gz/pycapacity-1.0.1/setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='pycapacity',
+    version='1.0.1',
+    author='Antun Skuric',
+    author_email='antun.skuric@inria.fr',
+    description='A Real-time capable robot capacity calculation module',
+    long_description=long_description, #'A Real-time capable robot capacity calculation module', 
+    long_description_content_type="text/markdown",
+    url='https://gitlab.inria.fr/askuric/pycapacity',
+    license='MIT',
+    packages=setuptools.find_packages(),
+    install_requires=['numpy','scipy']
+)
