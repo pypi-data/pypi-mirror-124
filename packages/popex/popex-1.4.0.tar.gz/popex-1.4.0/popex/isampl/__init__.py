00ab4acb159d8f8168f67b7f6fde725f5774db38
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 04:11:16 +0000

    Add package popex/1.4.0/popex-1.4.0.tar.gz

---
+++ packages/popex/popex-1.4.0.tar.gz/popex-1.4.0/popex/isampl/__init__.py
@@ -0,0 +1,8 @@
+from .neff import ne
+from .neff import ne_var
+from .neff import ne_gamma
+from .neff import alpha
+from .neff import find_fsigma
+from .neff import correct_w
+
+__all__ = ['ne', 'ne_var', 'ne_gamma', 'alpha', 'find_fsigma', 'correct_w']
