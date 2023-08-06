03c557b9ea4b365110390a307acafcab1571aafd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:27:21 +0000

    Add package pyLFDA-1.0/1.0/pyLFDA%201.0-1.0.tar.gz

---
+++ packages/pyLFDA-1.0/pyLFDA%201.0-1.0.tar.gz/pyLFDA 1.0-1.0/setup.py
@@ -0,0 +1,32 @@
+from distutils.core import setup
+
+setup(
+  name = 'pyLFDA 1.0',
+  packages = ['pyLFDA'],
+  version = '1.0',
+  license='MIT',
+  description = 'pyLFDA is a tool which allows analysis of pairwise lipid force distribution along with other functions such as curvature and diffusion.',
+  author = 'Bhavay Aggarwal',
+  author_email = 'bhavayaggarwal07@gmail.com',
+  url = 'https://github.com/Chokerino',
+  download_url = 'https://github.com/RayLabIIITD/pyLFDA/releases/v_1.tar.gz',
+  keywords = ['Python', 'Lipidomics', 'Analysis'],
+  install_requires=[
+          'numpy',
+          'matplotlib',
+          'MDAnalysis',
+          'scipy',
+          'membrane_curvature',
+      ],
+  classifiers=[
+    'Development Status :: 4 - Beta', 
+    'Intended Audience :: Developers', 
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.9',
+  ],
+)
