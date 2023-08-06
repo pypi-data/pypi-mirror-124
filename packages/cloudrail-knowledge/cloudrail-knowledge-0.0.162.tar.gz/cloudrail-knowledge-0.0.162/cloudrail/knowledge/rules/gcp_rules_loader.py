5b54e83bf1e11eb55d9fad3505ed02dfafc2fb37
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:37:22 +0000

    Add package cloudrail-knowledge/0.0.162/cloudrail-knowledge-0.0.162.tar.gz

---
+++ packages/cloudrail-knowledge/cloudrail-knowledge-0.0.162.tar.gz/cloudrail-knowledge-0.0.162/cloudrail/knowledge/rules/gcp_rules_loader.py
@@ -0,0 +1,16 @@
+from typing import Dict, List
+
+from cloudrail.knowledge.rules.base_rule import BaseRule
+from cloudrail.knowledge.rules.gcp.non_context_aware.sql_database_ssl_required_rule import SqlDatabaseSslRequiredRule
+from cloudrail.knowledge.rules.gcp.non_context_aware.sql_restrict_trusted_ip_rule import SqlDatabaseRestrictTrustedIpRule
+from cloudrail.knowledge.rules.abstract_rules_loader import AbstractRulesLoader
+
+
+class GcpRulesLoader(AbstractRulesLoader):
+
+    def load(self) -> Dict[str, BaseRule]:
+        rules: List[BaseRule] = [
+            SqlDatabaseSslRequiredRule(),
+            SqlDatabaseRestrictTrustedIpRule()
+        ]
+        return {rule.get_id(): rule for rule in rules}
