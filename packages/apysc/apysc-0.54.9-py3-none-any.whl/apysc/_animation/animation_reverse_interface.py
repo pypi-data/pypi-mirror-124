ff529ea3018867cd660b46536c97e3889cf966df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:58 +0000

    Add package apysc/0.54.9/apysc-0.54.9-py3-none-any.whl

---
+++ packages/apysc/apysc-0.54.9-py3-none-any.whl/apysc/_animation/animation_reverse_interface.py
@@ -0,0 +1,29 @@
+"""Class implementation for the animation_reverse interface.
+"""
+
+from apysc._type.variable_name_interface import VariableNameInterface
+
+
+class AnimationReverseInterface(VariableNameInterface):
+
+    def animation_reverse(self) -> None:
+        """
+        Reverse the all running animations.
+
+        Notes
+        -----
+        If you call this interface multiple times and animations
+        have been reached at the beginning or end of the animation,
+        then the reverse instruction will be ignored.
+        This means that the same interval's timer tick reverse
+        setting will not work correctly (since the same interval
+        setting will reach the animation start).
+        """
+        import apysc as ap
+        with ap.DebugInfo(
+                callable_=self.animation_reverse, locals_=locals(),
+                module_name=__name__, class_=AnimationReverseInterface):
+            expression: str = (
+                f'{self.variable_name}.timeline().reverse();'
+            )
+            ap.append_js_expression(expression=expression)
