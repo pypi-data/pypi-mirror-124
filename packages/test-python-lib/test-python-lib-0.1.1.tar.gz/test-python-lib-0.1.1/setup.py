4a8e88e5b65abb51f7cdc3e34aba3c8595c7c37a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:08:44 +0000

    Add package test-python-lib/0.1.1/test-python-lib-0.1.1.tar.gz

---
+++ packages/test-python-lib/test-python-lib-0.1.1.tar.gz/test-python-lib-0.1.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages, setup
+setup(
+    name='test-python-lib',
+    packages=find_packages(),
+    version='0.1.1',
+    description='My first Python library',
+    author='Me',
+    license='MIT',
+)
