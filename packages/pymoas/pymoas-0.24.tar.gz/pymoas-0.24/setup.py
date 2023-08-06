e2a18c0e67e1aac7d1fc36ab23770c8a814271ac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:36:42 +0000

    Add package pymoas/0.24/pymoas-0.24.tar.gz

---
+++ packages/pymoas/pymoas-0.24.tar.gz/pymoas-0.24/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='pymoas',
+      version='0.24',
+      description='short def',
+      packages=['pymoas'],
+      author_email='ivan.selyakovich@gmail.com',
+      zip_safe=False)
