eab054a8328a026158c07459ea1e5abb3ca32ae5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 02:26:56 +0000

    Add package sumandmul/0.0.2/sumandmul-0.0.2.tar.gz

---
+++ packages/sumandmul/sumandmul-0.0.2.tar.gz/sumandmul-0.0.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='sumandmul',
+    version='0.0.2',
+    description='A package for summarizing array and multiplying the result on a number',
+    url="https://github.com/brenscrazy/sumandmulitlyer",
+    author='Vinnikov Gleb',
+    author_email='glebmanufa@gmail.com',
+    packages=['sumandmul'],
+    install_requires=['numpy==1.19'
+                      ]
+    )
