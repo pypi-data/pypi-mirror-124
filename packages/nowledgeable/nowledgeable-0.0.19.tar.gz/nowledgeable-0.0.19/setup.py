b6a8cc3dd94d0b56e59f80acf95a8f1d7dcf692e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:38:18 +0000

    Add package nowledgeable/0.0.19/nowledgeable-0.0.19.tar.gz

---
+++ packages/nowledgeable/nowledgeable-0.0.19.tar.gz/nowledgeable-0.0.19/setup.py
@@ -0,0 +1,20 @@
+from distutils.core import setup
+import site
+import sys
+site.ENABLE_USER_SITE = "--user" in sys.argv[1:]
+
+setup(
+      name='nowledgeable',
+      version='0.0.19',
+      description='Auto checker',
+      author = "Laurent CETINSOY",
+      author_email =  "laurent.cetinsoy+pypi@nowledgeable.com" ,
+      url='https://www.python.org/sigs/distutils-sig/',
+      long_description = "file: README.md",
+      long_description_content_type = "text/markdown",
+      packages=['nowledgeable', 'lib'],
+   
+      entry_points={
+        'console_scripts': ['nowledgeable=main:main']
+      }
+)
