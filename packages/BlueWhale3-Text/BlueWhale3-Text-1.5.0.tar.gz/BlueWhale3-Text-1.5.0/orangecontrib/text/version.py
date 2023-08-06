d8c908b454f06d6256b5fdf99960bd78e2d2b996
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:43:18 +0000

    Add package BlueWhale3-Text/1.5.0/BlueWhale3-Text-1.5.0.tar.gz

---
+++ packages/BlueWhale3-Text/BlueWhale3-Text-1.5.0.tar.gz/BlueWhale3-Text-1.5.0/orangecontrib/text/version.py
@@ -0,0 +1,10 @@
+
+# THIS FILE IS GENERATED FROM ORANGE3-TEXT SETUP.PY
+short_version = '1.6.0'
+version = '1.6.0'
+full_version = '1.6.0.dev0+23807d2'
+git_revision = '23807d2673e8c634d2e294e64bd28033fedc1950'
+release = False
+if not release:
+    version = full_version
+    short_version += ".dev"
