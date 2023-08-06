a3f9ffde961edc32f1520be1ac159d3f8bb7364f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:39:36 +0000

    Add package mmhash2/1.3/mmhash2-1.3.tar.gz

---
+++ packages/mmhash2/mmhash2-1.3.tar.gz/mmhash2-1.3/setup.py
@@ -0,0 +1,34 @@
+from distutils.core import setup, Extension
+
+setup(
+    name='mmhash2',
+    version='1.3',
+    ext_modules=[
+        Extension('mmhash2',
+                  sources=['murmurhash2.cpp'],
+                  language='c++',),
+    ],
+    data_files=[('include', ['murmurhash2.hpp'])],
+    description="MurmurHash2 For Python3",
+    long_description="""
+murmurhash is a fast hash function ::
+    >>> import mmhash2
+
+    >>> print(mmhash2.murmurhash64a("zhenhao", 0x1234))
+    16299279845682521441
+
+    >>> print(mmhash2.dechex(mmhash2.murmurhash64a("zhenhao", 0x1234)))
+    'e232aca990972561'
+
+    >>> print(mmhash2.murmurhash64b('zhenhao', 0x1234))
+    4178523214822422458
+
+
+MurmurHash2 http://murmurhash.googlepages.com/
+MurmurHash2, 64-bit versions, by Austin Appleby
+
+Modified by hit_zhenhao@163.com
+
+linux python lib
+"""
+)
