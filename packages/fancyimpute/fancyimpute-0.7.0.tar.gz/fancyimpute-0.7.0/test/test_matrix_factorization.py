95b83cbe9494ae681e2bdb8844d809fb2163c3e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 09:15:11 +0000

    Add package fancyimpute/0.7.0/fancyimpute-0.7.0.tar.gz

---
+++ packages/fancyimpute/fancyimpute-0.7.0.tar.gz/fancyimpute-0.7.0/test/test_matrix_factorization.py
@@ -0,0 +1,15 @@
+from fancyimpute import MatrixFactorization
+
+from low_rank_data import XY, XY_incomplete, missing_mask
+from common import reconstruction_error
+
+
+def test_matrix_factorization_with_low_rank_random_matrix():
+    solver = MatrixFactorization(learning_rate=0.02, rank=5)
+    XY_completed = solver.fit_transform(XY_incomplete)
+    _, missing_mae = reconstruction_error(XY, XY_completed, missing_mask, name="MatrixFactorization")
+    assert missing_mae < 0.1, "Error too high!"
+
+
+if __name__ == "__main__":
+    test_matrix_factorization_with_low_rank_random_matrix()
