3a5e4b6cbc583bb5eabfc669d87b0fc7aae16e6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:00:52 +0000

    Add package Cpuppy/1.2/Cpuppy-1.2.tar.gz

---
+++ packages/Cpuppy/Cpuppy-1.2.tar.gz/Cpuppy-1.2/README.txt
@@ -0,0 +1,2 @@
+This library was created for personal used to simulate how a cpu or electronic logical gate works
+(I could use the integrated functions but that will be quite boring)
---
+++ packages/Cpuppy/Cpuppy-1.2.tar.gz/Cpuppy-1.2/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+classifiers = [
+    'Development Status :: 5 - Production/Stable',
+    'Intended Audience :: Education',
+    'Operating System :: Microsoft :: Windows :: Windows 10',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3'
+]
+
+setup(
+    name='Cpuppy',
+    version='1.2',
+    description='Library for simple CPU simulating',
+    long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
+    url='',
+    author='Katemi',
+    author_email='thewords32@gmail.com',
+    License='MIT',
+    classifiers=classifiers,
+    keywords='Binary',
+    packages=find_packages(),
+    install_requires=['']
+)
