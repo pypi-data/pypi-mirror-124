4930b7bc5dfde09fa20fa5a2a4b765407d926776
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:29:09 +0000

    Add package ce-bnch-sdk/1.0.0b0/ce_bnch_sdk-1.0.0b0-py3-none-any.whl

---
+++ packages/ce-bnch-sdk/ce_bnch_sdk-1.0.0b0-py3-none-any.whl/benchling_sdk/helpers/logging_helpers.py
@@ -0,0 +1,23 @@
+import logging
+from typing import List, Optional
+
+
+def default_logger(name: str = "benchling_sdk") -> logging.Logger:
+    """Construct the default logger for the SDK."""
+    logger = logging.getLogger(name)
+    logger.addHandler(logging.NullHandler())
+    return logger
+
+
+sdk_logger = default_logger()
+
+
+def log_deprecation(source_name: str, suggestion: str) -> None:
+    """Log a standardized message for use of deprecated functionality."""
+    sdk_logger.warning(f"{source_name} is deprecated. Please use {suggestion}")
+
+
+def check_for_csv_bug_fix(variable: str, value: Optional[List[str]]) -> None:
+    """Specific error for a fixed bug that might cause breaks. See BNCH-24087."""
+    if value and len(value) == 1 and "," in value[0]:
+        raise ValueError(f"Items in {variable} must not contain ','")
