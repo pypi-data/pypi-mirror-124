ae99338b40ca23b2e8a7a8782b92e2dea657ed71
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 13:59:57 +0000

    Add package requests-cache/0.9.0dev0/requests-cache-0.9.0dev0.tar.gz

---
+++ packages/requests-cache/requests-cache-0.9.0dev0.tar.gz/requests-cache-0.9.0dev0/examples/generate_test_db.py
@@ -0,0 +1,120 @@
+#!/usr/bin/env python
+"""An example of generating a test database with a large number of semi-randomized responses.
+This is useful, for example, for reproducing issues that only occur with large caches.
+"""
+import logging
+from datetime import datetime, timedelta
+from os import urandom
+from os.path import getsize
+from random import random
+from time import perf_counter as time
+
+import requests
+from rich.progress import Progress
+
+from requests_cache import ALL_METHODS, CachedResponse, CachedSession
+from requests_cache.models.response import format_file_size
+from tests.conftest import HTTPBIN_FORMATS, HTTPBIN_METHODS
+
+BASE_RESPONSE = requests.get('https://httpbin.org/get')
+CACHE_NAME = 'rubbish_bin.sqlite'
+HTTPBIN_EXTRA_ENDPOINTS = [
+    'anything',
+    'bytes/1024' 'cookies',
+    'ip',
+    'redirect/5',
+    'stream-bytes/1024',
+]
+MAX_EXPIRE_AFTER = 30
+MAX_RESPONSE_SIZE = 10000
+N_RESPONSES = 100000
+N_INVALID_RESPONSES = 10
+
+logging.basicConfig(level='INFO')
+logger = logging.getLogger('requests_cache')
+
+
+class InvalidResponse(CachedResponse):
+    """Response that will raise an exception when deserialized"""
+
+    def __setstate__(self, d):
+        raise ValueError
+
+
+def populate_cache(progress, task):
+    session = CachedSession(CACHE_NAME, backend='sqlite', allowable_methods=ALL_METHODS)
+    n_previous_responses = len(session.cache.responses)
+
+    # Cache a variety of different response formats, which may result in different behavior
+    urls = [
+        ('GET', f'https://httpbin.org/{endpoint}')
+        for endpoint in HTTPBIN_FORMATS + HTTPBIN_EXTRA_ENDPOINTS
+    ]
+    urls += [(method, 'https://httpbin.org/{method.lower()}') for method in HTTPBIN_METHODS]
+    for method, url in urls:
+        session.request(method, url)
+        progress.update(task, advance=1)
+
+    # Cache a large number of responses with randomized response content, which will expire at random times
+    with session.cache.responses.bulk_commit():
+        for i in range(N_RESPONSES):
+            new_response = get_randomized_response(i + n_previous_responses)
+            expires = datetime.now() + timedelta(seconds=random() * MAX_EXPIRE_AFTER)
+            session.cache.save_response(new_response, expires=expires)
+            progress.update(task, advance=1)
+
+    # Add some invalid responses
+    with session.cache.responses.bulk_commit():
+        for i in range(N_INVALID_RESPONSES):
+            new_response = InvalidResponse.from_response(BASE_RESPONSE)
+            new_response.request.url += f'/invalid_response_{i}'
+            key = session.cache.create_key(new_response.request)
+            session.cache.responses[key] = new_response
+            progress.update(task, advance=1)
+
+
+def get_randomized_response(i=0):
+    """Get a response with randomized content"""
+    new_response = CachedResponse.from_response(BASE_RESPONSE)
+    n_bytes = int(random() * MAX_RESPONSE_SIZE)
+    new_response._content = urandom(n_bytes)
+    new_response.request.url += f'/response_{i}'
+    return new_response
+
+
+def remove_expired_responses(expire_after=None):
+    logger.setLevel('DEBUG')
+    session = CachedSession(CACHE_NAME)
+    total_responses = len(session.cache.responses)
+
+    start = time()
+    session.remove_expired_responses(expire_after=expire_after)
+    elapsed = time() - start
+    n_removed = total_responses - len(session.cache.responses)
+    logger.info(
+        f'Removed {n_removed} expired/invalid responses in {elapsed:.2f} seconds '
+        f'(avg {(elapsed / n_removed) * 1000:.2f}ms per response)'
+    )
+
+
+def main():
+    total_responses = len(HTTPBIN_FORMATS + HTTPBIN_EXTRA_ENDPOINTS + HTTPBIN_METHODS)
+    total_responses += N_RESPONSES + N_INVALID_RESPONSES
+
+    with Progress() as progress:
+        task = progress.add_task('[cyan]Generating responses...', total=total_responses)
+        populate_cache(progress, task)
+
+    actual_total_responses = len(CachedSession(CACHE_NAME).cache.responses)
+    cache_file_size = format_file_size(getsize(CACHE_NAME))
+    logger.info(f'Generated cache with {actual_total_responses} responses ({cache_file_size})')
+
+
+if __name__ == '__main__':
+    main()
+
+    # Remove some responses (with randomized expiration)
+    # remove_expired_responses()
+
+    # Expire and remove all responses
+    # remove_expired_responses(expire_after=1)
---
+++ packages/requests-cache/requests-cache-0.9.0dev0.tar.gz/requests-cache-0.9.0dev0/requests_cache/__init__.py
@@ -0,0 +1,52 @@
+# flake8: noqa: E402,F401
+from logging import getLogger
+from typing import Callable, Iterable, Dict
+
+logger = getLogger(__name__)
+
+# Version is defined in pyproject.toml.
+# It's copied here to make it easier for client code to check the installed version.
+__version__ = '0.9.0'
+
+
+def get_placeholder_class(original_exception: Exception = None):
+    """Create a placeholder type for a class that does not have dependencies installed.
+    This allows delaying ImportErrors until init time, rather than at import time.
+    """
+    msg = 'Dependencies are not installed for this feature'
+
+    def _log_error():
+        logger.error(msg)
+        raise original_exception or ImportError(msg)
+
+    class Placeholder:
+        def __init__(self, *args, **kwargs):
+            _log_error()
+
+        def __getattr__(self, *args, **kwargs):
+            _log_error()
+
+        def dumps(self, *args, **kwargs):
+            _log_error()
+
+    return Placeholder
+
+
+def get_valid_kwargs(func: Callable, kwargs: Dict, extras: Iterable[str] = None) -> Dict:
+    """Get the subset of non-None ``kwargs`` that are valid params for ``func``"""
+    params = list(signature(func).parameters)
+    params.extend(extras or [])
+    return {k: v for k, v in kwargs.items() if k in params and v is not None}
+
+
+try:
+    from .backends import *
+    from .cache_control import *
+    from .cache_keys import *
+    from .models import *
+    from .patcher import *
+    from .serializers import *
+    from .session import *
+# Log and ignore ImportErrors, if imported outside a virtualenv (e.g., just to check __version__)
+except ImportError as e:
+    logger.warning(e)
