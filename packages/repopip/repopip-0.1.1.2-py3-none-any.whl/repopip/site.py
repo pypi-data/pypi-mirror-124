c364ea7bfa692bb57b5a65eb39723092eca1272f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:37:15 +0000

    Add package repopip/0.1.1.2/repopip-0.1.1.2-py3-none-any.whl

---
+++ packages/repopip/repopip-0.1.1.2-py3-none-any.whl/repopip/site.py
@@ -0,0 +1,27 @@
+import functools
+
+from flask import (
+    Blueprint, flash, g, redirect, render_template, request, session, url_for
+)
+
+from repopip.local_repo.repo import Repo
+
+repo = Repo()
+
+bp = Blueprint('site', __name__)
+
+@bp.route('/')
+def index():
+    return render_template('pages/index.html.j2', terminal = True)
+
+
+@bp.route('/contacto')
+def contacto():
+    return render_template('pages/contact.html.j2')
+
+
+@bp.route('/packages')
+def packages():
+    repo.loadPackages()
+    packages = repo.packages
+    return render_template('pages/packages.html.j2', packages = packages, total = len(packages), size = repo.size)
