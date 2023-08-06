1f6753a92f33a93187d9c572744d6a372223555c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:37:22 +0000

    Add package mathpad/0.1.9/mathpad-0.1.9.tar.gz

---
+++ packages/mathpad/mathpad-0.1.9.tar.gz/mathpad-0.1.9/mathpad/functions.py
@@ -0,0 +1,23 @@
+from typing import List, Optional, Tuple
+from sympy import Piecewise
+
+from mathpad.val import Val, GenericVal, Q
+from mathpad._quality_of_life import frac
+
+# TODO: improve API once ">", "<", ">=" etc operators are implemented for Val
+def piecewise(x: Val, region_vals: List[Tuple[float, Q[GenericVal]]]) -> GenericVal:
+    "a piecewise series of <"
+    assert any(region_vals)
+    assert region_vals[-1][0] == float("inf")
+    inp = []
+    prev_pqty: Optional[Val] = None
+    for lt, pqty in region_vals:
+        rescaled = pqty.in_units(prev_pqty) if prev_pqty is not None else pqty
+        inp.append((rescaled.val, x.val < lt))
+        prev_pqty = pqty
+
+    return prev_pqty.new(Piecewise(*inp))
+
+
+def sqrt(x: Q[Val]):
+    return x ** frac(1, 2)
