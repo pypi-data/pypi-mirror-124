3d716171660fe226b057fef7fff6b41697e52d73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:14:03 +0000

    Add package pykernel/0.0.1/Pykernel-0.0.1.tar.gz

---
+++ packages/pykernel/Pykernel-0.0.1.tar.gz/Pykernel-0.0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+setup(
+    name="Pykernel",
+    version="0.0.1",
+    description="Python & text Windows CMD Editior written with only native libaries",
+    py_modules=["pykernel"],
+    package_dir={"":"code"}
+)
