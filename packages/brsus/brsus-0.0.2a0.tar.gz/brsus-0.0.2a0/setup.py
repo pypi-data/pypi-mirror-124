96b3a83a6c77c8a2964e5624c170f5f48bea03f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:02:29 +0000

    Add package brsus/0.0.2a0/brsus-0.0.2a0.tar.gz

---
+++ packages/brsus/brsus-0.0.2a0.tar.gz/brsus-0.0.2a0/setup.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['brsus', 'brsus.interfaces', 'brsus.sim', 'brsus.toolbox']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['cantools==36.4.0', 'dbfread==2.0.7', 'pandas==1.3.4', 'pyarrow==5.0.0']
+
+entry_points = \
+{'console_scripts': ['fmt = scripts.code_quality:do_code_formatting',
+                     'fmt-check = scripts.code_quality:check_code_formatting',
+                     'isort-check = scripts.code_quality:check_import_order',
+                     'isort-fmt = scripts.code_quality:sort_import_order',
+                     'linter = scripts.code_quality:linter',
+                     'tests = scripts.code_quality:run_tests']}
+
+setup_kwargs = {
+    'name': 'brsus',
+    'version': '0.0.2a0',
+    'description': '',
+    'long_description': None,
+    'author': 'Ale Farias',
+    'author_email': '0800alefarias@gmail.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.8,<4.0',
+}
+
+
+setup(**setup_kwargs)
