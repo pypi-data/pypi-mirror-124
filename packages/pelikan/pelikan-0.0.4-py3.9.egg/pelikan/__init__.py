05138e6201e25468eea88954d4424bbbc3340108
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:37:24 +0000

    Add package pelikan/0.0.4/pelikan-0.0.4-py3.9.egg

---
+++ packages/pelikan/pelikan-0.0.4-py3.9.egg/pelikan/__init__.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2021-present, Hakan Özler
+# All rights reserved.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+from .Pelikan import Pelikan
+from .Pelikan import main
