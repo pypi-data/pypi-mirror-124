c4db91154fd7ee601e116127113c16e7a80825c1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:39:03 +0000

    Add package codetoolkit/0.1.11/codetoolkit-0.1.11.tar.gz

---
+++ packages/codetoolkit/codetoolkit-0.1.11.tar.gz/codetoolkit-0.1.11/test/test_delimiter.py
@@ -0,0 +1,8 @@
+import logging
+logging.basicConfig(level = logging.DEBUG)
+
+from codetoolkit.delimiter import Delimiter
+
+if __name__ == "__main__":
+
+    print(Delimiter.split_camel("threadpool"))
