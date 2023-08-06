eb47e16fa93bd7cc45476834c6da9fe65c88bb53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:36:43 +0000

    Add package funniestjoke/0.3/funniestjoke-0.3.tar.gz

---
+++ packages/funniestjoke/funniestjoke-0.3.tar.gz/funniestjoke-0.3/funniestjoke/__init__.py
@@ -0,0 +1,4 @@
+from markdown import markdown
+
+def joke():
+    return markdown('**Hello I am the boss This is purushotham reddy**')
---
+++ packages/funniestjoke/funniestjoke-0.3.tar.gz/funniestjoke-0.3/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(name='funniestjoke',
+      version='0.3',
+      description='The funniest joke in the world',
+      url='http://github.com/purushothamkdr453/funniestjoke',
+      author='purushotham reddy',
+      author_email='purushothamkdr453@gmail.com',
+      license='MIT',
+      packages=['funniestjoke'],
+      install_requires=[
+          'Markdown',
+      ],
+      zip_safe=False)
